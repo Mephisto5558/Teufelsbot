8d686bf64a0ab0b6d53dd829643adc58503fb158
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-04-25 11:47:37 +0000

    uptime now gets the uptime from music module + better formating

--- Commands/Information/uptime.js
+++ Commands/Information/uptime.js
@@ -8,26 +8,56 @@ module.exports = new Command({
   category : "Information",
   slashCommand: false,
   run: async (client, message, interaction) => {
-
+       
+    const axios = require('axios')
+    let response = {};
     let data;
-    let totalSeconds = (client.uptime / 1000);
-    let days = Math.floor(totalSeconds / 86400);
-    totalSeconds %= 86400;
-    let hours = Math.floor(totalSeconds / 3600);
-    totalSeconds %= 3600;
-    let minutes = Math.floor(totalSeconds / 60);
-    let seconds = Math.floor(totalSeconds % 60);
+    let days; let hours; let minutes; let seconds;
+    
+    try {
+      response = await axios.get('https://Teufelswerk-Music-Bot.mephisto5558.repl.co/uptime');
+      response = response.data.total;
+      
+      if(!response) response = false;
+    }
+    catch { response = false };
 
-    data = `The bot is online since exactly ${days} days, ${hours} hours, ${minutes} minutes and ${seconds} secounds.`
-    if(!seconds || secounds === 0) {
-      data = `The bot is online since exactly ${days} days, ${hours} hours and ${minutes} minutes.`
-      if(!minutes || minutes === 0) {
-        data = `The bot is online since exactly ${days} days and ${hours} hours.`
+    function formatUptime(totalSeconds) {
+      let days = Math.floor(totalSeconds / 86400);
+      totalSeconds %= 86400;
+      let hours = Math.floor(totalSeconds / 3600);
+      totalSeconds %= 3600;
+      let minutes = Math.floor(totalSeconds / 60);
+      let seconds = Math.floor(totalSeconds % 60);
+      data = `The main module is online since exactly ${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds.`
+
+      if(!days || days === 0) {
+        data = `The main module is online since exactly ${hours} hours, ${minutes} minutes and ${seconds} seconds.`;
         if(!hours || hours === 0) {
-          data = `The bot is online since exactly ${days} days.`
+          data = `The main module is online since exactly ${minutes} minutes and ${seconds} seconds.`;
+          if(!minutes || minutes === 0) {
+            data = `The main module is online since exactly ${seconds} seconds.`;
+            if(!days) data = false
+            if(days == 0) {
+              data = `The main module is online since exactly 0 seconds.`
+            }
+          }
         }
       }
-    }
-    client.functions.reply(data, message)
+    };
+    
+    formatUptime(client.uptime / 1000);
+    
+    if(message) client.functions.reply(data, message);
+    else interaction.reply(data);
+    
+    if(!response) return;
+    formatUptime(response / 1000);
+    if(!data) return;
+    data = data.replace('main module', 'music module');
+    
+    if(message) client.functions.reply(data, message);
+    else interaction.reply(data)
+
   }
 })
