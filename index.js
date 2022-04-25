5b4ac64f97258666e209821480b7be45f88c8fdf
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2022-04-25 11:47:37 +0000

    uptime now gets the uptime from music module + better formating

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ const express = require("express");
 const Discord = require("discord.js");
 const fs = require("fs");
 
+fs.rmSync('./Logs/debug.log', {force: true});
+
 const app = express();
 const client = new Discord.Client({
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
   intents: 32767,
 });
 
+client.on('debug', debug => fs.appendFileSync('./Logs/debug.log', debug + `\n`));
+
 client.owner = '691550551825055775';
 client.userID = '948978571802710047';
 client.prefix = '.';
 client.slashCommandList = [];
 client.functions = {};
-client.workStats = {messages: [], interactions: []};
 client.guildWhitelist = (fs.readFileSync('./Database/guildWhitelist.db')).toString().split(' ');
 client.aliases = new Discord.Collection();
 client.events = new Discord.Collection();
-client.cooldowns = new Discord.Collection();
+client.cooldown = new Discord.Collection();
 client.commands = new Discord.Collection();
 client.categories = fs.readdirSync("./Commands/");
 
@@ -29,11 +32,11 @@ fs.readdirSync("./Handlers").filter((file) => file.endsWith("_handler.js")).forE
   require(`./Handlers/${handler}`)(client);
 });
 
-//client.on('debug', console.log);
 client.login(process.env.token);
 
 app.listen(1000, () => { console.log("Website is online") })
 app.get("/", (req, res) => { res.send("Hello world!") })
+app.use('/website.ico', express.static('./website.ico'));
 
 process.on('exit', () => {
   client.destroy();
