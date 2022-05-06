dc7aec9eba07f3b942fc3c12d392a55dc805d011
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-05-06 10:27:43 +0000

    fixed err callback and moved file-releated action in file_handler.js

---
+++ Handlers/file_handler.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+  
+module.exports = async client => {
+ 
+  fs.rm('./Logs/debug.log', { force: true }, err => {
+    if(err) console.error(err)
+  })
+      
+  fs.readFile('./Logs/startCount.log', (err, data) => {
+    if(err) return console.error(err);
+    let fileContent = parseInt(data)
+    fileContent++
+    fs.writeFile('./Logs/startCount.log', fileContent.toString(), err => {
+      if(err) console.error(err)
+    })
+  });
+
+  fs.writeFile('./Logs/uptime.log', Date.now().toString(), err => {
+    if(err) console.error(err)
+  })
+
+  client.on('debug', debug => {
+    let date = new Date();
+    let timestamp = `[${
+      ('0' + date.getHours()).slice(-2) }:${
+      ('0' + date.getMinutes()).slice(-2) }:${
+      ('0' + date.getSeconds()).slice(-2)
+    }] `;
+
+    fs.appendFileSync('./Logs/debug.log', timestamp + debug + `\n`);
+    if(debug.includes('Hit a 429')) {
+      console.error('Hit a 429 while executing a request');
+      process.kill(1);
+    }
+  });
+  
+}
--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 console.log('Starting...');
 
-const Discord = require('discord.js');
-const fs = require('fs');
-const defaultSettings = require("./Settings/default.json");
-
-fs.readFileSync('./Logs/startCount.log', (err, data) => {
-  if(err) return console.error(err);
-  let fileContent = parseInt(data)
-  fileContent++
-  fs.writeFile('./Logs/startCount.log', fileContent.toString(), err => console.error(err));
-});
-
-const client = new Discord.Client({
-  partials: ["MESSAGE", "CHANNEL", "REACTION"],
-  intents: 32767,
-});
-
-fs.rmSync('./Logs/debug.log', { force: true });
-client.on('debug', debug => {
-  fs.appendFileSync('./Logs/debug.log', debug + `\n`);
-  if(debug.includes('Hit a 429')) {
-    console.error('Hit a 429 while executing a request');
-    process.kill(1);
-  }
-});
+const
+  { Client, Collection } = require('discord.js'),
+  fs = require('fs'),
+  defaultSettings = require("./Settings/default.json"),
+  
+  client = new Client({
+    partials: ["MESSAGE", "CHANNEL", "REACTION"],
+    intents: 32767,
+  });
 
 client.owner = defaultSettings.ownerID;
 client.prefix = defaultSettings.prefix;
@@ -31,11 +16,11 @@ client.functions = {};
 client.categories = fs.readdirSync("./Commands/");
 client.keys = require('./Settings/keys.js');
 client.guildWhitelist = (fs.readFileSync('./Database/guildWhitelist.db')).toString().split(' ');
-client.aliases = new Discord.Collection();
-client.events = new Discord.Collection();
-client.cooldown = new Discord.Collection();
-client.commands = new Discord.Collection();
-client.slashCommands = new Discord.Collection();
+client.aliases = new Collection();
+client.events = new Collection();
+client.cooldown = new Collection();
+client.commands = new Collection();
+client.slashCommands = new Collection();
 
 module.exports = client;
 
