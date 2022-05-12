f3703b5a89321b7fef451b17b947e3680fc9f54e
Mephisto555 <justinderererste@gmail.com>
2022-05-12 15:19:57 +0200

    changed some stuff for dev setting compatiblity (defaultSettings in index.js)

--- Handlers/slash_command_handler.js
+++ Handlers/slash_command_handler.js
@@ -6,7 +6,7 @@ const
 
 let commandCount = 0;
 let commands = [];
-  
+
 function work(option) {
   if(!option.type) option.type = 1
   else option.type = option.type.toString()
@@ -22,45 +22,45 @@ function work(option) {
 module.exports = async client => {
   const commandClient = new Client(
     client.keys.token,
-    require('../Settings/default.json').userID
+    client.userID
   );
-  
+
   await fs.readdirSync('./Commands').forEach(subFolder => {
     fs.readdirSync(`./Commands/${subFolder}/`).filter(file => file.endsWith('.js')).forEach(file => {
       let command = require(`../Commands/${subFolder}/${file}`);
-      if (!command.slashCommand || command.disabled) return;
-      
-      if(Array.isArray(command.options)) 
+      if(!command.slashCommand || command.disabled) return;
+
+      if(Array.isArray(command.options))
         command.options.forEach(option => { work(option) });
       else if(command.options)
-        for (let commandOption of command.options) { work(commandOption.options) };
+        for(let commandOption of command.options) { work(commandOption.options) };
       commands.push(command)
       client.slashCommands.set(command.name, command)
     })
   });
-  
-  for (let command of commands) {
+
+  for(let command of commands) {
     await commandClient.createCommand({
-      name: command.name,
-      description: command.description,
-      options: command.options
-    })
-    .then(_ => {
-      client.log(`Registered Slash Command ${command.name}`);
-      commandCount++
-    })
-    .catch(err => {
-      console.error(errorColor('[Error Handling] :: Unhandled Slash Handler Error/Catch'));
-      console.error(err);
-      if(err.response.data.errors)
-        console.error(errorColor(JSON.stringify(err.response.data, null, 2)));
-    });
+        name: command.name,
+        description: command.description,
+        options: command.options
+      })
+      .then(_ => {
+        client.log(`Registered Slash Command ${command.name}`);
+        commandCount++
+      })
+      .catch(err => {
+        console.error(errorColor('[Error Handling] :: Unhandled Slash Handler Error/Catch'));
+        console.error(err);
+        if(err.response.data.errors)
+          console.error(errorColor(JSON.stringify(err.response.data, null, 2)));
+      });
     await client.functions.sleep(10000);
   };
-  
+
   client.log(`Loaded ${commandCount} Slash commands\n`);
 
-  
+
   const eventName = 'interactionCreate';
   const event = require(`../Events/${eventName}.js`);
 
--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 console.log('Starting...');
 
-const
-  { Client, Collection } = require('discord.js'),
-  fs = require('fs'),
-  defaultSettings = require("./Settings/default.json"),
-  
-  client = new Client({
-    partials: ["MESSAGE", "CHANNEL", "REACTION"],
-    intents: 32767,
-  });
+const { Client, Collection } = require('discord.js'),
+  fs = require('fs');
 
-client.owner = defaultSettings.ownerID;
+client = new Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: 32767,
+});
+
+let defaultSettings = require("./env.json");
+defaultSettings = Object.assign({}, defaultSettings.global, defaultSettings.dev); //.dev is dev, .main is normal
+
+client.owner = defaultSettings.botOwnerID;
+client.userID = defaultSettings.botUserID;
 client.prefix = defaultSettings.prefix;
 client.functions = {};
 client.startTime = Date.now();
 client.categories = fs.readdirSync('./Commands/');
-client.keys = require('./Settings/keys.js');
+client.keys = Object.assign({}, defaultSettings.keys, { token: defaultSettings.token });
 client.guildWhitelist = (fs.readFileSync('./Database/guildWhitelist.db')).toString().split(' ');
 client.aliases = new Collection();
 client.events = new Collection();
@@ -24,12 +26,13 @@ client.commands = new Collection();
 client.slashCommands = new Collection();
 client.log = function log(...data) {
   let date = new Date().toLocaleString('en-GB', {
-    hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit'
+    hour12: false,
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
   });
   console.log(`[${date}] ${data}`)
 };
-
-//module.exports = client;
 
 fs.readdirSync('./Handlers')
   .filter(file => file.endsWith('_handler.js'))
@@ -38,7 +41,7 @@ fs.readdirSync('./Handlers')
   });
 
 client.login(client.keys.token)
-  .then(client.log('Logged in'));
+  .then(client.log(`Logged into ${defaultSettings.type}`));
 
 process.on('exit', _ => {
   client.destroy();
