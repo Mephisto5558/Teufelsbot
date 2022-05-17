7ad522d89fd4b556d9f909efa0bb65b9d13142d5
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-05-18 00:34:31 +0200

    added prefix to database and custom guild prefixes

--- Events/ready.js
+++ Events/ready.js
@@ -1,24 +1,39 @@
 module.exports = async client => {
 
-  checkGuilds()
   async function checkGuilds() {
     await client.db.ready();
     guildWhitelist = await client.db.get('guildWhitelist');
-
+  
     if(guildWhitelist) {
-      for await (guild of client.guilds.cache) {
-        if(!guildWhitelist.includes(guild[1].id)) {
+      for await (let guild of client.guilds.cache) {
+        guild = guild[1];
+        if(!guildWhitelist.includes(guild.id)) {
           (await guild.fetchOwner()).send(
             `Hi. I left one of your guilds: "${guild.name}"\n` +
             `I am currently a whitelist only bot, so please ask .̔̏𝗠𝗲𝗽𝗵𝗶𝘀𝘁𝗼#5558 (discord.gg/u6xjqzz) to whitelist your guild.`
           );
-          guild[1].leave();
+          guild.leave();
           client.log(`Left ${guild.name}(${guild.id}) because it's not in the whitelist`);
-         
         }
       };
     } else client.log('No guild whitlist found')
   }
+  
+  async function getGuildPrefix() {
+    await client.db.ready();
+    let prefixDB = await client.db.get('prefix');
+    if(!prefixDB.default) return console.error('no default prefix found')
+  
+    client.guildData.set('default', { prefix: prefixDB.default });
+  
+    for await (let guild of client.guilds.cache) {
+      guild = guild[1];
+      client.guildData.set(guild.id, { prefix: prefixDB[guild.id] || false });
+    }
+  }
+
+  checkGuilds();
+  getGuildPrefix();
 
   client.log('Ready to receive prefix commands');
   await client.functions.sleep(20000);
