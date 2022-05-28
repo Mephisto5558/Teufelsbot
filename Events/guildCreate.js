d1ddace96deb0e5eb0c51eb7c1698612d4d50672
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-05-28 13:07:04 +0200

    fixed code

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -1,14 +1,14 @@
 module.exports = async (client, guild) => {
 
   await client.db.ready();
-  guildWhitelist = await client.db.get('guildWhitelist');
+  let guildWhitelist = await client.db.get('guildWhitelist');
   if(!guildWhitelist.includes(guild.id)) {
-    guild.leave();
-    client.log(`Left guild "${guild.name}" (${guild.id}) because it's not in the whitelist`);
     (await guild.fetchOwner()).send(
       `Hi. Someone tried to add me to one of your guilds: "${guild.name}"\n` +
       `I am currently a whitelist only bot, so please ask .̔̏𝗠𝗲𝗽𝗵𝗶𝘀𝘁𝗼#5558 (discord.gg/u6xjqzz) to whitelist your guild.`
     );
+    client.log(`Left guild "${guild.name}" (${guild.id}) because it's not in the whitelist`);
+    guild.leave();
   }
   else client.log(`Joined ${guild.name} (${guild.id})`);
   
