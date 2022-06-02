b19492adf0d1a809554ff3d3e3fd03ee4c1ccc4b
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-02 13:53:25 +0200

    added description, category and beta arg

--- Commands/Owner-Only/test.js
+++ Commands/Owner-Only/test.js
@@ -1,18 +1,21 @@
 const { Command } = require("reconlx");
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const colorConfig = require('../../Settings/embed.json').colors;
 
 module.exports = new Command({
   name: 'test',
   aliases: [],
-  description: '',
+  description: 'testing',
   permissions: { client: [], user: [] },
   cooldowns: { global: '', user: '' },
-  category: '',
+  category: 'Owner-Only',
   slashCommand: true,
   prefixCommand: true,
+  beta: true,
   disabled: true,
 
-  run: async(client, _, interaction) => {
-       
+  run: async (client, _, interaction) => {
+  
+
   }
 })
