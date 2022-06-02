d5ba3d0c9adcdbb75d2aa85e59d16d5c17e5fbe2
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-02 21:40:35 +0200

    removed 4096 as size option because of problems

--- Commands/Fun/avatar.js
+++ Commands/Fun/avatar.js
@@ -30,8 +30,7 @@ module.exports = new Command({
         { name: 96,   value: 96   }, { name: 128,  value: 128  },
         { name: 256,  value: 256  }, { name: 300,  value: 300  },
         { name: 512,  value: 512  }, { name: 600,  value: 600  },
-        { name: 1024, value: 1024 }, { name: 2048, value: 2048 },
-        { name: 4096, value: 4096 }
+        { name: 1024, value: 1024 }, { name: 2048, value: 2048 }
       ]
     }
   ],
