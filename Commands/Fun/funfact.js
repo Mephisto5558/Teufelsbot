d40f5af9efa3f9026f0620127025fcbbe489075d
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-04 19:38:39 +0200

    added funfact command

---
+++ Commands/Fun/funfact.js
@@ -0,0 +1,30 @@
+const { Command } = require('reconlx');
+const axios = require('axios');
+
+module.exports = new Command({
+  name: 'funfact',
+  alias: ['fact'],
+  description: 'Get some funfacts',
+  usage: 'funfact',
+  permissions: { client: [], user: [] },
+  cooldowns: { global: 0, user: 1000 },
+  category: 'FUN',
+  slashCommand: false,
+  prefixCommand: true, beta: true,
+
+  run: async(client, message) => {
+
+    await axios.get(
+      'https://api.api-ninjas.com/v1/facts', {
+      headers: { 'X-Api-Key': client.keys.FunFactAPI},
+      contentType: 'application/json',
+    })
+    .then(res => {
+      client.functions.reply(`${res.data[0].fact}.`, message)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+
+  }
+})
