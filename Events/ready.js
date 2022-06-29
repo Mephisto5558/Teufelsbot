820fa3287067258dcb1e609660eaeebf93186d8b
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-06-29 22:03:21 +0200

    removed client.guildData

--- Events/ready.js
+++ Events/ready.js
@@ -1,31 +1,4 @@
-async function getGuildPrefix(client) {
-  const prefixDB = await client.db.get('prefix');
-
-  if (!prefixDB.default) return console.error('no default prefix in prefixDB found');
-
-  client.guildData.set('default', { prefix: prefixDB.default });
-
-  for await (let guild of client.guilds.cache) {
-    guild = guild[1];
-    client.guildData.set(guild.id, { prefix: prefixDB[guild.id] || false });
-  }
-}
-
-async function getAutoPublishGuilds(client) {
-  const publishDB = await client.db.get('autopublish');
-
-  if (!publishDB) return;
-
-  for(const guild of publishDB) {
-    if(client.guildData[guild]) client.guildData[guild].autoPublish = true
-    else client.guildData[guild] = { autoPublish: true };
-  }
-}
-
 module.exports = async client => {
-  getGuildPrefix(client);
-  getAutoPublishGuilds(client);
-
   const activity = await client.db.get('activity');
 
   client.user.setActivity(
