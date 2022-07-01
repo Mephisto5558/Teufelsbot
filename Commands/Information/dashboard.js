14d272d9712483ff2f6889ffb0ed39eb0f216d7c
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-01 13:23:21 +0200

    added dashboard command

---
+++ Commands/Information/dashboard.js
@@ -0,0 +1,27 @@
+const
+  { Command } = require('reconlx'),
+  { MessageEmbed } = require('discord.js'),
+  package = require('../../package.json'),
+  { colors } = require('../../Settings/embed.json');
+
+module.exports = new Command({
+  name: 'dashboard',
+  alias: [],
+  description: 'get the link to the dashboard',
+  usage: '',
+  permissions: { client: [], user: [] },
+  cooldowns: { global: 0, user: 0 },
+  category: 'Information',
+  slashCommand: true,
+  prefixCommand: true, beta: true,
+
+  run: async(client, message, interaction) => {
+    const embed = new MessageEmbed()
+      .setTitle('Dashboard')
+      .setDescription(`Click [here](https://${package.name}.${package.author}.repl.co/) to open the dashboard.`)
+      .setColor(colors.discord.BURPLE)
+
+    if(interaction) interaction.editReply({ embeds: [embed]});
+    else client.functions.reply({embeds: [embed]}, message);
+  }
+})
