78fde23e0e46e60405a3334cb008f5821d88b827
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-02 14:40:46 +0200

    fixed typo

--- Commands/Fun/say.js
+++ Commands/Fun/say.js
@@ -10,7 +10,7 @@ module.exports = new Command({
   category: 'Fun',
   slashCommand: true,
   prefixCommand: true,
-  ephemermalDefer: true,
+  ephemeralDefer: true,
   options: [
     {
       name: 'msg',
