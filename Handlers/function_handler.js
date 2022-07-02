7aaedfa54ea3b0e9398b35d5662470c147569312
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-02 14:30:55 +0200

    removed definition of client.functions

--- Handlers/function_handler.js
+++ Handlers/function_handler.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 let functionCount = 0;
 
 module.exports = client => {
-  client.functions = {};
-
   for(const file of fs.readdirSync("./Functions/global").filter(file => file.endsWith(".js"))) {
     const functionName = file.split(".")[0];
 
