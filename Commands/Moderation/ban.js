1c24e81cbc07ae0e795719fd0b61cc2cb325c7d5
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-02 18:30:01 +0200

    added option to kick and ban multiple members/ids

--- Commands/Moderation/ban.js
+++ Commands/Moderation/ban.js
@@ -15,17 +15,25 @@ module.exports = new Command({
   prefixCommand: false,
   options: [
     {
-      name: 'target',
-      description: 'Who do want you to get banned?',
-      type: 'USER',
+      name: 'targets',
+      description: 'Mention member(s) or put ID(s) in to ban them. Put a space between each target',
+      type: 'STRING',
       required: true
     },
     {
       name: 'reason',
-      description: 'The target will see the reason in a DM.',
+      description: 'The target(s) will see the reason in DMs.',
       type: 'STRING',
       required: true
     },
+    {
+      name: 'delete_days_of_messages',
+      description: 'Delete all messages of the targets of the last n days. max. 7d',
+      type: 'NUMBER',
+      min_value: 1,
+      max_value: 7,
+      required: false
+    }
     /*{
       name: 'duration',
       description: 'How long want you to get the target banned, empty for permament',
@@ -36,45 +44,59 @@ module.exports = new Command({
   ],
 
   run: async (_, __, interaction) => {
-    const target = interaction.options.getMember('target');
-    const reason = interaction.options.getString('reason');
+    const
+      targets = new Set([...interaction.options.getString('targets').replace(/[^0-9\s]/g, '').split(' ').filter(e => e?.length == 18)]),
+      reason = interaction.options.getString('reason'),
+      days = interaction.options.getNumber('delete_days_of_messages'),
+      embed = new MessageEmbed({
+        title: 'Banned',
+        description:
+          `You have been banned from \`${interaction.guild.name}\`.\n` +
+          `Moderator: ${interaction.user.tag}\n` +
+          `Reason: ${reason}`,
+        color: colors.RED
+      }),
+      resEmbed = new MessageEmbed({
+        title: 'Ban',
+        description:
+          `Moderator: ${interaction.user.tag}\n` +
+          `Reason: ${reason}\n\n`,
+        color: colors.RED
+      });
 
-    let errorMsg, noMsg;
-    
-    if(!target) errorMsg = `I cannot find that user.`;
-    else if (target.id === interaction.member.id) errorMsg = `You can't ban yourself!`;
-    else if (target.roles.highest.comparePositionTo(interaction.member.roles.highest) > -1 && interaction.guild.ownerId != interaction.user.id)
-      errorMsg = `You don't have the permission to do that!`;
-    else if (!target.bannable) errorMsg = `I don't have the permission to do that!`;
+    for (const rawTarget of targets) {
+      let target, errorMsg, noMsg;
 
-    if (errorMsg) return interaction.editReply(errorMsg);
+      try {
+        target = await interaction.guild.members.fetch(rawTarget);
+      } catch { target = { id: rawTarget } };
 
-    let embed = new MessageEmbed()
-      .setTitle('Banned')
-      .setDescription(
-        `You have been banned from \`${interaction.guild.name}\`.\n` +
-        `Moderator: ${interaction.user.tag}\n` +
-        `Reason: ${reason}`
-      )
-      .setColor(colors.RED);
+      if (target.id == interaction.member.id) errorMsg = `You can't ban yourself!`;
+      else if (target.roles && target.roles.highest.comparePositionTo(interaction.member.roles.highest) > -1 && interaction.guild.ownerId != interaction.user.id)
+        errorMsg = `You don't have the permission to do that!`;
+      else if (target.bannable === false) errorMsg = `I don't have the permission to do that!`;
 
-    try { await target.send({ embeds: [embed] }) }
-    catch { noMsg = true }
+      if (errorMsg) {
+        resEmbed.description += `**${target?.user?.tag ?? target.id}** couldn't been banned.\n${errorMsg}\n`;
+        continue;
+      }
 
-    try { await target.ban({ reason: reason }) }
-    catch (err) {
-      interaction.editReply("I couldn't ban the target");
-      throw new Error("couldn't ban/kick target", err)
+      try {
+        if (!target.send) noMsg = true;
+        else await target.send({ embeds: [embed] })
+      }
+      catch { noMsg = true }
+
+      await interaction.guild.bans.create(target.id, {
+        reason: reason,
+        days: days > 7 ? 7 : days < 1 ? 1 : days
+      });
+
+      resEmbed.description +=
+        `**${target?.user?.tag ?? target.id}** has been successfully banned.\n` +
+        `${noMsg ? `\nI couldn't DM the target.` : ''}`;
     }
 
-    embed
-      .setTitle('Ban')
-      .setDescription(
-        `${target.user.tag} has been successfully banned.\n` +
-        `Reason: ${reason}\n` +
-        `${noMsg ? `\nI couldn't DM the target.` : ''}`
-      );
-
-    interaction.editReply({ embeds: [embed] });
+    interaction.editReply({ embeds: [resEmbed] });
   }
 })
