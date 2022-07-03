72ec8f5a613a434034ad38bd2fe5b386df0b0cd3
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-04 00:57:10 +0200

    Added roleinfo, guildinfo, serverinfo

---
+++ Commands/Information/roleinfo.js
@@ -0,0 +1,52 @@
+const
+  { Command } = require('reconlx'),
+  { MessageEmbed } = require('discord.js');
+
+module.exports = new Command({
+  name: 'roleinfo',
+  alias: ['role-info'],
+  description: 'Get information about a role',
+  usage: 'PREFIX COMMAND: roleinfo <@role | role name | role id>',
+  permissions: { client: ['EMBED_LINKS'], user: [] },
+  cooldowns: { global: 0, user: 1000 },
+  category: 'Information',
+  slashCommand: true,
+  prefixCommand: true,
+  options: [{
+    name: 'role',
+    description: 'the role you want to get information about',
+    type: 'ROLE',
+    required: true
+  }],
+
+  run: async (client, message, interaction) => {
+    if (interaction) message = interaction;
+
+    const role = interaction?.options.getRole('role') || message.mentions.roles.first() || message.guild.roles.cache.find(e => e.id == message.args[0] || e.name == message.args[0]);
+    if (!role) return client.functions.reply('You need to provide a role (by id, name or mention)', message);
+
+    const color = role.hexColor.slice(1);
+    const embed = new MessageEmbed({
+      title: role.name,
+      description: ' ',
+      color: color,
+      fields: [
+        ['Mention', role],
+        ['Members', role.members.size],
+        ['Color', `[${role.hexColor}](https://www.color-hex.com/color/${color})`],
+        ['Mentionable', role.mentionable],
+        ['Hoist', role.hoist],
+        ['Managed', role.managed],
+        ['Position', role.position],
+        ['ID', `\`${role.id}\``],
+        ['Created At', `<t:${Math.round(role.createdTimestamp / 1000)}>`]
+      ].map(e => { return { name: e[0], value: e[1].toString(), inline: !(e[2] === false) } })
+    })
+      .setThumbnail(role.icon ? `https://cdn.discordapp.com/role-icons/${role.guild.id}/${role.icon}.webp?size=80&quality=lossless` : `https://dummyimage.com/80x80/${color}/${color}.png`);
+      
+    if (role.members.size < 16) embed.addField('Members', Array.from(role.members.values()).join(', '));
+    embed.addField('Permissions', `\`${role.permissions.toArray()?.join('`, `') || 'NONE'}\` (${role.permissions.toArray().length})`)
+
+    interaction ? interaction.editReply({ embeds: [embed] }) : client.functions.reply({ embeds: [embed] }, message);
+  }
+})
---
+++ Commands/Information/userinfo.js
@@ -0,0 +1,63 @@
+const
+  { Command } = require('reconlx'),
+  { MessageEmbed } = require('discord.js');
+
+module.exports = new Command({
+  name: 'userinfo',
+  alias: ['user-info'],
+  description: 'Get information about a user',
+  usage: 'PREFIX COMMAND: roleinfo <@user | user name | user tag | user nickname | user id>',
+  permissions: { client: ['EMBED_LINKS'], user: [] },
+  cooldowns: { global: 0, user: 1000 },
+  category: 'Information',
+  slashCommand: true,
+  prefixCommand: true,
+  options: [{
+    name: 'target',
+    description: 'the user you want to get information about',
+    type: 'USER',
+    required: false
+  }],
+
+  run: async (client, message, interaction) => {
+    if (interaction) message = interaction;
+    const
+      arg = message?.args?.[0].replace(/[<@>]/g, ''),
+      member =
+        interaction?.options.getUser('target') || interaction?.member ||
+          arg ? message.guild.members.cache.find(e => e.user.id == arg || e.nickname == arg || e.user.username == arg || e.user.tag == arg) : message.member,
+      user = member.user,
+      color = member.displayHexColor.slice(1);
+
+    let type = user.bot ? 'Bot, ' : '';
+
+    if (member.guild.ownerId == member.id) type += 'Guild Owner';
+    else if (member.permissions.has('ADMINISTRATOR')) type += 'Guild Administrator';
+    else if (member.permissions.has('MODERATE_MEMBERS')) type += 'Guild Moderator';
+    else type += 'Guild Member';
+
+    const embed = new MessageEmbed({
+      title: user.tag,
+      description: ' ',
+      color: color,
+      filter: [
+        ['Mention', user],
+        ['Type', type],
+        ['Roles', member.roles.cache.size],
+        ['Position', `${member.roles.highest.position}, ${member.roles.highest}`],
+        ['ID', `\`${user.id}\``],
+        ['Color', `[${member.displayHexColor}](https://www.color-hex.com/color/${color})`],
+        ['Moderatable', member.moderatable],
+        ['Created At', `<t:${Math.round(user.createdTimestamp / 1000)}>`],
+        ['Joined At', `<t:${Math.round(member.joinedTimestamp / 1000)}>`]
+      ].map(e => { return { name: e[0], value: e[1].toString(), inline: !(e[2] === false) } })
+    })
+      .setThumbnail(member.displayAvatarURL({ dynamic: true }))
+
+    if (member.isCommunicationDisabled()) embed.addField('Timed Out Until', `<t:${Math.round(member.communicationDisabledUntilTimestamp / 1000)}>`, true);
+    embed.addField('Roles with permissions', Array.from(member.roles.cache.values()).filter(e => e.permissions.toArray().length && e.name != '@everyone').join(', '));
+    embed.addField('Permissions', `\`${member.permissions.toArray()?.join('`, `') || 'NONE'}\` (${member.permissions.toArray().length})`)
+
+    interaction ? interaction.editReply({ embeds: [embed] }) : client.functions.reply({ embeds: [embed] }, message);
+  }
+})
