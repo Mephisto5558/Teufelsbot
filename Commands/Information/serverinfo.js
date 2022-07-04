ae36646fd2f8f1db5550deeb4074cb1fd5aa8063
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-04 15:48:24 +0200

    removed beta arg

--- Commands/Information/serverinfo.js
+++ Commands/Information/serverinfo.js
@@ -12,7 +12,7 @@ module.exports = new Command({
   cooldowns: { global: 0, user: 1000 },
   category: 'Information',
   slashCommand: true,
-  prefixCommand: true, beta: true,
+  prefixCommand: true,
 
   run: async (client, message, interaction) => {
     if (interaction) message = interaction;
