100fb143ce1a2ac71ad5f8ed38e6c88c3f5e723d
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-10 15:41:58 +0200

    fixed

--- Handlers/timeEvents_handler.js
+++ Handlers/timeEvents_handler.js
@@ -2,24 +2,24 @@ const
   { CronJob } = require('cron'),
   { MessageEmbed } = require('discord.js');
 
-function formatBirthday(msg, user, year, guild) {
-  if(!msg) return;
+function formatBirthday(msg, user, year) {
+  if (!msg) return;
   return msg
     .replace(/<user.nickname>/g, user.nickname)
     .replace(/<user.username>/g, user.username)
     .replace(/<user.id>/g, user.id)
     .replace(/<user.tag>/g, user.tag)
-    .replace(/<user.joinedAt>/g, joinedAt.toLocaleDateString('en'))
-    .replace(/<guild.id>/g, guild.id)
-    .replace(/<guild.memberCount>/g, guild.memberCount)
-    .replace(/<guild.name>/g, guild.name)
+    .replace(/<user.joinedAt>/g, user.joinedAt.toLocaleDateString('en'))
+    .replace(/<guild.id>/g, user.guild.id)
+    .replace(/<guild.memberCount>/g, user.guild?.memberCount)
+    .replace(/<guild.name>/g, user.guild?.name)
     .replace(/<bornyear>/g, year)
     .replace(/<date>/g, new Date().toLocaleDateString('en'))
-    .replace(/<age>/g, new Date().getFullYear() - year) //<guilds> gets replaced below
+    .replace(/<age>/g, parseInt(year) ? new Date().getFullYear() - year : '<age>') //<guilds> gets replaced below
 }
 
 module.exports = async client => {
- // if (client.botType == 'dev') return client.log('Disabled timed events due to dev version.');
+  // if (client.botType == 'dev') return client.log('Disabled timed events due to dev version.');
 
   await client.ready();
 
@@ -27,9 +27,9 @@ module.exports = async client => {
   new CronJob('00 00 00 * * *', async function () {
     const now = new Date().toLocaleString('en', { month: '2-digit', day: '2-digit' });
 
-    if (client.db.get('birthdays').lastCheckTS == now) return client.log('Already ran birthday check today');
+    //if (client.db.get('birthdays').lastCheckTS == now) return client.log('Already ran birthday check today');
     client.log('started birthday check');
-    
+
     await client.rateLimitCheck('/users/@me/guilds');
 
     const
@@ -39,10 +39,10 @@ module.exports = async client => {
 
     for (let guild of guilds) {
       const settings = gSettings[guild[0]]?.birthday;
-      if (settings?.disabled)
-        continue;
+      if (!gSettings[guild[0]]?.enabledModules?.includes('birthday')) continue;
 
-      guild = guild[1];
+      await client.rateLimitCheck('/users/@me/guilds');
+      guild = await client.guilds.fetch(guild[0]);
 
       for (const entry of Object.entries(oldData)) {
         let channel, user;
@@ -50,9 +50,7 @@ module.exports = async client => {
         entry[2] = entry0.shift();
         if (now != entry0.join('/') || entry[0] == 'lastCheckTS') continue;
 
-        try {
-          user = await guild.members.fetch(entry[0]);
-        }
+        try { user = await guild.members.fetch(entry[0]) }
         catch (err) {
           if (err.code == 10007) continue;
           else throw err;
@@ -65,23 +63,23 @@ module.exports = async client => {
           }
 
           const embed = new MessageEmbed({
-            title: formatBirthday(settings.channelAnnouncement?.embed?.title, user, entry[2], guild) || gSettings.default.birthday.channelAnnouncement.embed.title,
-            description: formatBirthday(settings.channelAnnouncement?.embed?.description, user, entry[2], guild) || gSettings.default.birthday.channelAnnouncement.embed.description,
+            title: formatBirthday(settings.channelAnnouncement?.embed?.title || gSettings.default.birthday.channelAnnouncement.embed.title, user, entry[2])?.replace(/<age>\.?/g, ''),
+            description: formatBirthday(settings.channelAnnouncement?.embed?.description || gSettings.default.birthday.channelAnnouncement.embed.description, user, entry[2])?.replace(/<age>\.?/g, ''),
             color: settings.channelAnnouncement?.embed?.color || gSettings.default.birthday.channelAnnouncement.embed.color
           });
 
-          await channel.send({ content: settings.channelAnnouncement?.content || gSettings.channelAnnouncement.content, embeds: [embed] });
+          await channel.send({ content: settings.channelAnnouncement?.content?.replace(/<age>\.?/g, '') || gSettings.default.birthday.channelAnnouncement.content, embeds: [embed] });
         }
 
         if (settings?.dmAnnouncement?.enabled) {
-          embed = new MessageEmbed({
-            title: formatBirthday(settings.dmAnnouncement?.embed?.title, user, entry[2]) || gSettings.default.birthday.dmAnnouncement.embed.title,
-            description: formatBirthday(settings.dmAnnouncement?.embed?.description, user, entry[2]) || gSettings.default.birthday.dmAnnouncement.embed.description,
+          const embed = new MessageEmbed({
+            title: formatBirthday(settings.dmAnnouncement?.embed?.title || gSettings.default.birthday.dmAnnouncement.embed.title, user, entry[2])?.replace(/<age>\.?/g, ''),
+            description: formatBirthday(settings.dmAnnouncement?.embed?.description || gSettings.default.birthday.dmAnnouncement.embed.description, user, entry[2])?.replace(/<age>\.?/g, ''),
             color: settings.dmAnnouncement?.color || gSettings.default.birthday.dmAnnouncement.embed.color
           });
 
           try {
-            await user.send({ content: settings.dmAnnouncement?.content || gSettings.dmAnnouncement.content, embeds: [embed] });
+            await user.send({ content: settings.dmAnnouncement?.content?.replace(/<age>\.?/g, '') || gSettings.default.birthday.dmAnnouncement.content, embeds: [embed] });
           } catch { }
         }
       }
