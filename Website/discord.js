06715e9840ef32ed6c46f3cbdc2cf9ce18b19301
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-20 20:41:36 +0200

    removed non-dashboard files until they get used agn

--- Website/discord.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  method: ['get'],
-
-  run: res => {
-    res.redirect('https://discord.com/invite/u6xjqzz');
-  }
-}
--- Website/uptime.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  method: ['get'],
-
-  run: async (res, _, client) => {
-    res.send(client.functions.uptime(client));
-  }
-} 
