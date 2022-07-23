a367cc9fa8154ade516976cd1b821c47416c4979
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-23 23:34:52 +0200

    fixed guild cooldown code

--- Functions/private/cooldowns.js
+++ Functions/private/cooldowns.js
@@ -1,18 +1,18 @@
 const { Collection } = require('discord.js');
 
-module.exports = async (client, { id }, { name, cooldowns }) => {
-  if (!client.cooldowns.has(name)) client.cooldowns.set(name, new Collection());
+module.exports = async (client, { user, guild }, { name, cooldowns }) => {
+    if (!client.cooldowns.has(name)) client.cooldowns.set(name, new Collection());
 
   const
     now = Date.now(),
     timestamps = client.cooldowns.get(name),
-    expirationTimeGuild = (timestamps.get('guild') ?? now),
-    expirationTimeUser = (timestamps.get(id) ?? now);
+    expirationTimeGuild = (timestamps.get(`g:${guild.id}`) ?? now),
+    expirationTimeUser = (timestamps.get(`u:${user.id}`) ?? now);
 
   if (expirationTimeGuild > now && expirationTimeGuild > expirationTimeUser) return Math.round((expirationTimeGuild - now) / 1000);
   else if (expirationTimeUser > now && expirationTimeUser > expirationTimeGuild) return Math.round((expirationTimeUser - now) / 1000);
   else {
-    timestamps.set(id, now + cooldowns.user);
-    timestamps.set('guild', now + cooldowns.guild);
+    timestamps.set(`u:${user.id}`, now + cooldowns.user);
+    timestamps.set(`g:${guild.id}`, now + cooldowns.guild);
   }
 }
