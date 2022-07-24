411fc624a5d34267d2a6c6485251846b9370b48e
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-24 16:46:06 +0200

    fixed error with interactions

--- Functions/private/cooldowns.js
+++ Functions/private/cooldowns.js
@@ -1,18 +1,18 @@
 const { Collection } = require('discord.js');
 
-module.exports = async (client, { user, guild }, { name, cooldowns }) => {
-    if (!client.cooldowns.has(name)) client.cooldowns.set(name, new Collection());
+module.exports = async (client, { guild, member }, { name, cooldowns }) => {
+  if (!client.cooldowns.has(name)) client.cooldowns.set(name, new Collection());
 
   const
     now = Date.now(),
     timestamps = client.cooldowns.get(name),
     expirationTimeGuild = (timestamps.get(`g:${guild.id}`) ?? now),
-    expirationTimeUser = (timestamps.get(`u:${user.id}`) ?? now);
+    expirationTimeUser = (timestamps.get(`u:${member.id}`) ?? now);
 
   if (expirationTimeGuild > now && expirationTimeGuild > expirationTimeUser) return Math.round((expirationTimeGuild - now) / 1000);
   else if (expirationTimeUser > now && expirationTimeUser > expirationTimeGuild) return Math.round((expirationTimeUser - now) / 1000);
   else {
-    timestamps.set(`u:${user.id}`, now + cooldowns.user);
     timestamps.set(`g:${guild.id}`, now + cooldowns.guild);
+    timestamps.set(`u:${member.id}`, now + cooldowns.user);
   }
 }
