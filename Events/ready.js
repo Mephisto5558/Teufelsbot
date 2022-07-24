353197145ef2bcf88b43e6e6ef92246d18d1cea8
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-24 19:28:35 +0200

    fixed default activity

--- Events/ready.js
+++ Events/ready.js
@@ -1,5 +1,7 @@
+const { ActivityType } = require('discord.js');
+
 module.exports = async client => {
-  client.user.setActivity(await client.db.get('activity') || { name: '/help', type: 'PLAYING' });
+  client.user.setActivity(await client.db.get('activity') || { name: '/help', type: ActivityType.Playing });
 
   client.log('Ready to receive prefix commands');
 }
