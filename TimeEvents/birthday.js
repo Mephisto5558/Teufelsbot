c2e31b0c3f9dab623058c55c2ad0f31dcb43ac52
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-24 16:56:45 +0200

    moved timeEvents to own directory, added handler

---
+++ TimeEvents/birthday.js
@@ -0,0 +1,85 @@
+const { EmbedBuilder } = require('discord.js');
+
+function formatBirthday(msg, user, year) {
+  if (!msg) return;
+  return msg
+    .replace(/<user.nickname>/g, user.nickname)
+    .replace(/<user.username>/g, user.username)
+    .replace(/<user.id>/g, user.id)
+    .replace(/<user.tag>/g, user.tag)
+    .replace(/<user.joinedAt>/g, user.joinedAt.toLocaleDateString('en'))
+    .replace(/<guild.id>/g, user.guild.id)
+    .replace(/<guild.memberCount>/g, user.guild?.memberCount)
+    .replace(/<guild.name>/g, user.guild?.name)
+    .replace(/<bornyear>/g, year)
+    .replace(/<date>/g, new Date().toLocaleDateString('en'))
+    .replace(/<age>/g, parseInt(year) ? new Date().getFullYear() - year : '<age>') //<guilds> gets replaced below
+}
+
+module.exports = {
+  time: '00 00 00 * * *',
+  startNow: true,
+
+  onTick: async ({ db, guilds, log }) => {
+    const now = new Date().toLocaleString('en', { month: '2-digit', day: '2-digit' });
+
+    if (db.get('birthdays').lastCheckTS == now) return log('Already ran birthday check today');
+    log('started birthday check');
+
+    const
+      guilds = await guilds.fetch(),
+      oldData = await db.get('birthdays');
+
+    for (let guild of guilds) {
+      const settings = await db.get('settings')[guild[0]]?.birthday;
+      const defaultSettings = await db.get('settings').default?.birthday;
+      if (!settings?.enable) continue;
+
+      guild = await guilds.fetch(guild[0]);
+
+      for (const entry of Object.entries(oldData)) {
+        let channel, user;
+        const entry0 = entry[1].split('/');
+        entry[2] = entry0.shift();
+        if (now != entry0.join('/') || entry[0] == 'lastCheckTS') continue;
+
+        try { user = await guild.members.fetch(entry[0]) }
+        catch (err) {
+          if (err.code == 10007) continue;
+          else throw err;
+        }
+
+        if (settings?.ch?.msg?.channel) {
+          try { channel = await guild.channels.fetch(settings.ch?.msg.channel) }
+          catch {
+            return (await guild.fetchOwner()).send(`INFO: the channel configured for the birthday feature in guild \`${guild.name}\` does not exist! Please re-configure it so i can send birthday messages!`)
+          }
+
+          const embed = new EmbedBuilder({
+            title: formatBirthday(settings.ch?.msg?.embed?.title || defaultSettings?.ch?.msg?.embed?.title, user, entry[2])?.replace(/<age>\.?/g, ''),
+            description: formatBirthday(settings.ch?.msg?.embed?.description || defaultSettings?.ch?.msg?.embed?.description, user, entry[2])?.replace(/<age>\.?/g, ''),
+            color: settings.ch?.msg?.embed?.color || defaultSettings?.ch?.msg?.embed.color
+          });
+
+          await channel.send({ content: settings.ch?.msg?.content?.replace(/<age>\.?/g, '') || defaultSettings?.ch?.msg?.content, embeds: [embed] });
+        }
+
+        if (settings?.dm?.msg?.enabled) {
+          const embed = new EmbedBuilder({
+            title: formatBirthday(settings.dm?.msg?.embed?.title || defaultSettings?.dm?.msg?.embed?.title, user, entry[2])?.replace(/<age>\.?/g, ''),
+            description: formatBirthday(settings.dm?.msg?.embed?.description || defaultSettings?.dm?.msg?.embed?.description, user, entry[2])?.replace(/<age>\.?/g, ''),
+            color: settings.dm?.msg?.embed?.color || defaultSettings?.dm?.msg?.embed?.color
+          });
+
+          try {
+            await user.send({ content: settings.dm?.msg?.content?.replace(/<age>\.?/g, '') || defaultSettings?.dm?.msg?.content, embeds: [embed] });
+          } catch { }
+        }
+      }
+    }
+
+    log('Finished birthday check');
+    db.set('birthdays', Object.assign({}, oldData, { 'lastCheckTS': now }));
+  }
+
+}
