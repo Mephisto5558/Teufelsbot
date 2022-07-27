7455ce15b0f435831dce1e0d0606d35deedd73c0
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-07-27 19:03:28 +0200

    added custom pages support, moved pull there

--- Website/custom/git/pull.js
+++ Website/custom/git/pull.js
@@ -1,6 +1,4 @@
-const app = require('express')();
 const { exec } = require('child_process');
-
 const pull = _ => exec('git pull', { maxBuffer: 1024 * 600 }, (err, stdout, stderr) => {
   if (err) console.error(`GIT PULL\nexec error: ${err}`);
   console.log(
@@ -10,12 +8,15 @@ const pull = _ => exec('git pull', { maxBuffer: 1024 * 600 }, (err, stdout, stde
   );
 });
 
+console.log('Git auto pull is running');
 pull();
 
-app.listen(8080);
-app.post('/github/hooks/pull', (_, res) => {
-  res.sendStatus(200);
-  pull();
-});
+module.exports = {
+  type: 'renderHtml',
+  path: '/git/pull',
 
-console.log('Git auto pull is running');
+  run: async _ => {
+    pull();
+    return 'OK';
+  }
+}
