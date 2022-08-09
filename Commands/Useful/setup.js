d23599933f067062ba30b593e2604ea4dc5c48fe
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-09 23:24:58 +0200

    added togglemodule command, added config alias

--- Commands/Useful/setup.js
+++ Commands/Useful/setup.js
@@ -2,8 +2,8 @@ const { Command } = require('reconlx');
 
 module.exports = new Command({
   name: 'setup',
-  aliases: { prefix: [], slash: [] },
-  description: 'Sets the slash commands up.',
+  aliases: { prefix: ['config'], slash: ['config'] },
+  description: 'Configure features of the bot.',
   usage: '',
   permissions: { client: [], user: ['ManageGuild'] },
   cooldowns: { guild: 0, user: 10000 },
@@ -11,19 +11,47 @@ module.exports = new Command({
   slashCommand: true,
   prefixCommand: false,
   beta: true,
-  options: [{
-    name: 'sync',
-    description: 'force syncs my slash commands with your guild',
-    type: 'Subcommand'
-  }],
+  options: [
+    {
+      name: 'sync',
+      description: 'force syncs my slash commands with your guild',
+      type: 'Subcommand'
+    },
+    {
+      name: 'toggle_module',
+      description: 'toggles a module',
+      type: 'Subcommand',
+      options: [{
+        name: 'module',
+        description: 'the module you want to toggle',
+        type: 'String',
+        required: true,
+        choices: [
+          { name: 'gatekeeper', value: 'gatekeeper' },
+          { name: 'birthday', value: 'birthday' }
+        ]
+      }]
+    }
+  ],
 
   run: async (interaction, client) => {
-    const cmd = interaction.options.getSubcommand();
+    switch (interaction.options.getSubcommand()) {
+      case 'sync': {
+        await require('../../Handlers/slash_command_handler.js')(client, interaction.guild.id);
 
-    if (cmd == 'sync') {
-      await require('../../Handlers/slash_command_handler.js')(client, interaction.guild.id);
+        return interaction.editReply('Finished syncing.');
+      }
 
-      interaction.editReply('Finished syncing.');
+      case 'toggle_module': {
+        const
+          module = interaction.options.getString('module'),
+          oldData = client.db.get('guildSettings'),
+          setting = oldData?.[module]?.enable,
+          newData = Object.merge(oldData, { [interaction.guild.id]: { [module]: { enable: !setting } } });
+
+        await client.db.set('guildSettings', newData);
+        return interaction.editReply(`The \`${module}\` module has been ${setting ? 'disabled' : 'enabled'}.`);
+      }
     }
 
   }
