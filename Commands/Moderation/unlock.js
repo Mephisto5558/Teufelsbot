36f9165ab7169bf7e17a0b48ca1ff7a72ee17a5f
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-13 12:23:03 +0200

    added unlock command. lock command coming soon

---
+++ Commands/Moderation/unlock.js
@@ -0,0 +1,69 @@
+const
+  { Command } = require('reconlx'),
+  { PermissionFlagsBits, OverwriteType, EmbedBuilder, Colors, Message } = require('discord.js');
+
+module.exports = new Command({
+  name: 'unlock',
+  aliases: { prefix: [], slash: [] },
+  description: 'Unlocks a channel locked with the lock command.',
+  usage: 'unlock [channel] [reason]',
+  permissions: { client: ['ManageRoles'], user: ['ManageRoles'] },
+  cooldowns: { guild: 0, user: 1000 },
+  category: 'Moderation',
+  slashCommand: true,
+  prefixCommand: true, beta: true,
+  options: [
+    {
+      name: 'channel',
+      description: 'the channel you want to unlock',
+      type: 'Channel',
+      required: false
+    },
+    {
+      name: 'reason',
+      description: 'the reason for unlocking the channel',
+      type: 'String',
+      required: false
+    }
+  ],
+
+  run: async (message, lang, { db }) => {
+    let msg;
+    message instanceof Message ? msg = await message.reply(lang('global.loading')) : message.editReply(lang('global.loading'));
+    message.args?.shift();
+
+    const
+      channel = message.options?.getChannel('channel') || message.mentions?.channels.first() || message.channel,
+      reason = message.options?.getString('reason') || message.args?.join(' ') || 'no reason given',
+      oldData = db.get('guildSettings'),
+      overwrites = Object.entries(oldData[message.guild.id]?.lockedChannels?.[channel.id] || [])?.filter(async ([k, v]) => {
+        if (channel.permissionOverwrites.cache.get(k)?.allow.has(PermissionFlagsBits.SendMessages)) return;
+        if (v == OverwriteType.Role) return (await message.guild.roles.fetch(k)).comparePositionTo(message.guild.members.me.roles.highest) < 0;
+        return (await message.guild.members.fetch(k)).manageable;
+      });
+
+    if (!overwrites.length) return message instanceof Message ? msg.edit('This channel is not locked.') : message.editReply('This channel is not locked.');
+
+    for (const [id, type] of overwrites) {
+      await channel.permissionOverwrites.edit(id,
+        { [PermissionFlagsBits.SendMessages]: true },
+        { type, reason: `unlock command, moderator ${message.user.tag}` }
+      );
+    }
+
+    delete oldData[message.guild.id].lockedChannels[channel.id];
+    db.set('guildSettings', oldData);
+
+    const embed = new EmbedBuilder({
+      title: 'Channel unlocked!',
+      description:
+        'This Channel has been unlocked.\n' +
+        `Moderator: ${message.user.tag}\n` +
+        `Reason: ${reason}`,
+      color: Colors.Red
+    });
+
+    await channel.send({ embeds: [embed] });
+    message instanceof Message ? msg.edit('The channel has been successfully unlocked.') : message.editReply('The channel has been successfully unlocked.');
+  }
+})
