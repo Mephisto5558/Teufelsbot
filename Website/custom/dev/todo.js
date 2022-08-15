a88ade5dccb431e58f08029f092a2799bfc63f42
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-16 01:55:57 +0200

    added todo url

---
+++ Website/custom/dev/todo.js
@@ -0,0 +1,7 @@
+module.exports = {
+  type: 'renderHtml',
+  path: '/dev/todo',
+  title: 'ToDo List',
+
+  run: async _ => '<iframe src="https://docs.google.com/document/d/e/2PACX-1vSP4bpVt4IyUHoJcCN-e_DUKHrBEe4-JwFSUnMpU4s6HtyiidR7okgMSK4xJshFY50qcWpFy1Uve4Eo/pub?embedded=true" style="position: fixed;width:100%;height:100%;" frameborder="0">Loading…</iframe>'
+}
