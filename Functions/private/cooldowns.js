3bc7fe10f09d6c5b6f5eebe0ed2d6af255a190b1
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-17 13:35:30 +0200

    optimized some code

--- Functions/private/cooldowns.js
+++ Functions/private/cooldowns.js
@@ -6,13 +6,14 @@ module.exports = async (client, { guild, member }, { name, cooldowns }) => {
   const
     now = Date.now(),
     timestamps = client.cooldowns.get(name),
-    expirationTimeGuild = (timestamps.get(`g:${guild.id}`) ?? now),
-    expirationTimeUser = (timestamps.get(`u:${member.id}`) ?? now);
+    expirationTimeGuild = timestamps.get(`g:${guild.id}`) ?? now,
+    expirationTimeUser = timestamps.get(`u:${member.id}`) ?? now;
 
   if (expirationTimeGuild > now && expirationTimeGuild > expirationTimeUser) return Math.round((expirationTimeGuild - now) / 1000);
   else if (expirationTimeUser > now && expirationTimeUser > expirationTimeGuild) return Math.round((expirationTimeUser - now) / 1000);
   else {
     timestamps.set(`g:${guild.id}`, now + cooldowns.guild);
     timestamps.set(`u:${member.id}`, now + cooldowns.user);
+    return 0;
   }
 }
