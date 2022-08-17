6962ab86589071c1b9176d7614c67b83c3bdb240
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-17 12:25:30 +0200

    optimized code

--- Commands/Owner-Only/blacklistuser.js
+++ Commands/Owner-Only/blacklistuser.js
@@ -4,7 +4,7 @@ module.exports = new Command({
   name: 'blacklistuser',
   aliases: { prefix: [], slash: [] },
   description: 'blocks a user from using the bot',
-  usage: 'PREFIX Command: blacklistuser <userID | @user>',
+  usage: 'PREFIX Command: blacklistuser <user id>',
   permissions: { client: [], user: [] },
   cooldowns: { guild: 0, user: 0 },
   category: 'Owner-Only',
@@ -15,17 +15,18 @@ module.exports = new Command({
     if (!message.args[0]) return;
 
     const oldData = await db.get('botSettings');
-    let newData;
 
     if (message.args[0] == 'off') {
       if (!oldData.blacklist.includes(message.args[1])) return functions.reply(lang('notFound'), message);
+      db.set('botSettings', Object.merge(oldData, { blacklist: oldData.blacklist.filter(entry => entry != message.args[1]) }));
 
-      newData = Object.merge(oldData, { blacklist: oldData.blacklist.filter(entry => entry != message.args[1]) });
+      return functions.reply(lang('removed', message.args[1]), message)
     }
-    else if (message.args[0] == application.owner.id) return functions.reply(lang('cantBlacklistOwner'), message);
-    else oldData.blacklist.push(message.args[0]);
 
-    await db.set('botSettings', newData || oldData);
-    functions.reply(newData ? lang('removed', message.args[1]) : lang('saved', message.args[0]), message);
+    if (message.args[0] == application.owner.id) return functions.reply(lang('cantBlacklistOwner'), message);
+    
+    oldData.blacklist.push(message.args[0]);
+    await db.set('botSettings', oldData);
+    functions.reply(lang('saved', message.args[0]), message);
   }
 })
