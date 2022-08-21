07eacc7c7310fc772ec76e4a4aadba8482aab234
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-21 12:27:12 +0200

    added help info

--- Commands/Useful/math.js
+++ Commands/Useful/math.js
@@ -11,7 +11,7 @@ module.exports = new Command({
   name: 'math',
   aliases: { prefix: [], slash: [] },
   description: 'run some basic math',
-  usage: 'math [expression]',
+  usage: 'math [expression | "help"]',
   permissions: { client: [], user: [] },
   cooldowns: { guild: 0, user: 100 },
   category: 'Useful',
@@ -19,14 +19,19 @@ module.exports = new Command({
   prefixCommand: true, beta: true,
   options: [{
     name: 'expression',
-    description: 'what do you want me to solve?',
+    description: 'what do you want me to solve? Write "help" for help',
     type: 'String',
     required: true
   }],
 
   run: (message, lang, { functions }) => {
     const expression = (message.args?.[0] || message.options?.getString('expression'))?.replace('\n', ';');
-    if (!expression) message instanceof Message ? functions.reply(lang('noInput'), message) : message.editReply(lang('noInput'));
+    if (!expression) return message instanceof Message ? functions.reply(lang('noInput'), message) : message.editReply(lang('noInput'));
+    
+    if(expression == 'help') {
+      embed.data.description = lang('help');
+      return message instanceof Message ? functions.reply({ embeds: [embed] }, message) : message.editReply({ embeds: [embed] });
+    }
 
     let data;
 
