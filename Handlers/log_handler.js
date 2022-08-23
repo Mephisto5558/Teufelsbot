de7037588d5fda1889b664b99e92e9c409481cce
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-23 22:54:43 +0200

    fixed minor issues with error logs, removed global.errorColor, added stack trace

--- Handlers/log_handler.js
+++ Handlers/log_handler.js
@@ -2,6 +2,7 @@ const
   fs = require('fs'),
   date = new Date().toLocaleDateString('en').replace(/\//g, '-'),
   startCount = parseInt(fs.readFileSync('./Logs/startCount.log') || 0) + 1,
+  errorColor = '\x1b[1;31m%s\x1b[0m',
   getTime = _ => new Date().toLocaleTimeString('en', { timeStyle: 'medium', hour12: false }),
   writeLogFile = (type, ...data) => fs.appendFileSync(`./Logs/${date}_${type}.log`, `[${getTime()}] ${data.join(' ')}\n`);
 
