8049e4cccf211bc792b0100eadb54107090ec712
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-24 11:44:58 +0200

    added git pull command

---
+++ Commands/Owner-Only/gitpull.js
@@ -0,0 +1,19 @@
+const { Command } = require('reconlx');
+
+module.exports = new Command({
+  name: 'gitpull',
+  aliases: { prefix: [], slash: [] },
+  description: 'Run a git pull',
+  usage: '',
+  permissions: { client: [], user: [] },
+  cooldowns: { guild: 0, user: 0 },
+  category: 'Owner-Only',
+  slashCommand: false,
+  beta: true,
+
+  run: async (message, lang, { functions }) => {
+    await require('../../Website/custom/git/pull.js').run();
+    functions.reply(lang('success'), message);
+  }
+})
+
