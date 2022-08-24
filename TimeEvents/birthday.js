4361c28a1dd3afb9102b9beafe684b242b3499ab
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-24 11:39:07 +0200

    fixed birthday check

--- TimeEvents/birthday.js
+++ TimeEvents/birthday.js
@@ -1,9 +1,7 @@
 const { EmbedBuilder } = require('discord.js');
 
 function formatBirthday(msg, user, year) {
-  if (!msg) return;
-  return msg
-    .replace(/<user.nickname>/g, user.nickname)
+  return msg?.replace(/<user.nickname>/g, user.nickname)
     .replace(/<user.username>/g, user.username)
     .replace(/<user.id>/g, user.id)
     .replace(/<user.tag>/g, user.tag)
@@ -28,11 +26,11 @@ module.exports = {
 
     const
       guildList = (await guilds.fetch()).map(e => e.fetch()),
-      oldData = await db.get('userSettings');
+      oldData = db.get('userSettings');
 
     for await (const guild of guildList) {
-      const settings = await db.get('guildSettings')[guild.id]?.birthday;
-      const defaultSettings = await db.get('guildSettings').default?.birthday;
+      const settings = db.get('guildSettings')[guild.id]?.birthday;
+      const defaultSettings = db.get('guildSettings').default.birthday;
       if (!settings?.enable) continue;
 
       const userList = Object.entries(oldData)
@@ -48,30 +46,30 @@ module.exports = {
           else throw err;
         }
 
-        if (settings?.ch?.msg?.channel) {
-          try { channel = await guild.channels.fetch(settings.ch?.msg.channel) }
+        if (settings?.ch?.channel) {
+          try { channel = await guild.channels.fetch(settings.ch.channel) }
           catch {
             return (await guild.fetchOwner()).send(`INFO: the channel configured for the birthday feature in guild \`${guild.name}\` does not exist! Please re-configure it so i can send birthday messages!`)
           }
 
           const embed = new EmbedBuilder({
-            title: formatBirthday(settings.ch?.msg?.embed?.title || defaultSettings?.ch?.msg?.embed?.title, user, entry[2])?.replace(/<age>\.?/g, ''),
-            description: formatBirthday(settings.ch?.msg?.embed?.description || defaultSettings?.ch?.msg?.embed?.description, user, entry[2])?.replace(/<age>\.?/g, ''),
-            color: settings.ch?.msg?.embed?.color || defaultSettings?.ch?.msg?.embed.color
+            title: formatBirthday(settings.ch?.msg?.embed?.title || defaultSettings.ch.msg.embed?.title, user, entry[2])?.replace(/<age>\.?/g, ''),
+            description: formatBirthday(settings.ch?.msg?.embed?.description || defaultSettings.ch.msg.embed?.description, user, entry[2])?.replace(/<age>\.?/g, ''),
+            color: settings.ch?.msg?.embed?.color || defaultSettings.ch.msg.embed?.color
           });
 
-          await channel.send({ content: settings.ch?.msg?.content?.replace(/<age>\.?/g, '') || defaultSettings?.ch?.msg?.content, embeds: [embed] });
+          await channel.send({ content: settings.ch?.msg?.content?.replace(/<age>\.?/g, '') || defaultSettings.ch.msg.content, embeds: [embed] });
         }
 
-        if (settings?.dm?.msg?.enabled) {
+        if (settings?.dm?.enable) {
           const embed = new EmbedBuilder({
-            title: formatBirthday(settings.dm?.msg?.embed?.title || defaultSettings?.dm?.msg?.embed?.title, user, entry[2])?.replace(/<age>\.?/g, ''),
-            description: formatBirthday(settings.dm?.msg?.embed?.description || defaultSettings?.dm?.msg?.embed?.description, user, entry[2])?.replace(/<age>\.?/g, ''),
-            color: settings.dm?.msg?.embed?.color || defaultSettings?.dm?.msg?.embed?.color
+            title: formatBirthday(settings.dm?.msg?.embed?.title || defaultSettings.dm.msg.embed?.title, user, entry[2])?.replace(/<age>\.?/g, ''),
+            description: formatBirthday(settings.dm?.msg?.embed?.description || defaultSettings.dm.msg.embed?.description, user, entry[2])?.replace(/<age>\.?/g, ''),
+            color: settings.dm?.msg?.embed?.color || defaultSettings.dm.msg.embed?.color
           });
 
           try {
-            await user.send({ content: settings.dm?.msg?.content?.replace(/<age>\.?/g, '') || defaultSettings?.dm?.msg?.content, embeds: [embed] });
+            await user.send({ content: settings.dm?.msg?.content?.replace(/<age>\.?/g, '') || defaultSettings.dm.msg.content, embeds: [embed] });
           } catch { }
         }
       }
