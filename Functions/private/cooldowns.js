22e209432a256d8c26360455a3599bc979aca53c
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-26 10:13:36 +0200

    revert changes, added check if cooldown is given

--- Functions/private/cooldowns.js
+++ Functions/private/cooldowns.js
@@ -1,19 +1,19 @@
 const { Collection } = require('discord.js');
 
-module.exports = async (client, { guild, member }, { name, cooldowns }, hc = {}) => {
+module.exports = async (client, { guild, member }, { name, cooldowns }) => {
   if (!client.cooldowns.has(name)) client.cooldowns.set(name, new Collection());
 
   const
     now = Date.now(),
     timestamps = client.cooldowns.get(name),
-    expirationTimeGuild = timestamps.get(hc.id || `g:${guild.id}`) ?? now,
-    expirationTimeUser = timestamps.get(hc.id || `u:${member.id}`) ?? now;
+    expirationTimeGuild = timestamps.get(`g:${guild.id}`) ?? now,
+    expirationTimeUser = timestamps.get(`u:${member.id}`) ?? now;
 
   if (expirationTimeGuild > now && expirationTimeGuild > expirationTimeUser) return Math.round((expirationTimeGuild - now) / 1000);
   else if (expirationTimeUser > now && expirationTimeUser > expirationTimeGuild) return Math.round((expirationTimeUser - now) / 1000);
   else {
-    timestamps.set(hc.id || `g:${guild.id}`, now + (hc.cd || cooldowns.guild));
-    timestamps.set(hc.id || `u:${member.id}`, now + (hc.cd || cooldowns.user));
+    if (cooldowns.guild) timestamps.set(`g:${guild.id}`, now + cooldowns.guild);
+    if (cooldowns.user) timestamps.set(`u:${member.id}`, now + cooldowns.user);
     return 0;
   }
 }
