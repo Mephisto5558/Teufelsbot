10b1f196fda54ae9b232a64c87464ffd02a41de5
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-28 17:11:23 +0200

    switched msg localization package

--- Handlers/2_localisation_handler.js
+++ Handlers/2_localisation_handler.js
@@ -1,29 +1,32 @@
 const
   { readdirSync } = require('fs'),
-  I18n = require('@eartharoid/i18n'),
-  { langaugesToImport } = require('../config.json'),
-  locales = {};
+  { I18n } = require('i18n'),
+  { languagesToImport } = require('../config.json'),
+  staticCatalog = {};
 
-for (const lang of readdirSync('./Locales', { withFileTypes: true }).filter(({ name }) => langaugesToImport.includes(name)).map(e => e.name.split('.')[0])) {
+for (const lang of readdirSync('./Locales', { withFileTypes: true }).filter(({ name }) => languagesToImport.includes(name)).map(e => e.name.split('.')[0])) {
   for (const e of readdirSync(`./Locales/${lang}`, { withFileTypes: true })) {
-    if (!locales[lang]) locales[lang] = {};
+    if (!staticCatalog[lang]) staticCatalog[lang] = {};
     e.name = e.name.split('.')[0];
 
     if (e.isDirectory()) {
-      for (const e2 of readdirSync(`./Locales/${lang}/${e.name}`).map(e2 => e2.split('.')[0])) {
-        if (!locales[lang][e.name]) locales[lang][e.name] = {};
+      for (const e2 of readdirSync(`./Locales/${lang}/${e.name}`).map(e => e.split('.')[0])) {
+        if (!staticCatalog[lang][e.name]) staticCatalog[lang][e.name] = {};
         delete require.cache[require.resolve(`../Locales/${lang}/${e.name}/${e2}`)];
-        locales[lang][e.name][e2] = require(`../Locales/${lang}/${e.name}/${e2}`);
+        staticCatalog[lang][e.name][e2] = require(`../Locales/${lang}/${e.name}/${e2}`);
       }
     }
     else {
       delete require.cache[require.resolve(`../Locales/${lang}/${e.name}`)];
-      locales[lang][e.name] = require(`../Locales/${lang}/${e.name}`);
+      staticCatalog[lang][e.name] = require(`../Locales/${lang}/${e.name}`);
     }
   }
 }
 
-module.exports = async client => {
-  client.lang = new I18n(client.db.get('guildSettings').default.config.lang, locales);
-  client.defaultLangData = client.lang.getLocale(client.db.get('guildSettings').default.config.lang);
-}
+module.exports = client => client.lang = new I18n({
+  defaultLocale: client.db.get('guildSettings').default.config.lang,
+  missingKeyFn: (_, value) => value,
+  staticCatalog,
+  objectNotation: true,
+  retryInDefaultLocale: true
+});
