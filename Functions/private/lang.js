f1e98af8ab0983f9d8d6e55755bb0d35491cd941
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-08-28 17:11:23 +0200

    switched msg localization package

--- Functions/private/lang.js
+++ Functions/private/lang.js
@@ -1,18 +1,8 @@
-module.exports = async (client, { guild }, command) => {
-  let guildLang = client.db.get('guildSettings')[guild.id]?.config?.lang || guild.preferredLocale.slice(0, 2);
-  if (!client.lang.locales.includes(guildLang)) guildLang = client.lang.default_locale;
-  const langData = client.lang.getLocale(guildLang);
+module.exports = ({ db, lang }, { id, preferredLocale }, command) => {
+  const locale = db.get('guildSettings')[id]?.config?.lang || preferredLocale.slice(0, 2);
 
-  return function lang(message, ...args) {
-    let data;
-    if (Object.keys(client.lang.messages[client.lang.default_locale]).includes(message.split('.')[0])) data = langData(message);
-    else data = langData(`commands.${command.category.toLowerCase()}.${command.name.toLowerCase()}.${message}`, ...args);
-
-    if (data !== undefined) return data;
-
-    if (Object.keys(client.lang.messages[client.lang.default_locale]).includes(message.split('.')[0])) data = client.defaultLangData(message);
-    else data = client.defaultLangData(`commands.${command.category.toLowerCase()}.${command.name.toLowerCase()}.${message}`, ...args);
-
-    return data || `NO_TEXT_FOUND: ${message}`;
+  return function language(phrase, ...replacements) {
+    if (phrase.split('.')[0] in lang.getCatalog()[lang.getLocale()]) return lang.__({ phrase, locale, ...replacements });
+    else return lang.__({ phrase: `commands.${command.category.toLowerCase()}.${command.name.toLowerCase()}.${phrase}`, locale }, ...replacements);
   }
 }
