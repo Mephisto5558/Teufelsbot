87a46e51566c58baa67723131f89471435e031df
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-09-02 16:08:04 +0200

    removed unused file

--- Functions/private/lang.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({ db, lang }, { id, preferredLocale }, command) => {
-  const locale = db.get('guildSettings')[id]?.config?.lang || preferredLocale.slice(0, 2);
-
-  return function language(phrase, ...replacements) {
-    if (phrase.split('.')[0] in lang.getCatalog()[lang.getLocale()]) return lang.__({ phrase, locale, ...replacements });
-    else return lang.__({ phrase: `commands.${command.category.toLowerCase()}.${command.name.toLowerCase()}.${phrase}`, locale }, ...replacements);
-  }
-}
