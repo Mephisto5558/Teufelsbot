d683555b3cc28e8f4c6f9af4cfe9ad27122a7ba4
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-09-05 13:55:01 +0200

    .

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -1,9 +1,5 @@
 module.exports = async (client, guild) => {
-  try {
-    const oldData = client.db.get('guildSettings');
-    const newData = oldData.fMerge({ [guild.id]: { position: (Object.values(oldData).sort((a, b) => b.position - a.position)[0].position || 0) + 1 } });
+  const newData = oldData.fMerge({ [guild.id]: { position: (Object.values(client.db.get('guildSettings')).sort((a, b) => b?.position - a?.position)[0].position || 0) + 1 } });
 
-    client.db.set('guildSettings', newData);
-    await require('../Handlers/slash_command_handler.js')(client, guild);
-  } catch { }
+  client.db.set('guildSettings', newData);
 }
