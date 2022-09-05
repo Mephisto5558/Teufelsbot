c8ebf0aebe91be2c4ecf29c418413287547d8b99
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-09-05 12:52:33 +0200

    small fix and optimization

--- Commands/Moderation/mute.js
+++ Commands/Moderation/mute.js
@@ -1,10 +1,12 @@
-const { EmbedBuilder, Colors, PermissionFlagsBits } = require('discord.js');
+const
+  { EmbedBuilder, Colors, PermissionFlagsBits } = require('discord.js'),
+  { getMilliseconds } = require('better-ms');
 
 module.exports = {
   name: 'mute',
   aliases: { prefix: ['timeout'], slash: ['timeout'] },
   description: 'timeouts a member of a given time (max 28d, default 1h)',
-  usage: 'Duration options: you need to use at least one.',
+  usage: '',
   permissions: { client: ['MuteMembers'], user: ['MuteMembers'] },
   cooldowns: { guild: 0, user: 100 },
   category: 'Moderation',
@@ -24,42 +26,19 @@ module.exports = {
       required: true
     },
     {
-      name: 'duration_days',
-      description: 'for how much days you want to mute the target',
-      type: 'Number',
-      maxValue: 28,
-      required: false
-    },
-    {
-      name: 'duration_hours',
-      description: 'for how much hours you want to mute the target',
-      type: 'Number',
-      maxValue: 24,
-      required: false
-    },
-    {
-      name: 'duration_minutes',
-      description: 'for how much minutes you want to mute the target',
-      type: 'Number',
-      maxValue: 60,
-      required: false
-    },
-    {
-      name: 'duration_seconds',
-      description: 'for how much seconds you want to mute the target',
-      type: 'Number',
-      maxValue: 60,
+      name: 'duration',
+      description: 'for how long you want to mute the target (eg. 1w3d2h5min10s)',
+      type: 'String',
       required: false
     }
   ],
 
   run: async (interaction, lang) => {
-
     const
       target = interaction.options.getMember('target'),
       reason = interaction.options.getString('reason'),
-      date = new Date(),
-      oldDate = new Date(date);
+      duration = getMilliseconds(interaction.options.getString('duration') ?? '0'),
+      date = new Date();
 
     let errorMsg, noMsg;
 
@@ -69,20 +48,11 @@ module.exports = {
       errorMsg = lang('noPerm', lang('global.you'));
     else if (target.permissions.has(PermissionFlagsBits.Administrator)) errorMsg = lang('targetIsAdmin');
     else if (!target.moderatable) errorMsg = lang('noPerm', lang('global.i'));
+    else if (typeof duration == 'string') errorMsg = lang('invalidDuration');
 
     if (errorMsg) return interaction.editReply(errorMsg);
 
-    for (const option of interaction.options.data) {
-      switch (option.name.replace('duration_', '')) {
-        case 'days': date.setDate(date.getDate() + option.value); break;
-        case 'hours': date.setTime(date.getTime() + option.value * 3600000); break;
-        case 'minutes': date.setTime(date.getTime() + option.value * 60000); break;
-        case 'seconds': date.setTime(date.getTime() + option.value * 1000); break;
-      }
-    }
-
-    if (date == oldDate) date.setTime(date.getTime() + 3600000); //1h
-    else if (date - oldDate > 2419000000) date.setTime(date.getTime() + 2419000000); //28d
+    date.setTime(date.getTime() + duration.limit({ min: 3600000, max: 2419000000 }));
 
     try { await target.disableCommunicationUntil(date.getTime(), `${reason}, moderator ${interaction.user.tag}`) }
     catch (err) { return interaction.editReply(lang('error', err)) }
@@ -91,7 +61,7 @@ module.exports = {
       title: lang('dmEmbedTitle'),
       description: lang('dmEmbedDescription', {
         guild: interaction.guild.name, mod: interaction.user.tag, reason,
-        until: Math.round(target.communicationDisabledUntilTimestamp / 1000)
+        time: Math.round(target.communicationDisabledUntilTimestamp / 1000)
       }),
       color: Colors.Red
     });
@@ -100,10 +70,9 @@ module.exports = {
     catch { noMsg = true }
 
     embed.data.title = lang('infoEmbedTitle');
-    embed.data.description = lang('infoEmbedDescription', { user: target.user.tag, reason, until: Math.round(target.communicationDisabledUntilTimestamp / 1000) });
+    embed.data.description = lang('infoEmbedDescription', { user: target.user.tag, reason, time: Math.round(target.communicationDisabledUntilTimestamp / 1000) });
     if (noMsg) embed.data.description += lang('noDm');
 
     interaction.editReply({ embeds: [embed] });
-
   }
 }
