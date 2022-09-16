0433d8981b87643e7ac14fcd87a1edba4d4f6d5a
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-09-16 21:02:10 +0200

    fix

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -1,5 +1,6 @@
-module.exports = async (client, guild) => {
-  const newData = oldData.fMerge({ [guild.id]: { position: (Object.values(client.db.get('guildSettings')).sort((a, b) => b?.position - a?.position)[0].position || 0) + 1 } });
-
+module.exports = async ({db, botType}, guild) => {
+  if(botType == 'dev') return;
+  
+  const newData = db.get('guildSettings').fMerge({ [guild.id]: { position: (Object.values(db.get('guildSettings')).sort((a, b) => b?.position - a?.position)[0].position || 0) + 1 } });
   client.db.set('guildSettings', newData);
 }
