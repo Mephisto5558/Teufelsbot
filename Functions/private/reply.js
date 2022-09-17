2463c3110ba622f51fd2bd69ff73f52cc19e259a
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-09-17 13:58:49 +0200

    fixed a possible error

--- Functions/private/reply.js
+++ Functions/private/reply.js
@@ -3,7 +3,7 @@ const { CommandInteraction, Message } = require('discord.js')
 module.exports = async function customReply(reply, deleteTime, allowedMentions = { repliedUser: false }) {
   let sentMessage;
 
-  if (typeof reply != 'object') reply.content = reply;
+  if (typeof reply != 'object') reply = { content: reply };
   if (!reply.allowedMentions) reply.allowedMentions = allowedMentions;
 
   if (this instanceof CommandInteraction) {
@@ -17,7 +17,7 @@ module.exports = async function customReply(reply, deleteTime, allowedMentions =
     try { sentMessage = this.author.id == this.client.user.id ? await this.edit(reply) : await this.reply(reply) }
     catch { sentMessage = await this.channel.send(reply) }
   }
-  else throw new TypeError(`Unsupported Class! Got ${this.constructor.name}`);
+  else throw new Error(`Unsupported Class! Got ${this.constructor.name}`);
 
   if (!isNaN(deleteTime) && sentMessage?.deletable && !this.ephemeral) return setTimeout(sentMessage.delete.bind(sentMessage), deleteTime);
 
