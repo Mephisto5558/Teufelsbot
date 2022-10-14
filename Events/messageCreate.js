43cffac0fe7a0b07dbe24929ff5a9be6dd8b4dbb
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-10-14 17:46:18 +0200

    added new command: afk

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -1,7 +1,9 @@
 const
-  { EmbedBuilder, Colors, ChannelType, PermissionFlagsBits } = require('discord.js'),
-  I18nProvider = require('../Functions/private/I18nProvider.js');
+  { EmbedBuilder, Colors, ChannelType, PermissionFlagsBits, Message } = require('discord.js'),
+  I18nProvider = require('../Functions/private/I18nProvider.js'),
+  cooldowns = require('../Functions/private/cooldowns.js');
 
+/**@this {Message} */
 module.exports = async function messageCreate() {
   let prefixLength;
 
@@ -9,7 +11,7 @@ module.exports = async function messageCreate() {
   const { blacklist, stats = {} } = this.client.db.get('botSettings');
   if (blacklist?.includes(this.author.id)) return;
 
-  const { config, triggers, economy } = this.client.db.get('guildSettings')[this.guild?.id] || {};
+  const { config, triggers, economy, afkMessages } = this.client.db.get('guildSettings')[this.guild?.id] || {};
   const locale = config?.lang || this.guild?.preferredLocale.slice(0, 2) || defaultSettings.config.lang;
 
   if (this.crosspostable && config?.autopublish) this.crosspost();
@@ -19,23 +21,39 @@ module.exports = async function messageCreate() {
 
   this.content = this.content.replaceAll('<@!', '<@');
 
-  const runTriggers = () => {
-    if (this.client.botType != 'dev')
+  const runMessages = async () => {
+    if (this.client.botType != 'dev' && !(await cooldowns.call(this, { name: 'triggers', cooldowns: { user: 1000 } }))) {
       for (const trigger of triggers?.filter(e => this.content?.toLowerCase()?.includes(e.trigger.toLowerCase())) || [])
         this.customReply(trigger.response);
+    }
+
+    const userSettings = this.client.db.get('userSettings');
+    if (userSettings[this.author.id]?.afkMessage?.message || afkMessages?.[this.author.id]?.message) {
+      this.client.db.update('userSettings', `${this.author.id}.afkMessage`, {});
+      this.client.db.update('guildSettings', `${this.guild.id}.afkMessages.${this.author.id}`, {});
+      if (this.member.moderatable && this.member.nickname?.startsWith('[AFK] ')) this.member.setNickname(this.member.nickname.substring(6));
+      this.customReply(I18nProvider.__({ locale }, 'events.afkEnd'));
+    }
+
+    if (!(await cooldowns.call(this, { name: 'afkMsg', cooldowns: { user: 1000 } }))) {
+      for (const member of this.mentions.members.filter((_, e) => (afkMessages?.[e]?.message || userSettings[e]?.afkMessage?.message) && e != this.author.id).values()) {
+        const afkMsg = afkMessages?.[member.id] || userSettings[member.id]?.afkMessage;
+        this.customReply(I18nProvider.__({ locale }, 'events.afkMsg', { member: member.displayName, message: afkMsg.message, timestamp: afkMsg.createdAt }));
+      }
+    }
   };
 
   if (this.content.startsWith(config?.prefix?.caseinsensitive ? guildPrefix.toLowerCase() : guildPrefix)) prefixLength = guildPrefix.length;
   else if (this.content.startsWith(`<@${this.client.user.id}>`)) prefixLength = this.client.user.id.length + 3;
   else {
-    runTriggers();
+    runMessages();
     const eco = economy?.[this.author.id];
     if (
       this.channel.type != ChannelType.DM && economy?.enable && eco?.gaining?.chat && eco.currency < eco.currencyCapacity &&
       this.content.length > (economy.config?.gaining?.chat?.min_message_length ?? defaultSettings.economy.gaining.chat.min_message_length) &&
       this.content.length < (economy.config?.gaining?.chat?.max_message_length ?? defaultSettings.economy.gaining.chat.max_message_length) &&
       !(economy.config.blacklist?.channel?.includes(this.channel.id) || economy.config.blacklist?.users?.includes(this.user.id) || this.member.roles.cache.hasAny(economy.config.blacklist?.roles)) &&
-      !(await require('../Functions/private/cooldowns.js').call(this, { name: 'economy', cooldowns: { user: 20000 } }))
+      !(await cooldowns.call(this, { name: 'economy', cooldowns: { user: 20000 } }))
     ) {
       const currency = parseFloat((eco.currency + eco.gaining.chat + eco.skills.currency_bonus_absolute.lvl ** 2 + eco.gaining.chat * eco.skills.currency_bonus_percentage.lvl ** 2 / 100).limit(0, eco.currencyCapacity).toFixed(3));
 
@@ -55,11 +73,11 @@ module.exports = async function messageCreate() {
   if (!command && this.client.slashCommands.get(this.commandName)) return this.customReply(I18nProvider.__({ locale }, 'events.slashCommandOnly'));
   if ( //DO NOT REMOVE THIS STATEMENT!
     !command || (command.category.toLowerCase() == 'owner-only' && this.author.id != this.client.application.owner.id)
-  ) return runTriggers();
+  ) return runMessages();
 
   const lang = I18nProvider.__.bind(I18nProvider, { locale, backupPath: `commands.${command.category.toLowerCase()}.${command.name}` });
 
-  const cooldown = await require('../Functions/private/cooldowns.js').call(this, command);
+  const cooldown = await cooldowns.call(this, command);
   if (cooldown && !this.client.botType == 'dev') return this.customReply(lang('events.cooldown', cooldown));
 
   if (command.requireEconomy) {
@@ -82,7 +100,8 @@ module.exports = async function messageCreate() {
     return this.reply({ embeds: [embed] });
   }
 
-  command.run.call(this, lang, this.client)
-    .then(() => { if (this.client.botType != 'dev') this.client.db.update('botSettings', `stats.${command.name}`, stats[command.name] + 1 || 1); })
-    .catch(err => require('../Functions/private/error_handler.js').call(this.client, err, this, lang));
+  try {
+    command.run.call(this, lang, this.client)?.catch(err => require('../Functions/private/error_handler.js').call(this.client, err, this, lang));
+    if (this.client.botType != 'dev') this.client.db.update('botSettings', `stats.${command.name}`, stats[command.name] + 1 || 1);
+  } catch (err) { require('../Functions/private/error_handler.js').call(this.client, err, this, lang); }
 };
