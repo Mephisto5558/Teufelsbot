f465d7ff68050879488bd3bd7f5bdfb98227d8a8
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-10-14 17:46:18 +0200

    added new command: afk

---
+++ Commands/Fun/afk.js
@@ -0,0 +1,31 @@
+module.exports = {
+  name: 'afk',
+  aliases: { prefix: [], slash: [] },
+  permissions: { client: [], user: [] },
+  cooldowns: { guild: 0, user: 20 },
+  category: 'Fun',
+  slashCommand: true,
+  prefixCommand: true,
+  options: [
+    {
+      name: 'message',
+      type: 'String',
+      maxLength: 1000
+    },
+    { name: 'global', type: 'Boolean' }
+  ],
+
+  run: function (lang) {
+    const
+      message = this.options?.getString('message') || this.content?.substring(0, 1000) || 'AFK',
+      global = this.options?.getBoolean('global'),
+      createdAt = Math.round(this.createdTimestamp / 1000).toString();
+
+    if (global) this.client.db.update('userSettings', `${this.user.id}.afkMessage`, { message, createdAt });
+    else this.client.db.update('guildSettings', `${this.guild.id}.afkMessages.${this.user.id}`, { message, createdAt });
+
+    if (this.member.moderatable && this.member.displayName.length < 26 && !this.member.nickname?.startsWith('[AFK] ')) this.member.setNickname(`[AFK] ${this.member.displayName}`);
+
+    this.customReply(global ? lang('globalSuccess', message) : lang('success', message));
+  }
+};
