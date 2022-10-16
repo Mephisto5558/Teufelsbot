6a06ea10a221bd9e9b16c406977888444dfa908e
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-10-16 12:00:54 +0200

    new lastping command

---
+++ Commands/Useful/lastping.js
@@ -0,0 +1,37 @@
+const { ChatInputCommandInteraction, BaseGuildTextChannel } = require('discord.js');
+const { EmbedBuilder, Colors } = require('discord.js');
+module.exports = {
+  name: 'lastping',
+  description: 'Shows you where you got pinged.',
+  aliases: { prefix: [], slash: [] },
+  permissions: { client: [], user: [] },
+  cooldowns: { guild: 200, user: 1000 },
+  category: 'Useful',
+  slashCommand: true,
+  prefixCommand: true,
+  ephemeralDefer: true,
+  options: [{
+    name: 'channel',
+    type: 'Channel', description: 'The channel you want to check.',
+    channelTypes: ['GuildText', 'GuildVoice', 'GuildAnnouncement', 'GuildPublicThread', 'GuildPrivateThread']
+  }], beta: true,
+
+  /**@this {ChatInputCommandInteraction} */
+  run: async function (lang, client) {
+    /**@type {BaseGuildTextChannel} */
+    const channel = this.options?.getChannel('channel') || this.mentions?.channels.first() || this.channel;
+    if (!channel.isTextBased()) return this.customReply(lang('invalid'));
+
+    const { url, content = lang('unknown'), author = lang('unknown'), createdAt } = (await channel.messages.fetch({ limit: 100 })).find(e => e.mentions.members.has(this.user.id)) || {};
+    if (!url) return this.customReply(lang('noneFound'));
+
+    const embed = new EmbedBuilder({
+      title: lang('embedTitle'),
+      description: lang('embedDescription', { url, content: content.substring(0, 200), author }),
+      color: Colors.White,
+      footer: { text: createdAt?.toLocaleString(this.locale) }
+    });
+
+    this.customReply({ embeds: [embed] });
+  }
+};
