bad163477f0284b1a00e40143c9c399899a5d0de
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-10-16 13:45:44 +0200

    moved ban & kick code to one function to reduce duplicate code. This makes it currently not possible to reload the command.

--- Commands/Moderation/kick.js
+++ Commands/Moderation/kick.js
@@ -1,5 +1,3 @@
-const { EmbedBuilder, Colors } = require('discord.js');
-
 module.exports = {
   name: 'kick',
   aliases: { prefix: [], slash: [] },
@@ -21,48 +19,5 @@ module.exports = {
     }
   ],
 
-  run: async function (lang) {
-    const
-      targets = new Set([...this.options.getString('targets').replace(/[^0-9\s]/g, '').split(' ').filter(e => e?.length == 18)]),
-      reason = this.options.getString('reason'),
-      embed = new EmbedBuilder({
-        title: lang('infoEmbedTitle'),
-        description: lang('dmEmbedDescription', { guild: this.guild.name, mod: this.user.tag, reason }),
-        color: Colors.Red
-      }),
-      resEmbed = new EmbedBuilder({
-        title: lang('infoEmbedTitle'),
-        description: lang('infoEmbedDescription', { mod: this.user.tag, reason }),
-        color: Colors.Red
-      });
-
-    for (const rawTarget of targets) {
-      let target, errorMsg, noMsg;
-
-      try { target = await this.guild.members.fetch(rawTarget); }
-      catch { target = { id: rawTarget }; }
-
-      if (target.id == this.member.id) errorMsg = lang('cantKickSelf');
-      else if (target.roles.highest.comparePositionTo(this.member.roles.highest) > -1 && this.guild.ownerId != this.user.id)
-        errorMsg = lang('noPerm', lang('global.you'));
-      else if (!target.kickable) errorMsg = lang('noPerm', lang('global.i'));
-
-      if (errorMsg) {
-        resEmbed.data.description += lang('error', { user: target?.user?.tag ?? target.id, err: errorMsg });
-        continue;
-      }
-
-      try { await target.send({ embeds: [embed] }); }
-      catch { noMsg = true; }
-
-      await target.kick(reason);
-
-      resEmbed.data.description += lang('success', target?.user?.tag ?? target.id);
-      if (noMsg) resEmbed.data.description += lang('noDM');
-    }
-
-    if (resEmbed.data.description == lang('infoEmbedDescription', { user: this.user.tag, reason })) resEmbed.data.description += lang('noneFound');
-
-    this.editReply({ embeds: [resEmbed] });
-  }
+  run: require('../../Functions/private/bankick.js')
 };
---
+++ Functions/private/bankick.js
@@ -0,0 +1,48 @@
+const { EmbedBuilder, Colors } = require('discord.js');
+
+module.exports = async function bankick(lang) {
+  if (!['ban', 'kick'].includes(this.commandName)) throw new Error(`"${this.commandName}" is not an accepted commandName.`);
+
+  const
+    targets = new Set([...this.options.getString('targets').replace(/[^0-9\s]/g, '').split(' ').filter(e => e?.length == 18)]),
+    reason = this.options.getString('reason'),
+    embed = new EmbedBuilder({
+      title: lang('infoEmbedTitle'),
+      description: lang('dmEmbedDescription', { guild: this.guild.name, mod: this.user.tag, reason }),
+      color: Colors.Red
+    }),
+    resEmbed = new EmbedBuilder({
+      title: lang('infoEmbedTitle'),
+      description: lang('infoEmbedDescription', { mod: this.user.tag, reason }),
+      color: Colors.Red
+    });
+
+  for (const rawTarget of targets) {
+    let target, errMsg, noMsg;
+
+    try { target = await this.guild.members.fetch(rawTarget); }
+    catch { target = { id: rawTarget }; }
+
+    if (target.id == this.member.id) errMsg = lang('cantPunishSelf');
+    else if (target.roles.highest.comparePositionTo(this.member.roles.highest) > -1 && this.guild.ownerId != this.user.id)
+      errMsg = lang('noPerm', lang('global.you'));
+    else if (!target.manageable) errMsg = lang('noPerm', lang('global.i'));
+
+    if (errMsg) {
+      resEmbed.data.description += lang('error', { user: target?.user?.tag ?? target.id, err: errMsg });
+      continue;
+    }
+
+    try { await target.send({ embeds: [embed] }); }
+    catch { noMsg = true; }
+
+    await (this.commandName == 'kick' ? target.kick(reason) : this.guild.bans.create(target.id, { reason, deleteMessageDays: this.options.getNumber('delete_days_of_messages') }));
+
+    resEmbed.data.description += lang('success', target?.user?.tag ?? target.id);
+    if (noMsg) resEmbed.data.description += lang('noDM');
+  }
+
+  if (resEmbed.data.description == lang('infoEmbedDescription', { mod: this.user.tag, reason })) resEmbed.data.description += lang('noneFound');
+
+  this.editReply({ embeds: [resEmbed] });
+};
