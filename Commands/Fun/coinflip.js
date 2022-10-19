7f08d1cea14bf0ac327a39dba30383e810a10bf9
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-10-19 17:12:47 +0200

    added a coinflip command

---
+++ Commands/Fun/coinflip.js
@@ -0,0 +1,14 @@
+const { randomInt } = require('crypto');
+
+module.exports = {
+  name: 'coinflip',
+  aliases: { prefix: [], slash: [] },
+  permissions: { client: [], user: [] },
+  cooldowns: { guild: 0, user: 100 },
+  category: 'Minigames',
+  slashCommand: true,
+  prefixCommand: true,
+  dmPermission: true,
+
+  run: function () { this.customReply(lang(randomInt(3001) == 0 ? 'side' : 'response')); }
+};
