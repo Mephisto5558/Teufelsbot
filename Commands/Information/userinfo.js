3d842732e4fba9103af5e1e9309e75ace99a4e81
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-11-04 16:49:47 +0100

    added birthday to userinfo, moved getAge function to its own file, changed some text

--- Commands/Information/userinfo.js
+++ Commands/Information/userinfo.js
@@ -1,6 +1,7 @@
 const
   { PermissionFlagsBits, EmbedBuilder } = require('discord.js'),
-  { getAverageColor } = require('fast-average-color-node');
+  { getAverageColor } = require('fast-average-color-node'),
+  getAge = require('../../Functions/private/getAge.js');
 
 module.exports = {
   name: 'userinfo',
@@ -19,7 +20,8 @@ module.exports = {
     const
       member = this.options?.getMember('target') || this.mentions?.members.first() || this.guild.members.cache.find(e => [e.user.id, e.user.username, e.user.tag, e.nickname].some(e => [...this.args, this.content].includes(e))) || this.member,
       user = member.user,
-      color = parseInt((await getAverageColor(member.displayAvatarURL())).hex.substring(1), 16);
+      color = parseInt((await getAverageColor(member.displayAvatarURL())).hex.substring(1), 16),
+      birthday = this.client.db.get('userSettings')[member.id]?.birthday;
 
     let type = user.bot ? 'Bot, ' : '';
 
@@ -33,14 +35,14 @@ module.exports = {
       color: color,
       fields: [
         { name: lang('mention'), value: user.toString(), inline: true },
-        { name: lang('type'), value: type, inline: true },
-        { name: lang('roles'), value: `\`${member.roles.cache.size}\``, inline: true },
-        { name: lang('position'), value: `\`${member.roles.highest.position}\`, ${member.roles.highest}`, inline: true },
         { name: 'ID', value: `\`${user.id}\``, inline: true },
+        { name: lang('type'), value: type, inline: true },
+        { name: lang('position'), value: `\`${member.roles.highest.position}\`, ${member.roles.highest}`, inline: true },
+        { name: lang('roles'), value: `\`${member.roles.cache.size}\``, inline: true },
         { name: lang('color'), value: `[${member.displayHexColor}](https://www.color-hex.com/color/${member.displayHexColor.substring(1)})`, inline: true },
-        { name: lang('moderatable'), value: member.moderatable, inline: true },
         { name: lang('createdAt'), value: `<t:${Math.round(user.createdTimestamp / 1000)}>`, inline: true },
         { name: lang('joinedAt'), value: `<t:${Math.round(member.joinedTimestamp / 1000)}>`, inline: true },
+        birthday ? { name: lang('birthday'), value: `<t:${Math.round(new Date(birthday).getTime() / 1000)}:D> (${getAge(birthday.split('/'))})`, inline: true } : null,
         member.isCommunicationDisabled() ? { name: lang('timedOutUntil'), value: `<t:${Math.round(member.communicationDisabledUntilTimestamp / 1000)}>`, inline: true } : null,
         { name: 'Roles with permissions', value: Array.from(member.roles.cache.values()).filter(e => e.permissions.toArray().length && e.name != '@everyone').join(', '), inline: false },
         { name: 'Permissions', value: `\`${member.permissions.has(PermissionFlagsBits.Administrator) ? lang('admin') : member.permissions.toArray()?.join('`, `') || lang('global.none')}\` (${member.permissions.toArray().length})`, inline: false }
