4ff572e8df659554df914fdf69a9094d9ee2ea3e
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-11-05 13:07:51 +0100

    optimized code, added pic button, translated true and false

--- Commands/Information/emojiinfo.js
+++ Commands/Information/emojiinfo.js
@@ -1,5 +1,5 @@
 const
-  { parseEmoji, EmbedBuilder } = require('discord.js'),
+  { parseEmoji, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js'),
   { getAverageColor } = require('fast-average-color-node');
 
 module.exports = {
@@ -21,24 +21,35 @@ module.exports = {
     const emoji = emojis.cache.get(parsedEmoji.id) || parsedEmoji;
 
     if (!emoji.id) return this.customReply(lang('notFound'));
+    if (!emoji.url) emoji.url = `https://cdn.discordapp.com/emojis/${emoji.id}.webp?size=2048`;
 
     const embed = new EmbedBuilder({
       title: lang('embedTitle', `<:${emoji.name}:${emoji.id}>`),
-      color: parseInt((await getAverageColor(emoji.url())).hex.substring(1), 16),
+      color: parseInt((await getAverageColor(emoji.url)).hex.substring(1), 16),
+      thumbnail: { url: emoji.url },
       fields: [
         { name: lang('name'), value: emoji.name, inline: true },
         { name: lang('id'), value: emoji.id, inline: true },
-        { name: lang('url'), value: emoji.url ? `[Link](${emoji.url})` : lang('unknown'), inline: true },
         { name: lang('guild'), value: emoji.guild?.name ? `${emoji.guild.name} (${emoji.guild.id})` : lang('unknown'), inline: true },
-        { name: lang('animated'), value: emoji.animated.toString(), inline: true },
+        { name: lang('animated'), value: lang(`global.${emoji.animated}`), inline: true },
         { name: lang('creator'), value: emoji.author?.tag ?? lang('unknown'), inline: true },
-        { name: lang('available'), value: emoji.available?.toString() ?? lang('unknown'), inline: true },
+        { name: lang('available'), value: emoji.available ? lang(`global.${emoji.available}`) : lang('unknown'), inline: true },
         { name: lang('createdAt'), value: emoji.createdTimestamp ? `<t:${Math.round(emoji.createdTimestamp / 1000)}>` : lang('unknown'), inline: true },
-        { name: lang('requiresColons'), value: emoji.requiresColons ?? lang('unknown'), inline: true },
-        emoji.roles?.cache.size ? { name: lang('allowedRoles'), value: `<@&${emoji.roles.cache.map(e => e.id).join('>, <@&')}>`, inline: false } : null
+        { name: lang('requiresColons'), value: emoji.requiresColons ? lang(`global.${emoji.requiresColons}`) : lang('unknown'), inline: true },
+        emoji.roles?.cache.size && { name: lang('allowedRoles'), value: `<@&${emoji.roles.cache.map(e => e.id).join('>, <@&')}>`, inline: false }
       ].filter(Boolean)
-    }).setThumbnail(emoji.url);
+    });
 
-    this.customReply({ embeds: [embed] });
+    const component = new ActionRowBuilder({
+      components: [
+        new ButtonBuilder({
+          label: lang('download'),
+          style: ButtonStyle.Link,
+          url: emoji.url
+        })
+      ]
+    });
+
+    this.customReply({ embeds: [embed], components: [component] });
   }
 };
