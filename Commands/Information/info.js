c7ebcdbf4f94e03f9e5a85a6b06d0309bbc7d181
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-11-09 17:24:19 +0100

    added commands count

--- Commands/Information/info.js
+++ Commands/Information/info.js
@@ -18,7 +18,8 @@ module.exports = {
       description =
         `${lang('dev')}: [${owner.tag}](https://discord.com/users/${owner.id})\n` +
         `${lang('shard')}: \`${this.guild.shardId}\`\n` +
-        `${lang('global.guild')}: \`${client.db.get('guildSettings')[this.guild.id]?.position || 0}\n\`` +
+        `${lang('global.guild')}: \`${client.db.get('guildSettings')[this.guild.id]?.position || 0}\`\n` +
+        `${lang('commands')}: \`${new Set(client.prefixCommands.filter(e => !e.aliasOf), client.slashCommands.filter(e => !e.aliasOf)).size}\`\n` +
         `${lang('starts')}: \`${startCount}\`\n` +
         `${lang('lastStart')}: <t:${startTime}> (<t:${startTime}:R>)\n` +
         lang('translation', { de: '[.̔̏𝗠𝗲𝗽𝗵𝗶𝘀𝘁𝗼#5558](https://discord.com/users/691550551825055775) & [Koikarpfen#4992](https://discord.com/users/636196723852705822)', en: '[.̔̏𝗠𝗲𝗽𝗵𝗶𝘀𝘁𝗼#5558](https://discord.com/users/691550551825055775)' }) +
