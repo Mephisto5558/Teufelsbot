64cdcc6ebe0a148bbbf2ed5ae42a0fe81142ceef
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-12-05 23:38:00 +0100

    optimize code

--- Utils/findAllEntries.js
+++ Utils/findAllEntries.js
@@ -1,9 +1,12 @@
 /**@this {{}}@param {string}key@returns {{}}object with found entries*/
-module.exports = function findAllEntries(key, entryList = {}) {
-  if (key in this) entryList = { ...entryList, ...this[key] };
-  if (`${{}}` == this) for (const entry of Object.keys(this).filter(e => e != key)) {
-    const data = findAllEntries.call(this[entry], key, entryList[entry]);
-    if (Object.keys(data || {}).length) entryList[entry] = data;
+module.exports = function findAllEntries(key) {
+  const entryList = {};
+  for (const entry of Object.keys(this)) {
+    if (entry == key) entryList[key] = this[key];
+    else if (typeof this[entry] === 'object') {
+      const data = findAllEntries.call(this[entry], key);
+      if (Object.keys(data).length) entryList[entry] = data;
+    }
   }
   return entryList;
 };
