4a0552596e6ed09bc34aafbe8a8a54ce0340e6f7
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2022-12-05 23:38:00 +0100

    optimize code

--- Utils/getAge.js
+++ Utils/getAge.js
@@ -1,4 +1,4 @@
 module.exports = function getAge([year, month, day]) {
-  const now = new Date();
-  return now.getMonth() - month < -1 || (now.getMonth() - month == -1 && now.getDate() < day) ? now.getFullYear() - year - 1 : now.getFullYear() - year;
+  const now = new Date(), nowMonth = now.getMonth(), nowDay = now.getDate(), age = now.getFullYear() - year;
+  return nowMonth - month < -1 || (nowMonth - month == -1 && nowDay < day) ? age - 1 : age;
 };
