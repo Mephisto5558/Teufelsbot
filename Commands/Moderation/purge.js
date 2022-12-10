3754d1a00101f518120ed99c9b74947f514fb1ce
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-12-10 13:31:25 +0100

    fixed an issue in thread channels and optimized code

--- Commands/Moderation/purge.js
+++ Commands/Moderation/purge.js
@@ -2,33 +2,30 @@ const
   { Message, CommandInteraction, Constants } = require('discord.js'),
   notNullEqual = (str1, str2) => str1 && str1 == str2,
   adRegex = str => /((?=discord)(?<!support\.)(discord(?:app)?[\W_]*(com|gg|me|net|io|plus|link)\/|(?<=\w\.)\w+\/)(?=.)|watchanimeattheoffice[\W_]*com)(?!\/?(attachments|channels)\/)|(invite|dsc)[\W_]*gg|disboard[\W_]*org/gi.test(str), //filters discord invites, invite.gg, dsc.gg, disboard.org links
-  filterCheck = (msg, type) => {
-    switch (type) {
-      case 'text': if (msg.content.length) return; break;
-      case 'embeds': if (msg.embeds?.length) return; break;
-      case 'mentions': if (msg.mentions.users.size) return; break;
-      case 'images': if (msg.attachments?.find(e => e.contentType.includes('image')).length) return; break;
-      case 'server_ads': if (adRegex(msg.content) || msg.embeds?.find(e => adRegex(e.description))) return; break;
-    }
-    return true;
+  filterCheck = {
+    text: msg => msg.content.length,
+    embeds: msg => msg.embeds?.length,
+    mentions: msg => msg.mentions.users.size,
+    images: msg => msg.attachments?.find(e => e.contentType.includes('image')).length,
+    server_ads: msg => adRegex(msg.content) || msg.embeds?.find(e => adRegex(e.description)),
   };
 
-/**@returns {undefined|null|boolean}if the messages should NOT be removed*/
 function checkMsg(msg, getStr) {
-  const options = this.options.data.map(e => e.name);
-  if (
-    (options.includes('member') && msg.user.id != this.options.getUser('member').id) ||
-    (options.includes('user_type') && (msg.user.bot && getStr('user_type') == 'human' || !msg.user.bot && getStr('user_type') == 'bot')) ||
-    (options.includes('only_containing') && filterCheck(msg, getStr('only_containing'))) ||
-    (options.includes('caps_percentage') && (msg.content.replace(/[a-z]/g, '').length / msg.content.length * 100 < this.options.getNumber('caps_percentage'))) ||
-    (options.includes('contains') && (!msg.content.includes(getStr('contains')) && !msg.embeds?.find(e => e.description.includes(getStr('contains'))))) ||
-    (options.includes('does_not_contain') && (msg.content.includes(getStr('does_not_contain')) || msg.embeds?.find(e => e.description.includes(getStr('does_not_contain'))))) ||
-    (options.includes('starts_with') && (!msg.content.statsWith(getStr('starts_with')) && !msg.embeds?.find(e => e.description.startsWith(getStr('starts_with'))))) ||
-    (options.includes('not_starts_with') && (msg.content.statsWith(getStr('not_starts_with')) || msg.embeds?.find(e => e.description.startsWith(getStr('not_starts_with'))))) ||
-    (options.includes('ends_with') && (!msg.content.endsWith(getStr('ends_with')) && !msg.embeds?.find(e => e.description.endsWith(getStr('ends_with'))))) ||
-    (options.includes('not_ends_with') && (msg.content.endsWith(getStr('not_ends_with')) || msg.embeds?.find(e => e.description.endsWith(getStr('not_ends_with')))))
-  ) return;
-  return this.options.getBoolean('remove_pinned') || !msg.pinned;
+  const options = this.options?.data.map(e => e.name);
+
+  return !!(msg.bulkDeleteable &&
+    !(msg.system && Constants.ThreadChannelTypes.includes(msg.channel.type)) && //This is to prevent "DiscordAPIError[50021]: Cannot execute action on a system message". discord.js currently returns true on bulkDeleteable and deleteable for thread system messages.
+    !(options?.includes('member') && msg.user.id != this.options.getUser('member').id) &&
+    !(options?.includes('user_type') && (!msg.user.bot && getStr('user_type') == 'human' || msg.user.bot && getStr('user_type') == 'bot')) &&
+    !(options?.includes('only_containing') && filterCheck[getStr('only_containing')](msg)) &&
+    !(options?.includes('caps_percentage') && (msg.content.replace(/[a-z]/g, '').length / msg.content.length * 100 < this.options.getNumber('caps_percentage'))) &&
+    !(options?.includes('contains') && (!msg.content.includes(getStr('contains')) && !msg.embeds?.find(e => e.description.includes(getStr('contains'))))) &&
+    !(options?.includes('does_not_contain') && (msg.content.includes(getStr('does_not_contain')) || msg.embeds?.find(e => e.description.includes(getStr('does_not_contain'))))) &&
+    !(options?.includes('starts_with') && (!msg.content.statsWith(getStr('starts_with')) && !msg.embeds?.find(e => e.description.startsWith(getStr('starts_with'))))) &&
+    !(options?.includes('not_starts_with') && (msg.content.statsWith(getStr('not_starts_with')) || msg.embeds?.find(e => e.description.startsWith(getStr('not_starts_with'))))) &&
+    !(options?.includes('ends_with') && (!msg.content.endsWith(getStr('ends_with')) && !msg.embeds?.find(e => e.description.endsWith(getStr('ends_with'))))) &&
+    !(options?.includes('not_ends_with') && (msg.content.endsWith(getStr('not_ends_with')) || msg.embeds?.find(e => e.description.endsWith(getStr('not_ends_with'))))) &&
+    !(this.options.getBoolean('remove_pinned') || !msg.pinned));
 }
 
 module.exports = {
@@ -95,16 +92,19 @@ module.exports = {
       if (getStr('contains')?.includes(getStr('does_not_contain')) || getStr('does_not_contains')?.includes(getStr('contains')) || notNullEqual(getStr('starts_with'), getStr('not_starts_with')) || notNullEqual(getStr('ends_with'), getStr('not_ends_with')))
         return this.editReply(lang('paramExcludesOther'));
 
-      messages = [...(await channel.messages.fetch({ limit: amount, before: getStr('before_message'), after: getStr('after_message') }))?.filter(e => checkMsg.call(this, e, getStr)).keys() || []];
-
-      if (!messages.length) return this.editReply(lang('noneFound'));
+      messages = await channel.messages.fetch({ limit: amount, before: getStr('before_message'), after: getStr('after_message') });
     }
 
-    for (let i = 0; i < (messages?.length ?? amount); i += 100) {
-      deleted += (await channel.bulkDelete(messages?.slice(i, i + 100) ?? (amount - i).limit({ max: 100 }), true))?.size ?? 0;
-      if (messages?.[i + 1] ?? amount - i > 0) await sleep(2000);
+    if (!messages?.size && amount) messages = await this.channel.messages.fetch({ limit: amount });
+
+    messages = [...(messages.filter(e => checkMsg.call(this, e, getStr)).keys() || [])];
+    if (!messages?.length) return this.customReply(lang('noneFound'));
+
+    for (let i = 0; i < messages.length; i += 100) {
+      deleted += (await channel.bulkDelete(messages.slice(i, i + 100).limit({ max: 100 })))?.size ?? 0;
+      if (messages[i + 1]) await sleep(2000);
     }
 
-    if (this instanceof CommandInteraction) this.editReply(lang('success', { count: deleted, all: messages?.length ?? amount }));
+    if (this instanceof CommandInteraction) this.editReply(lang('success', { count: deleted, all: messages.length }));
   }
 };
