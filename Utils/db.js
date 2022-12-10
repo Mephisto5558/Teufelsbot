8fc8d158d0b84f3e128ef06fd2294c575af277d4
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-12-10 16:24:43 +0100

    small code smell fixes

--- Utils/db.js
+++ Utils/db.js
@@ -90,10 +90,9 @@ module.exports = class DB {
     this.collection.delete(key);
   }
 
-  /**@param {{}}obj gets mutated! @param {string}key@example DB.mergeWithFlat({a: {b:1}}, 'a.c', 2):{a: {b:1, c:2}}*/
+  /**@param {{}}obj gets mutated! @param {string}key@example DB.mergeWithFlat({a: {b:1}}, 'a.c', 2) @returns reduce return value */
   static mergeWithFlat(obj, key, val) {
     const keys = key.split('.');
-    keys.reduce((acc, e, i) => acc[e] = keys.length - 1 == i ? val : acc[e] ?? {}, obj);
-    return obj;
+    return keys.reduce((acc, e, i) => acc[e] = keys.length - 1 == i ? val : acc[e] ?? {}, obj);
   }
 };
