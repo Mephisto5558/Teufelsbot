745a7db5c54a96f77259e0859e7e90af458763af
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-12-14 13:16:52 +0100

    fixed {s} Seconds

--- Utils/uptime.js
+++ Utils/uptime.js
@@ -1,21 +1,21 @@
 module.exports = (asMessage, lang) => {
   let data;
   
-  const s = process.uptime();
-  const d = Math.floor(s / (60 * 60 * 24));
-  const h = Math.floor((s / (60 * 60)) % 24);
-  const m = Math.floor((s / 60) % 60);
-  const sec = Math.floor(s % 60);
+  const up = process.uptime();
+  const d = Math.floor(up / (60 * 60 * 24));
+  const h = Math.floor((up / (60 * 60)) % 24);
+  const m = Math.floor((up / 60) % 60);
+  const s = Math.floor(up % 60);
   
   if (asMessage && lang) {
-    if (d > 0) data = lang('dhms', { d, h, m, sec });
-    else if (h > 0) data = lang('hms', { h, m, sec });
-    else if (m > 0) data = lang('ms', { m, sec });
-    else data = lang('s', sec);
+    if (d > 0) data = lang('dhms', { d, h, m, s });
+    else if (h > 0) data = lang('hms', { h, m, s });
+    else if (m > 0) data = lang('ms', { m, s });
+    else data = lang('up', s);
   }
   
   return {
     total: process.uptime() * 1000,
-    formatted: data || `${d.toString().padStart(2, 0)}:${h.toString().padStart(2, 0)}:${m.toString().padStart(2, 0)}:${sec.toString().padStart(2, 0)}`
+    formatted: data || `${d.toString().padStart(2, 0)}:${h.toString().padStart(2, 0)}:${m.toString().padStart(2, 0)}:${s.toString().padStart(2, 0)}`
   };
 };
