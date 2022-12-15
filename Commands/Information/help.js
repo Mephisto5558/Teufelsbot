b2ce410ef6102105f3e1b2d83c1d747bc5d7a7a2
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2022-12-15 10:48:18 +0100

    fixed alias displaying

--- Commands/Information/help.js
+++ Commands/Information/help.js
@@ -47,8 +47,8 @@ module.exports = {
         embed.data.description = helpLang('description') ?? lang('one.noDescription');
         if (helpLang('usage')) embed.data.footer = { text: lang('one.embedFooterText', this.guild.db.config?.prefix || this.guild.defaultSettings.config.prefix) };
         embed.data.fields = [
-          cmd.aliases?.prefix?.length && { name: lang('one.prefixAlias'), value: `\`${listCommands(cmd.aliases.prefix, '', 1)[0].replaceAll('> ', '')}\``, inline: true },
-          cmd.aliases?.slash?.length && { name: lang('one.slashAlias'), value: `\`${listCommands(cmd.aliases.slash, '', 1)[0].replaceAll('> ', '')}\``, inline: true },
+          cmd.aliases?.prefix?.length && { name: lang('one.prefixAlias'), value: `\`${cmd.aliases.prefix.join('`, `')}\``, inline: true },
+          cmd.aliases?.slash?.length && { name: lang('one.slashAlias'), value: `\`${cmd.aliases.slash.join('`, `')}\``, inline: true },
           cmd.permissions?.client?.length && { name: lang('one.botPerms'), value: `\`${permissionTranslator(cmd.permissions.client).join('`, `')}\``, inline: false },
           cmd.permissions?.user?.length && { name: lang('one.userPerms'), value: `\`${permissionTranslator(cmd.permissions.user).join('`, `')}\``, inline: true },
           (cmd.cooldowns?.user || cmd.cooldowns?.guild) && {
