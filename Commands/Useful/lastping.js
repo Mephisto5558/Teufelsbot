f93b1c0f9c2f1a38320cfe770e20dc3cf569ae07
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-01-09 14:33:06 +0100

    added member option, fixed an issue with storing

--- Commands/Useful/lastping.js
+++ Commands/Useful/lastping.js
@@ -11,9 +11,9 @@ module.exports = {
       name: 'channel',
       type: 'Channel',
       channelTypes: Constants.TextBasedChannelTypes
-    }/*,
-    { name: 'member', type: 'User' },
-    {
+    },
+    { name: 'member', type: 'User' }
+    /*{
       name: 'amount',
       type: 'Number',
       minValue: 0,
@@ -24,20 +24,24 @@ module.exports = {
   run: async function (lang) {
     const
       channel = this.options?.getChannel('channel') || this.mentions?.channels.first(),
-      target = (this.options?.getUser('member') || this.mentions?.users.first() || this.user).id,
-      mentionsCache = this.guild.mentionsCache.get(target);
+      target = this.options?.getUser('member') || this.mentions?.users.first();
 
-    if (channel && !channel.isTextBased()) return this.customReply(lang('invalid'));
-    if (!mentionsCache?.size) return this.customReply(lang('noneFound'));
+    if (target) {
+      if (!channel) return this.customReply(lang('memberRequiresChannel'));
+      if (!channel.isTextBased()) return this.customReply(lang('invalidChannel'));
+    }
 
-    const { url, content, author, createdAt } = (channel ? mentionsCache.filter(e => e.channel.id == channel.id) : mentionsCache)?.last() || {};
+    const { url, content, author, createdAt } = (channel ? channel.messages.cache.find(e =>
+      (!target || e.author.id == target.id) && e.mentions.everyone || e.mentions.users.has(this.user.id) || e.mentions.roles.hasAny(this.member.roles.cache.keys())
+    ) : this.guild.db.lastMentions?.[this.user.id]) || {};
+
     if (!url) return this.customReply(lang('noneFound'));
 
     const embed = new EmbedBuilder({
       title: lang('embedTitle'),
-      description: lang('embedDescription', { url, content: content?.substring(0, 200) ?? lang('unknown'), author }),
+      description: lang('embedDescription', { url, content: content?.substring(0, 200) ?? lang('unknown'), author: author.id || author }),
       color: Colors.White,
-      footer: { text: createdAt?.toLocaleString(this.guild.preferredLocale, { month: '2-digit', day: '2-digit' }) }
+      footer: { text: createdAt?.toLocaleTimeString('de', { month: '2-digit', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' }) }
     });
 
     this.customReply({ embeds: [embed] });
