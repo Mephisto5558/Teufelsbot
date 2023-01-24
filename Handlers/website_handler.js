14c4895f51623ad5bb11382a83795033883c0120
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-01-24 17:08:42 +0100

    I have no Idea how tf to get this to work

--- Handlers/website_handler.js
+++ Handlers/website_handler.js
@@ -67,7 +67,7 @@ module.exports = function websiteHandler() {
           this.db.fetch(req.query.db);
           return res.sendStatus(200);
         }
-        case '/git/pull': return res.body.ref == 'refs/heads/main' ? res.send(await gitpull()): res.sendStatus(200);
+        case '/git/pull': return res.send(await gitpull());
         case '/': return res.sendStatus(200);
         default: res.sendStatus(404);
       }
