cbf8c470a046344b63a8f5a26b4a64b17b13ecc9
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-01-31 11:52:33 +0100

    removed duplicate code, fixed some issues

--- Utils/permissionTranslator.js
+++ Utils/permissionTranslator.js
@@ -3,6 +3,6 @@ const I18nProvider = require('./I18nProvider.js');
 /**@param {string|string[]}perms*/
 module.exports = (perms, locale) => {
   if (!perms?.length) return perms;
-  if (typeof perms == 'string') return I18nProvider.__({ locale, undefinedNotFound: true }`others.${perms}`) || perms;
+  if (typeof perms == 'string') return I18nProvider.__({ locale, undefinedNotFound: true }, `others.Perms.${perms}`) || perms;
   return perms.map(perm => I18nProvider.__({ locale, undefinedNotFound: true }, `others.Perms.${perm}`) || perm);
 };
