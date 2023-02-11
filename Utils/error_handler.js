fa1f18eb749f45a8ef137522905dc0a81800a127
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-02-11 14:28:19 +0100

    remove the button after reporting an error

--- Utils/error_handler.js
+++ Utils/error_handler.js
@@ -33,37 +33,39 @@ module.exports = async function errorHandler(err, message, lang) {
   if (this.botType == 'dev') return;
 
   const collector = msg.createMessageComponentCollector({ filter: i => i.customId == 'reportError', max: 1, componentType: ComponentType.Button, time: 6e4 });
-  collector.on('collect', async button => {
-    await button.deferUpdate();
-    collector.stop();
+  collector
+    .on('collect', async button => {
+      await button.deferUpdate();
+      collector.stop();
 
-    const title = `${err.name}: "${err.message}" in command "${message.commandName}"`;
+      const title = `${err.name}: "${err.message}" in command "${message.commandName}"`;
 
-    try {
-      const issues = await octokit.request(`GET /repos/${Github.UserName}/${Github.RepoName}/issues`, {});
+      try {
+        const issues = await octokit.request(`GET /repos/${Github.UserName}/${Github.RepoName}/issues`, {});
 
-      if (issues.data.filter(e => e.title == title && e.state == 'open').length) {
-        embed.data.description = lang('events.errorHandler.alreadyReported', issues.data[0].html_url);
-        return msg.edit({ embeds: [embed], components: [comp] });
+        if (issues.data.filter(e => e.title == title && e.state == 'open').length) {
+          embed.data.description = lang('events.errorHandler.alreadyReported', issues.data[0].html_url);
+          return msg.edit({ embeds: [embed], components: [] });
+        }
+
+        await octokit.request(`POST /repos/${Github.UserName}/${Github.RepoName}/issues`, {
+          title, body: `<h3>Reported by ${button.user.tag} (${button.user.id}) with bot ${button.client.user.id}</h3>\n\n${err.stack}`,
+          assignees: [Github.UserName],
+          labels: ['bug']
+        });
+
+        embed.data.description = lang('events.errorHandler.reportSuccess', encodeURI(`${Github.Repo}/issues?q=is:open+is:issue+${title} in:title`));
+        msg.edit({ embeds: [embed], components: [] });
       }
+      catch (err) {
+        message.customReply(lang('events.errorHandler.reportFail', err?.response.statusText || 'unknown error'));
+        this.error(err.stack);
+      }
+    })
+    .on('end', collected => {
+      if (collected.size) return;
 
-      await octokit.request(`POST /repos/${Github.UserName}/${Github.RepoName}/issues`, {
-        title, body: `<h3>Reported by ${button.user.tag} (${button.user.id}) with bot ${button.client.user.id}</h3>\n\n${err.stack}`,
-        assignees: [Github.UserName],
-        labels: ['bug']
-      });
-
-      embed.data.description = lang('events.errorHandler.reportSuccess', encodeURI(`${Github.Repo}/issues?q=is:open+is:issue+${title} in:title`));
+      comp.components[0].data.disabled = true;
       msg.edit({ embeds: [embed], components: [comp] });
-    }
-    catch (err) {
-      message.customReply(lang('events.errorHandler.reportFail', err?.response.statusText || 'unknown error'));
-      this.error(err.stack);
-    }
-  });
-
-  collector.on('end', () => {
-    comp.components[0].setDisabled(true);
-    msg.edit({ embeds: [embed], components: [comp] });
-  });
+    });
 };
