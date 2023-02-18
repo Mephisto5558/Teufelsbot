6c84a6e56a085d4ec0c9cf815914895f50cf9fab
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-02-18 15:31:33 +0100

    added options to reload by path, disabled commands now show up in client.prefixCommands and client.slashCommands

--- Handlers/command_handler.js
+++ Handlers/command_handler.js
@@ -1,8 +1,9 @@
 const
   { readdirSync } = require('fs'),
-  { resolve } = require('path');
+  { resolve } = require('path'),
+  { HideDisabledCommandLog, HideNonBetaCommandLog } = require('../config.json');
 
-let commandCount = 0;
+let enabledCommandCount = 0, disabledCommandCount = 0;
 
 module.exports = function commandHandler() {
   for (const subFolder of getDirectoriesSync('./Commands')) {
@@ -12,21 +13,27 @@ module.exports = function commandHandler() {
         ...(require(`../Commands/${subFolder}/${e}`) || {}),
         filePath: resolve(`Commands/${subFolder}/${e}`),
         category: subFolder
-      })).filter(e => e.prefixCommand && !e.disabled && !(this.botType == 'dev' && !e.beta))
+      }))
+      .filter(e => e.prefixCommand)
     ) {
-      if (!command.run?.toString().startsWith('function') && !command.run?.toString().startsWith('async function')) throw new Error(`The run function of file "${command.filePath}" is not a function. You cannot use arrow functions.`);
+      if (!command.disabled && !command.run?.toString().startsWith('function') && !command.run?.toString().startsWith('async function')) throw new Error(`The run function of file "${command.filePath}" is not a function. You cannot use arrow functions.`);
 
       this.prefixCommands.set(command.name, command);
-      this.log(`Loaded Prefix Command ${command.name}`);
-      commandCount++;
+      if (command.disabled) HideDisabledCommandLog ? void 0 : this.log(`Loaded Disabled Prefix Command ${command.name}`);
+      else if (!command.beta && this.botType == 'dev') HideNonBetaCommandLog ? void 0 : this.log(`Loaded Non-Beta Prefix Command ${command.name}`);
+      else this.log(`Loaded Prefix Command ${command.name}`);
+      command.disabled ? disabledCommandCount++ : enabledCommandCount++;
 
       for (const alias of command.aliases?.prefix || []) {
         this.prefixCommands.set(alias, { ...command, aliasOf: command.name });
-        this.log(`Loaded Alias ${alias} of Prefix Command ${command.name}`);
-        commandCount++;
+        if (command.disabled) HideDisabledCommandLog ? void 0 : this.log(`Loaded Alias ${alias} of Prefix Command ${command.name} (disabled)`);
+        else this.log(`Loaded Alias ${alias} of Prefix Command ${command.name}`);
+        command.disabled ? disabledCommandCount++ : enabledCommandCount++;
       }
     }
   }
 
-  this.log(`Loaded ${commandCount} Prefix Commands\n`);
+  this.log(`Loaded ${enabledCommandCount} Enabled Prefix Commands`);
+  if (!HideDisabledCommandLog) this.log(`Loaded ${disabledCommandCount} Disabled Prefix Commands`);
+  console.log();
 };
--- Handlers/slash_command_handler.js
+++ Handlers/slash_command_handler.js
@@ -2,9 +2,8 @@ const
   { Collection, ApplicationCommandType, ApplicationCommandOptionType, PermissionsBitField, ChannelType } = require('discord.js'),
   { readdirSync } = require('fs'),
   { resolve } = require('path'),
-  { I18nProvider } = require('../Utils');
-
-let deletedCommandCount = 0;
+  { I18nProvider } = require('../Utils'),
+  { HideNonBetaCommandLog, HideDisabledCommandLog } = require('../config.json');
 
 function equal(a, b) {
   if (!a?.toString() && !b?.toString()) return true;
@@ -31,17 +30,17 @@ function format(option, path) {
   if (option.autocompleteOptions) option.autocomplete = true;
 
   if (option.description.length > 100) {
-    console.warn(`WARN: Description of option "${option.name}" (${path}.description) is too long (max length is 100)! Slicing.`);
+    if (!option.disabled) console.warn(`WARN: Description of option "${option.name}" (${path}.description) is too long (max length is 100)! Slicing.`);
     option.description = option.description.substring(0, 100);
   }
 
   for (const [locale] of [...I18nProvider.availableLocales].filter(([e]) => e != I18nProvider.config.defaultLocale)) {
     if (!option.descriptionLocalizations) option.descriptionLocalizations = {};
     let localeText = I18nProvider.__({ locale, undefinedNotFound: true }, `${path}.description`);
-    if (localeText?.length > 100) console.warn(`WARN: "${locale}" description localization of option "${option.name}" (${path}.description) is too long (max length is 100)! Slicing.`);
+    if (localeText?.length > 100 && !option.disabled) console.warn(`WARN: "${locale}" description localization of option "${option.name}" (${path}.description) is too long (max length is 100)! Slicing.`);
 
     if (localeText) option.descriptionLocalizations[locale] = localeText?.slice(0, 100);
-    else console.warn(`WARN: Missing "${locale}" description localization for option "${option.name}" (${path}.description)`);
+    else if (!option.disabled) console.warn(`WARN: Missing "${locale}" description localization for option "${option.name}" (${path}.description)`);
 
     if (option.choices?.length) option.choices.map(e => {
       if (e.__SCHandlerCustom) {
@@ -51,21 +50,21 @@ function format(option, path) {
 
       if (!e.nameLocalizations) e.nameLocalizations = {};
       let localeText = I18nProvider.__({ locale, undefinedNotFound: true }, `${path}.choices.${e.value}`);
-      if (localeText?.length < 2) console.warn(`WARN: Choice name localization ("${e.name}") "${locale}" of option "${option.name}" (${path}.choices.${e.name}) is too short (min length is 2)! Using undefined.`);
-      else if (localeText?.length > 32) console.warn(`WARN: Choice name localization ("${e.name}") "${locale}" of option "${option.name}" (${path}.choices.${e.name}) is too long (max length is 32)! Slicing.`);
+      if (localeText?.length < 2) option.disabled ? void 0 : console.warn(`WARN: Choice name localization ("${e.name}") "${locale}" of option "${option.name}" (${path}.choices.${e.name}) is too short (min length is 2)! Using undefined.`);
+      else if (localeText?.length > 32) option.disabled ? void 0 : console.warn(`WARN: Choice name localization ("${e.name}") "${locale}" of option "${option.name}" (${path}.choices.${e.name}) is too long (max length is 32)! Slicing.`);
 
       if (localeText && localeText.length > 2) e.nameLocalizations[locale] = localeText.slice(0, 32);
-      else if (e.name != e.value) console.warn(`WARN: Missing "${locale}" choice name localization for "${e.name}" in option "${option.name}" (${path}.choices.${e.name})`);
+      else if (e.name != e.value) option.disabled ? void 0 : console.warn(`WARN: Missing "${locale}" choice name localization for "${e.name}" in option "${option.name}" (${path}.choices.${e.name})`);
 
       return e;
     });
   }
 
   if (option.run) {
-    if (!option.run.toString().startsWith('function') && !option.run.toString().startsWith('async function')) throw new Error(`The run function of file "${path}" is not a function. You cannot use arrow functions.`);
+    if (!option.disabled && !option.run.toString().startsWith('function') && !option.run.toString().startsWith('async function')) throw new Error(`The run function of file "${path}" is not a function. You cannot use arrow functions.`);
 
     if (!option.type) option.type = ApplicationCommandType.ChatInput;
-    else if (!ApplicationCommandType[option.type]) throw new Error(`Invalid option.type, got "${option.type}" (${path})`);
+    else if (!ApplicationCommandType[option.type]) { if (!option.disabled) throw new Error(`Invalid option.type, got "${option.type}" (${path})`); }
     else if (isNaN(option.type)) option.type = ApplicationCommandType[option.type];
 
     if (!option.usage) option.usage = I18nProvider.__({ undefinedNotFound: true }, `${path}.usage`);
@@ -76,21 +75,21 @@ function format(option, path) {
   }
 
   if (/[A-Z]/.test(option.name)) {
-    console.error(`${option.name} (${path}) has uppercase letters! Fixing`);
+    if (!option.disabled) console.error(`${option.name} (${path}) has uppercase letters! Fixing`);
     option.name = option.name.toLowerCase();
   }
 
   if (option.channelTypes) option.channelTypes = option.channelTypes?.map(e => {
-    if (!ChannelType[e] && ChannelType[e] != 0) throw Error(`Invalid option.channelType, got "${e}" (${path})`);
+    if (!ChannelType[e] && ChannelType[e] != 0 && !option.disabled) throw Error(`Invalid option.channelType, got "${e}" (${path})`);
     return isNaN(e) ? ChannelType[e] : e;
   });
 
-  if (!option.type || !ApplicationCommandOptionType[option.type]) throw Error(`Missing or invalid option.type, got "${option.type}" (${path})`);
+  if (!option.type || !ApplicationCommandOptionType[option.type] && !option.disabled) throw Error(`Missing or invalid option.type, got "${option.type}" (${path})`);
   if (isNaN(option.type)) option.type = ApplicationCommandOptionType[option.type];
 
-  if ([ApplicationCommandOptionType.Number, ApplicationCommandOptionType.Integer].includes(option.type) && ('minLength' in option || 'maxLength' in option))
+  if ([ApplicationCommandOptionType.Number, ApplicationCommandOptionType.Integer].includes(option.type) && ('minLength' in option || 'maxLength' in option) && !option.disabled)
     throw new Error(`Number and Integer options do not support "minLength" and "maxLength" (${path})`);
-  if (option.type == ApplicationCommandOptionType.String && ('minValue' in option || 'maxValue' in option))
+  if (option.type == ApplicationCommandOptionType.String && ('minValue' in option || 'maxValue' in option) && !option.disabled)
     throw new Error(`String options do not support "minValue" and "maxValue" (${path})`);
 
   return option;
@@ -98,6 +97,8 @@ function format(option, path) {
 
 module.exports = async function slashCommandHandler(syncGuild) {
   await this.awaitReady();
+
+  let deletedCommandCount = 0, registeredCommandCount = 0;
 
   const skippedCommands = new Collection();
   const applicationCommands = this.application.commands.fetch(undefined, { guildId: syncGuild && syncGuild != '*' ? syncGuild : undefined });
@@ -110,14 +111,14 @@ module.exports = async function slashCommandHandler(syncGuild) {
         let command = require(`../Commands/${subFolder}/${file}`);
         let skipped = false;
 
-        if (!command.slashCommand || command.disabled || (this.botType == 'dev' && !command.beta)) continue;
+        if (!command.slashCommand) continue;
 
         command = format(command, `commands.${subFolder.toLowerCase()}.${file.slice(0, -3)}`);
         command.filePath = resolve(`Commands/${subFolder}/${file}`);
         command.category = subFolder;
 
         for (const [, applicationCommand] of await applicationCommands) {
-          if (syncGuild || !equal(command, applicationCommand)) continue;
+          if (!syncGuild && (command.disabled || !equal(command, applicationCommand))) continue;
           this.log(`Skipped Slash Command ${command.name}`);
           skipped = true;
           skippedCommands.set(command.name, command);
@@ -132,8 +133,13 @@ module.exports = async function slashCommandHandler(syncGuild) {
   }
 
   for (const [commandName, command] of this.slashCommands) {
-    await this.application.commands.create(command, syncGuild && syncGuild != '*' ? syncGuild : null);
-    this.log(`Registered Slash Comand ${commandName}`);
+    if (command.disabled) HideDisabledCommandLog ? void 0 : this.log(`Skipped Disabled Slash Command ${commandName}`);
+    else if (this.botType == 'dev' && !command.beta) HideNonBetaCommandLog ? void 0 : this.log(`Skipped Non-Beta Slash Command ${commandName}`);
+    else {
+      await this.application.commands.create(command, syncGuild && syncGuild != '*' ? syncGuild : null);
+      this.log(`Registered Slash Command ${commandName}`);
+      registeredCommandCount++;
+    }
   }
 
   const commandNames = [...this.slashCommands, ...skippedCommands].map(e => e[0]);
@@ -141,13 +147,13 @@ module.exports = async function slashCommandHandler(syncGuild) {
     if (commandNames.includes(command.name)) continue;
 
     await this.application.commands.delete(command, syncGuild && syncGuild != '*' ? syncGuild : null);
-    this.log(`Deleted Slash Comand ${command.name}`);
+    this.log(`Deleted Slash Command ${command.name}`);
     deletedCommandCount++;
   }
 
   if (syncGuild) return;
 
-  this.log(`Registered ${this.slashCommands.size} Slash Commands`);
+  this.log(`Registered ${registeredCommandCount} Slash Commands`);
 
   for (const skippedCommand of skippedCommands) this.slashCommands.set(...skippedCommand);
 
