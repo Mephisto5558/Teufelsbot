9122f9a64b38eab71913981eafd0abb6483edb72
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-02-19 18:20:34 +0100

    plz be fixed now

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -21,11 +21,11 @@ async function autocompleteHandler(command, locale) {
   options = options.find(e => e.name == this.focused.name).autocompleteOptions;
   if (typeof options == 'function') options = await options.call(this);
 
-  if (options.constructor.name == 'Object') return this.respond(options);
+  if (options.constructor.name == 'Object') return this.respond([options]);
   return this.respond(
     typeof options == 'string' ? [response(options)] : options
       .filter(e => !this.focused.value || e.toLowerCase().includes(this.focused.value.toLowerCase()))
-      .slice(0, 25).map(response)
+      .slice(0, 25).map(e => typeof e == 'object' ? e : response(e))
   );
 }
 
