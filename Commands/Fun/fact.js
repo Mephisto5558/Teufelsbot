f802eb0dd107b91efe8889407d0b4250856ad3a2
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-03-15 09:48:07 +0100

    added advice command, updated fact command (using different api now)

--- Commands/Fun/fact.js
+++ Commands/Fun/fact.js
@@ -1,15 +1,23 @@
-const fetch = require('node-fetch').default;
+const
+  { EmbedBuilder } = require('discord.js'),
+  fetch = require('node-fetch').default;
 
 module.exports = {
   name: 'fact',
   cooldowns: { guild: 100 },
   slashCommand: true,
   prefixCommand: true,
-  dmPermission: true,
+  dmPermission: true, beta: true,
 
-  run: async function() {
-    const data = await fetch('https://api.api-ninjas.com/v1/facts', { headers: { 'X-Api-Key': this.client.keys.FunFactAPI } }).then(e => e.json());
+  run: async function (lang) {
+    const
+      data = await fetch(`https://uselessfacts.jsph.pl/api/v2/facts/random?language=${lang.__boundArgs__[0].locale}`).then(e => e.json()),
+      embed = new EmbedBuilder({
+        title: lang('embedTitle'),
+        description: `${data.text}\n\nSource: [${data.source}](${data.source_url})`,
+        footer: { text: '- https://uselessfacts.jsph.pl' }
+      }).setColor('Random');
 
-    return this.customReply(`${data[0].fact}.`);
+    return this.customReply({ embeds: [embed] });
   }
 };
