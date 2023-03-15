12442a8a8c52ab914dc732c75c7620ff99f03028
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-03-15 08:13:08 +0100

    using the original command instead of its alias if it is one

--- Commands/Information/help.js
+++ Commands/Information/help.js
@@ -23,37 +23,38 @@ module.exports = {
       query = (this.args?.[0] || this.options?.getString('command'))?.toLowerCase();
 
     if (query) {
-      const cmd = this.client.prefixCommands.get(query) || this.client.slashCommands.get(query);
+      let cmd = this.client.prefixCommands.get(query) || this.client.slashCommands.get(query);
+      if (cmd.aliasOf) cmd = this.client.prefixCommands.get(cmd.aliasOf) || this.client.slashCommands.get(cmd.aliasOf);
 
       if (!cmd?.name || cmd.hideInHelp || cmd.disabled || (ownerOnlyFolders.includes(cmd.category.toUpperCase()) && this.user.id != this.client.application.owner.id)) {
         embed.data.description = lang('one.notFound', query);
         embed.data.color = Colors.Red;
+        return this.customReply({ embeds: [embed] });
       }
-      else {
-        const helpLang = I18nProvider.__.bind(I18nProvider, { undefinedNotFound: true, locale: this.guild.localeCode, backupPath: `commands.${cmd.category.toLowerCase()}.${cmd.name}` });
 
-        embed.data.title = lang('one.embedTitle', cmd.name);
-        embed.data.description = helpLang('description') ?? lang('one.noDescription');
-        if (helpLang('usage')) embed.data.footer = { text: lang('one.embedFooterText', this.guild.db.config?.prefix || this.client.defaultSettings.config.prefix) };
-        embed.data.fields = [
-          cmd.aliases?.prefix?.length && { name: lang('one.prefixAlias'), value: `\`${cmd.aliases.prefix.join('`, `')}\``, inline: true },
-          cmd.aliases?.slash?.length && { name: lang('one.slashAlias'), value: `\`${cmd.aliases.slash.join('`, `')}\``, inline: true },
-          cmd.aliasOf && { name: lang('one.aliasOf'), value: `\`${cmd.aliasOf}\``, inline: true },
-          cmd.permissions?.client?.length && { name: lang('one.botPerms'), value: `\`${permissionTranslator(cmd.permissions.client, lang.__boundArgs__[0].locale).join('`, `')}\``, inline: false },
-          cmd.permissions?.user?.length && { name: lang('one.userPerms'), value: `\`${permissionTranslator(cmd.permissions.user, lang.__boundArgs__[0].locale).join('`, `')}\``, inline: true },
-          (cmd.cooldowns?.user || cmd.cooldowns?.guild) && {
-            name: lang('one.cooldowns'), inline: false,
-            value: Object.entries(cmd.cooldowns).filter(([, e]) => e).map(([k, v]) => {
-              let min = Math.floor(v / 60000), sec = (v % 60000 / 1000);
-              sec = !(sec % 1) ? Math.floor(sec) : sec.toFixed(2);
+      const helpLang = I18nProvider.__.bind(I18nProvider, { undefinedNotFound: true, locale: this.guild.localeCode, backupPath: `commands.${cmd.category.toLowerCase()}.${cmd.name}` });
 
-              if (min && sec) return `${lang('global.' + k)}: ${min}min ${sec}s`;
-              return lang(`global.${k}`) + ': ' + (min ? `${min}min` : `${sec}s`);
-            }).join(', ')
-          },
-          helpLang('usage') && { name: lang('one.usage'), value: `${cmd.slashCommand ? lang('one.lookAtDesc') : ''} ${helpLang('usage') || ''}`, inline: false }
-        ].filter(Boolean);
-      }
+      embed.data.title = lang('one.embedTitle', cmd.name);
+      embed.data.description = helpLang('description') ?? lang('one.noDescription');
+      if (helpLang('usage')) embed.data.footer = { text: lang('one.embedFooterText', this.guild.db.config?.prefix || this.client.defaultSettings.config.prefix) };
+      embed.data.fields = [
+        cmd.aliases?.prefix?.length && { name: lang('one.prefixAlias'), value: `\`${cmd.aliases.prefix.join('`, `')}\``, inline: true },
+        cmd.aliases?.slash?.length && { name: lang('one.slashAlias'), value: `\`${cmd.aliases.slash.join('`, `')}\``, inline: true },
+        cmd.aliasOf && { name: lang('one.aliasOf'), value: `\`${cmd.aliasOf}\``, inline: true },
+        cmd.permissions?.client?.length && { name: lang('one.botPerms'), value: `\`${permissionTranslator(cmd.permissions.client, lang.__boundArgs__[0].locale).join('`, `')}\``, inline: false },
+        cmd.permissions?.user?.length && { name: lang('one.userPerms'), value: `\`${permissionTranslator(cmd.permissions.user, lang.__boundArgs__[0].locale).join('`, `')}\``, inline: true },
+        (cmd.cooldowns?.user || cmd.cooldowns?.guild) && {
+          name: lang('one.cooldowns'), inline: false,
+          value: Object.entries(cmd.cooldowns).filter(([, e]) => e).map(([k, v]) => {
+            let min = Math.floor(v / 60000), sec = (v % 60000 / 1000);
+            sec = !(sec % 1) ? Math.floor(sec) : sec.toFixed(2);
+
+            if (min && sec) return `${lang('global.' + k)}: ${min}min ${sec}s`;
+            return lang(`global.${k}`) + ': ' + (min ? `${min}min` : `${sec}s`);
+          }).join(', ')
+        },
+        helpLang('usage') && { name: lang('one.usage'), value: `${cmd.slashCommand ? lang('one.lookAtDesc') : ''} ${helpLang('usage') || ''}`, inline: false }
+      ].filter(Boolean);
 
       return this.customReply({ embeds: [embed] });
     }
