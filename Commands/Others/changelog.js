8b6e84b415d367682747ecc1c0c6ce3d4af634fc
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-03-21 12:00:30 +0100

    added setchangelog and changelog

---
+++ Commands/Others/changelog.js
@@ -0,0 +1,16 @@
+const
+  { EmbedBuilder, Colors } = require('discord.js'),
+  embed = new EmbedBuilder({ color: Colors.White });
+
+module.exports = {
+  name: 'changelog',
+  aliases: { prefix: ['changelogs'] },
+  cooldowns: { user: 1000 },
+  slashCommand: true,
+  prefixCommand: true,
+  dmPermission: true,
+
+  run: function (lang) {
+    return this.customReply({ embeds: [embed.setTitle(lang('embedTitle')).setDescription(this.client.settings.changelog || lang('noneFound'))] });
+  }
+};
