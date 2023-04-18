4800cec44044e14a82d490178324923dffb49f79
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-04-18 13:36:33 +0200

    small improvements

--- Utils/error_handler.js
+++ Utils/error_handler.js
@@ -11,65 +11,64 @@ module.exports = async function errorHandler(err, message, lang) {
 
   if (!message) return;
 
+  lang.__boundArgs__[0].backupPath = 'events.errorHandler';
+
   const
+    { aliasOf } = this.slashCommands.get(message.commandName) || this.prefixCommands.get(message.commandName) || {},
     embed = new EmbedBuilder({
-      title: lang('events.errorHandler.embedTitle'),
-      description: lang('events.errorHandler.embedDescription', { name: err.name, command: message.commandName }),
-      footer: { text: lang('events.errorHandler.embedFooterText') },
+      title: lang('embedTitle'),
+      description: lang('embedDescription', { name: err.name, command: aliasOf ? this.slashCommands.get(aliasOf)?.name || this.prefixCommands.get(aliasOf)?.name : message.commandName }),
+      footer: { text: lang('embedFooterText') },
       color: Colors.DarkRed
     }),
-    comp = new ActionRowBuilder({
+    component = new ActionRowBuilder({
       components: [new ButtonBuilder({
-        customId: 'reportError',
-        label: lang('events.errorHandler.reportButton') + (this.botType == 'dev' ? lang('events.errorHandler.reportButtonDisabled') : ''),
+        customId: 'errorHandler.reportError',
+        label: lang('reportButton') + (this.botType == 'dev' ? lang('reportButtonDisabled') : ''),
         style: ButtonStyle.Danger,
         disabled: this.botType == 'dev'
       })]
     }),
-    msg = await message.customReply({ embeds: [embed], components: [comp] });
+    msg = await message.customReply({ embeds: [embed], components: [component] });
 
   if (this.botType == 'dev') return;
 
-  const collector = msg.createMessageComponentCollector({
-    filter: i => i.customId == 'reportError',
-    max: 1, componentType: ComponentType.Button, time: 6e4
-  });
-
-  collector
+  msg.createMessageComponentCollector({ max: 1, componentType: ComponentType.Button, time: 6e4 })
     .on('collect', async button => {
       await button.deferUpdate();
 
       try {
-        const res = await fetch(`https://api.github.com/repos/${Github.UserName}/${Github.RepoName}/issues`, {
-          method: 'POST',
-          headers: {
-            Authorization: `Token ${this.keys.githubKey}`,
-            'User-Agent': `Bot ${Github.Repo}`
-          },
-          body: JSON.stringify({
-            title: `${err.name}: "${err.message}" in command "${message.commandName}"`,
-            body: `<h3>Reported by ${button.user.tag} (${button.user.id}) with bot ${button.client.user.id}</h3>\n\n${err.stack}`,
-            labels: ['bug']
-          })
-        });
-        const json = await res.json();
+        const
+          res = await fetch(`https://api.github.com/repos/${Github.UserName}/${Github.RepoName}/issues`, {
+            method: 'POST',
+            headers: {
+              Authorization: `Token ${this.keys.githubKey}`,
+              'User-Agent': `Bot ${Github.Repo}`
+            },
+            body: JSON.stringify({
+              title: `${err.name}: "${err.message}" in command "${message.commandName}"`,
+              body: `<h3>Reported by ${button.user.tag} (${button.user.id}) with bot ${button.client.user.id}</h3>\n\n${err.stack}`,
+              labels: ['bug']
+            })
+          }),
+          json = await res.json();
 
         if (!res.ok) throw new Error(json);
 
         const attachment = new AttachmentBuilder(Buffer.from(JSON.stringify({ ...message }, (_, v) => typeof v == 'bigint' ? v.toString() : v, 2)), { name: 'data.json' });
         try { (this.application.owner.owner || this.application.owner).send({ content: json.html_url, files: [attachment] }); } catch { }
 
-        return msg.edit({ embeds: [embed.setFooter(null).setDescription(lang('events.errorHandler.reportSuccess', json.html_url))], components: [] });
+        return msg.edit({ embeds: [embed.setFooter(null).setDescription(lang('reportSuccess', json.html_url))], components: [] });
       }
       catch (err) {
         this.error(err.stack);
-        return message.customReply(lang('events.errorHandler.reportFail', err?.message || 'unknown error'));
+        return message.customReply(lang('reportFail', err?.message || 'unknown error'));
       }
     })
     .on('end', collected => {
       if (collected.size) return;
 
-      comp.components[0].data.disabled = true;
-      return msg.edit({ embeds: [embed], components: [comp] });
+      component.components[0].data.disabled = true;
+      return msg.edit({ embeds: [embed], components: [component] });
     });
 };
