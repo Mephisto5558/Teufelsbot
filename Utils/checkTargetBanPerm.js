9bc5420c84b604db2d4552fdd387545347ccc43f
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-05-03 09:05:50 +0200

    fixed running lang on lang str (lang(lang('txt'))

--- Utils/checkTargetBanPerm.js
+++ Utils/checkTargetBanPerm.js
@@ -1,3 +1,4 @@
+/**@param {import('discord.js').GuildMember}member @returns {string|undefined} translated error message or undefined if no error happend*/
 module.exports = function checkTarget(member, lang) {
   if (member.id == this.member.id) return lang('cantPunishSelf');
   if (member.roles?.highest.comparePositionTo(this.member.roles.highest) > -1 && this.guild.ownerId != this.user.id) return lang('global.noPermUser');
