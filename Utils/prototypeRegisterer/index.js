05b8fdc891c687c3440879fec77824252802439b
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-05-20 12:15:37 +0200

    removed the economy module (fixes #228

--- Utils/prototypeRegisterer/index.js
+++ Utils/prototypeRegisterer/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   _patch: require('./message__patch.js'),
   customReply: require('./message_customReply.js'),
-  runEco: require('./message_runEco.js'),
   runMessages: require('./message_runMessages.js'),
   playAgain: require('./TicTacToe_playAgain.js')
 };
