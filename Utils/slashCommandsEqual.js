7b79b0086e92a0057a3080bce5817e0d864425e4
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-05-20 13:44:44 +0200

    fixed check for changed choices

--- Utils/slashCommandsEqual.js
+++ Utils/slashCommandsEqual.js
@@ -4,12 +4,13 @@ module.exports = function equal(a, b) {
   if (
     !!a != !!b || a.name != b.name || a.description != b.description || a.type != b.type || a.autocomplete != b.autocomplete || a.dmPermission != b.dmPermission ||
     a.value != b.value || (a.options?.length ?? 0) != (b.options?.length ?? 0) || (a.channelTypes?.length ?? 0) != (b.channelTypes?.length ?? 0) ||
-    a.minValue != b.minValue || a.maxValue != b.maxValue || a.minLength != b.minLength || a.maxLength != b.maxLength || !!a.required != !!b.required ||
-    !equal(a.choices, b.choices) || a.defaultMemberPermissions?.bitfield != b.defaultMemberPermissions?.bitfield ||
+    (a.choices?.length ?? 0) != (b.choices?.length ?? 0) || a.minValue != b.minValue || a.maxValue != b.maxValue || a.minLength != b.minLength ||
+    a.maxLength != b.maxLength || !!a.required != !!b.required || a.defaultMemberPermissions?.bitfield != b.defaultMemberPermissions?.bitfield ||
     !equal(a.nameLocalizations, b.nameLocalizations) || !equal(a.description_localizations, b.description_localizations)
   ) return false;
 
-  for (let i = 0; i < (a.options?.length || 0); i++) if (!equal(a.options?.[i], b?.options?.[i])) return false;
+  for (let i = 0; i < (a.choices?.length || 0); i++) if (!equal(a.choices?.[i], b.choices?.[i])) return false;
+  for (let i = 0; i < (a.options?.length || 0); i++) if (!equal(a.options?.[i], b.options?.[i])) return false;
   for (const channelType of (a.channelTypes || [])) if (!b.channelTypes.includes(channelType)) return false;
 
   return true;
