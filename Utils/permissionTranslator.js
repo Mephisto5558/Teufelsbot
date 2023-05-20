214b899de5a2bc85011cd68bfa5c1427cf432b11
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-05-20 12:37:14 +0200

    added return info, returning empty array on empty perms param

--- Utils/permissionTranslator.js
+++ Utils/permissionTranslator.js
@@ -1,8 +1,8 @@
 const I18nProvider = require('./I18nProvider.js');
 
-/**@param {string|string[]}perms*/
+/**@param {string|string[]}perms @returns {string|string[]}*/
 module.exports = (perms, locale) => {
-  if (!perms?.length) return perms;
+  if (!perms?.length) return [];
   if (typeof perms == 'string') return I18nProvider.__({ locale, undefinedNotFound: true }, `others.Perms.${perms}`) || perms;
   return perms.map(perm => I18nProvider.__({ locale, undefinedNotFound: true }, `others.Perms.${perm}`) || perm);
 };
