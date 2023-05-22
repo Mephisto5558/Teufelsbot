acae8e4be4ec90a3443d6fde1f3a5ab7910e5a0f
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-05-22 16:42:48 +0200

    fixed only sending one arg

--- Handlers/event_handler.js
+++ Handlers/event_handler.js
@@ -1,4 +1,6 @@
-const { readdir } = require('fs/promises');
+const
+  { readdir } = require('fs/promises'),
+  { Client } = require('discord.js');
 
 module.exports = async function eventHandler() {
   let eventCount = 0;
@@ -6,7 +8,7 @@ module.exports = async function eventHandler() {
     if (!file.endsWith('js') || file == 'interactionCreate.js') continue; //InteractionCreate gets loaded when all slash commands are registred
 
     const eventName = file.split('.')[0];
-    this.on(eventName, args => require(`../Events/${file}`).call(...[].concat(args ?? this)));
+    this.on(eventName, (...args) => args.length == 1 && args[0] instanceof Client ? require(`../Events/${file}`).call(this) : require(`../Events/${file}`).call(...args, this));
     this.log(`Loaded Event ${eventName}`);
     eventCount++;
   }
