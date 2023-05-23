053e5e4857b6b05131650626c47d9e231db95c11
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-05-23 16:22:22 +0200

    added autocomplete validation in messageCreate & nsfw check

---
+++ Utils/autocompleteGenerator.js
@@ -0,0 +1,17 @@
+const I18nProvider = require('./I18nProvider.js');
+
+/**@this {import('discord.js').ChatInputCommandInteraction} */
+module.exports = async function autocompleteGenerator(command, locale) {
+  const response = v => ({ name: I18nProvider.__({ locale, undefinedNotFound: true }, `commands.${command.category.toLowerCase()}.${command.name}.options.${this.options?._group ? this.options._group + '.' : ''}${this.options?._subcommand ? this.options._subcommand + '.' : ''}${this.focused.name}.choices.${v}`) ?? v, value: v });
+
+  let { options } = command.fMerge();
+  if (this.options?._group) options = options.find(e => e.name == this.options._group);
+  if (this.options?._subcommand) options = options.find(e => e.name == this.options._subcommand).options;
+  options = options.find(e => e.name == this.focused.name).autocompleteOptions;
+  if (typeof options == 'function') options = await options.call(this);
+
+  if (options.constructor.name == 'Object') return [options];
+  return typeof options == 'string' ? [response(options)] : options
+    .filter(e => !this.focused.value || (e.toLowerCase?.() || e.value.toLowerCase()).includes(this.focused.value.toLowerCase()))
+    .slice(0, 25).map(e => typeof e == 'object' ? e : response(e));
+};
