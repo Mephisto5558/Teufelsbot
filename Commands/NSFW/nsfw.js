1761bd5af1adf80b3843e2d7999d1676e0b75866
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-05-23 16:22:26 +0200

    added nsfw command :c (implements #32)

---
+++ Commands/NSFW/nsfw.js
@@ -0,0 +1,37 @@
+const
+  { EmbedBuilder } = require('discord.js'),
+  fetch = require('node-fetch');
+
+module.exports = {
+  name: 'nsfw',
+  cooldowns: { guild: 0, user: 1000 },
+  slashCommand: true,
+  prefixCommand: true,
+  ephemeralDefer: true,
+  options: [{
+    name: 'type',
+    type: 'String',
+    autocompleteOptions: [
+      'hass', 'hmidriff', 'pgif', '4k', 'hentai', 'hneko', 'neko', 'hkitsune', 'anal', 'hanal',
+      'gonewild', 'ass', 'pussy', 'thigh', 'hthigh', 'paizuri', 'tentacle', 'boobs', 'hboobs', 'yaoi'
+    ],
+    strictAutocomplete: true
+  }],
+
+  run: async function (lang) {
+    const data = await fetch(`https://nekobot.xyz/api/image?type=${(this.options?.getString('type') || this.args?.[0] || 'hentai').toLowerCase()}`).then(e => e.json());
+    if (!data?.success) {
+      this.customReply(lang('error'));
+      return this.client.error('NSFW Command API Error: ', JSON.stringify(data));
+    }
+
+    const embed = new EmbedBuilder({
+      color: data.color,
+      image: { url: data.message }
+    });
+
+    if (!Math.floor(Math.random() * 10000)) embed.data.title = lang('embedTitle');
+
+    return this.customReply({ embeds: [embed] });
+  }
+};
