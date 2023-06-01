dacbd20c5becc4167bf51f811ad0b7cba493b93f
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-01 17:59:31 +0200

    rewrote /rps, added pvp, moved buttonhandlers to own folder

--- Commands/Minigames/rps.js
+++ Commands/Minigames/rps.js
@@ -1,97 +1,14 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Collection, ComponentType } = require('discord.js');
+const { rps_sendChallenge } = require('../../Utils/buttonPressHandler/');
 
 module.exports = {
   name: 'rps',
   aliases: { prefix: ['rockpaperscissors'] },
+  cooldowns: { user: 1000 },
   slashCommand: true,
   prefixCommand: true,
+  options: [{ name: 'opponent', type: 'User' }],
 
-  run: async function (lang) {
-    const
-      hand = new Collection([[lang('rock'), { id: 0, emoji: '✊' }], [lang('paper'), { id: 1, emoji: '🤚' }], [lang('scissors'), { id: 2, emoji: '✌️' }]]),
-      originalData = {
-        embeds: [new EmbedBuilder({ title: lang('embedTitle'), description: lang('embedDescription') }).setColor('Random')],
-        components: [
-          new ActionRowBuilder({
-            components: [
-              new ButtonBuilder({
-                customId: '0',
-                label: `✊ ${lang('rock')}`,
-                style: ButtonStyle.Primary
-              }),
-              new ButtonBuilder({
-                customId: '1',
-                label: `🤚 ${lang('paper')}`,
-                style: ButtonStyle.Primary
-              }),
-              new ButtonBuilder({
-                customId: '2',
-                label: `✌️ ${lang('scissors')}`,
-                style: ButtonStyle.Primary
-              })
-            ]
-          }),
-          new ActionRowBuilder({
-            components: [
-              new ButtonBuilder({
-                customId: 'cancel',
-                label: lang('global.cancel'),
-                style: ButtonStyle.Danger
-              })
-            ]
-          })
-        ]
-      },
-      msg = await this.customReply(originalData),
-      collector = msg.createMessageComponentCollector({ filter: i => this.member.id == i.user.id, componentType: ComponentType.Button, idle: 15000 });
-
-    let botMove = hand.random();
-    let win;
-
-    collector
-      .on('collect', async button => {
-        await button.deferUpdate();
-        switch (button.customId) {
-          case 'cancel': return collector.stop();
-          case 'playAgain': {
-            collector.empty();
-            botMove = hand.random();
-            msg.edit(originalData);
-            break;
-          }
-          case '0':
-          case '1':
-          case '2': {
-            const data = JSON.parse(JSON.stringify(originalData));
-
-            if (botMove.id == button.customId) win = [lang('tie'), '='];
-            else if (botMove.id < button.customId || botMove.id == 2 && !button.customId) win = [lang('win'), '>'];
-            else win = [lang('lose'), '<'];
-
-            data.embeds[0].description = lang('chose', { chose: [...hand.entries()].find(([, e]) => e.id == botMove.id)[0], win: win[0], you: hand.find(e => e.id == button.customId).emoji, symbol: win[1], i: botMove.emoji });
-            for (const compButton of data.components[0].components) {
-              if (compButton.custom_id == button.customId) button.style = ButtonStyle.Secondary;
-              compButton.disabled = true;
-            }
-
-            data.components[1] = new ActionRowBuilder({
-              components: [new ButtonBuilder({
-                customId: 'playAgain',
-                label: lang('global.playAgain'),
-                style: ButtonStyle.Success
-              })]
-            });
-
-            return msg.edit(data);
-          }
-        }
-      })
-      .on('end', collected => {
-        if (!collected.size || collected.last().customId == 'cancel') originalData.embeds[0].data.description = lang('timedOut');
-
-        for (const row of originalData.components) for (const button of row.components) button.setDisabled(true);
-
-        return msg.edit(originalData);
-      });
+  run: function (lang) {
+    return rps_sendChallenge.call(this, this.member, this.options?.getMember('opponent') || this.args?.[0], lang);
   }
 };
---
+++ Utils/buttonPressHandler/rps.js
@@ -0,0 +1,103 @@
+const
+  { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js'),
+  sendChallenge = require('./rps_sendChallenge.js'),
+  emojis = { r: '✊', p: '🤚', s: '✌️' };
+
+function sendGame(initiator, opponent, lang) {
+  const
+    embed = new EmbedBuilder({
+      title: lang('accept.embedTitle', { player1: initiator.displayName, player2: opponent.displayName }),
+      description: lang('accept.embedDescription')
+    }).setColor('Random'),
+    component = new ActionRowBuilder({
+      components: [
+        new ButtonBuilder({
+          customId: `rps.${initiator.id}.r.${opponent.id}`,
+          emoji: emojis.r,
+          label: lang('r'),
+          style: ButtonStyle.Primary
+        }),
+        new ButtonBuilder({
+          customId: `rps.${initiator.id}.p.${opponent.id}`,
+          emoji: emojis.p,
+          label: lang('p'),
+          style: ButtonStyle.Primary
+        }),
+        new ButtonBuilder({
+          customId: `rps.${initiator.id}.s.${opponent.id}`,
+          emoji: emojis.s,
+          label: lang('s'),
+          style: ButtonStyle.Primary
+        })
+      ]
+    });
+
+  return this.message.edit({ embeds: [embed], components: [component] });
+}
+
+/** this.customId: `rps.<initiatorId>.<mode>.<opponentId>`
+ * @this {import('discord.js').ButtonInteraction} @param {string}initiatorId @param {string}opponentId*/
+module.exports = async function rps(lang, initiatorId, mode, opponentId) {
+  if (this.user.id != initiatorId && this.user.id != opponentId) return;
+  if (mode.length != 1) await this.deferUpdate();
+
+  lang.__boundArgs__[0].backupPath = 'commands.minigames.rps';
+
+  const
+    initiator = await this.guild.members.fetch(initiatorId).catch(() => { }),
+    opponent = await this.guild.members.fetch(opponentId).catch(() => { });
+
+  if (!initiator || !opponent) {
+    this.message.embeds[0].data.description = lang('memberNotFound');
+    return this.message.edit({ embeds: this.message.embeds, components: [] });
+  }
+
+  switch (mode) {
+    case 'cancel':
+    case 'decline': {
+      this.message.embeds[0].data.description = lang('declined', this.member.displayName);
+      return this.message.edit({ embeds: this.message.embeds, components: [] });
+    }
+    case 'accept': if (opponent.user.bot || this.user.id == opponentId) return sendGame.call(this, initiator, opponent, lang); break;
+    case 'playAgain': {
+      if (opponent.user.bot) return sendGame.call(this, initiator, opponent, lang);
+      return sendChallenge.call(this, this.member, initiatorId == this.user.id ? opponent : initiator, lang);
+    }
+    case 'r':
+    case 'p':
+    case 's': {
+      const choices = this.client.db.get('guildSettings', `${this.guild.id}.minigames.rps.${this.message.id}`) || {};
+      if (opponentId == this.client.user.id) choices.player2 = ['r', 'p', 's'].random();
+
+      if (choices.player1 && this.user.id == initiatorId || choices.player2 && this.user.id == opponentId) return this.reply({ content: lang('win.alreadyChosen', choices.player1 || choices.player2), ephemeral: true });
+
+      if (this.user.id == initiatorId) {
+        choices.player1 = mode;
+        if (!choices.player2) await this.client.db.update('guildSettings', `${this.guild.id}.minigames.rps.${this.message.id}.player1`, mode);
+      }
+      else {
+        choices.player2 = mode;
+        if (!choices.player1) await this.client.db.update('guildSettings', `${this.guild.id}.minigames.rps.${this.message.id}.player2`, mode);
+      }
+
+      if (!choices.player1 || !choices.player2) return this.reply({ content: lang('end.saved', lang(mode)), ephemeral: true });
+      else await this.client.db.delete('guildSettings', `${this.guild.id}.minigames.rps.${this.message.id}`);
+
+      if (choices.player1 == choices.player2) this.message.embeds[0].data.description = lang('end.tie', emojis[mode]);
+      else {
+        const winner = choices.player1 == 'r' && choices.player2 == 's' || choices.player1 == 'p' && choices.player2 == 'r' || choices.player1 == 's' && choices.player2 == 'p' ? initiatorId : opponentId;
+        this.message.embeds[0].data.description = lang('end.win', { winner, winEmoji: emojis[initiatorId == winner ? choices.player1 : choices.player2], loseEmoji: emojis[initiatorId == winner ? choices.player2 : choices.player1] });
+      }
+
+      const component = new ActionRowBuilder({
+        components: [new ButtonBuilder({
+          customId: `rps.${initiatorId}.playAgain.${opponentId}`,
+          label: lang('global.playAgain'),
+          style: ButtonStyle.Success
+        })]
+      });
+
+      return this.update({ embeds: this.message.embeds, components: [component] });
+    }
+  }
+};
