d0c2ff659fe1dfe6781015d5dc31a39bf8e181a7
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-02 18:13:01 +0200

    fixed localizations checks

--- Utils/slashCommandsEqual.js
+++ Utils/slashCommandsEqual.js
@@ -1,12 +1,13 @@
 module.exports = function equal(a, b) {
   if (!a?.toString() && !b?.toString()) return true;
   if (typeof a == 'string' || typeof b == 'string') return a == b;
+  if (a == undefined && !Object.keys(b ?? {}).length || b == undefined && !Object.keys(a ?? {}).length) return true;
   if (
     !!a != !!b || a.name != b.name || a.description != b.description || a.type != b.type || a.autocomplete != b.autocomplete || a.dmPermission != b.dmPermission ||
     a.value != b.value || (a.options?.length ?? 0) != (b.options?.length ?? 0) || (a.channelTypes?.length ?? 0) != (b.channelTypes?.length ?? 0) ||
     (a.choices?.length ?? 0) != (b.choices?.length ?? 0) || a.minValue != b.minValue || a.maxValue != b.maxValue || a.minLength != b.minLength ||
     a.maxLength != b.maxLength || !!a.required != !!b.required || a.defaultMemberPermissions?.bitfield != b.defaultMemberPermissions?.bitfield ||
-    !equal(a.nameLocalizations, b.nameLocalizations) || !equal(a.description_localizations, b.description_localizations)
+    !equal(a.nameLocalizations, b.nameLocalizations) || !equal(a.descriptionLocalizations, b.descriptionLocalizations)
   ) return false;
 
   for (let i = 0; i < (a.choices?.length || 0); i++) if (!equal(a.choices?.[i], b.choices?.find(e => e.name == a.choices?.[i]?.name))) return false;
