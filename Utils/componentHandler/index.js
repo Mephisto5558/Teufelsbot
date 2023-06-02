0669821fb5f9376a7d72a0588e03fbb0661f7989
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-02 16:51:20 +0200

    added sorting options to mgstats

--- Utils/componentHandler/index.js
+++ Utils/componentHandler/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   infoCMDs: require('./infoCMDs.js'),
-  rps: require('./rps.js'),
+  mgStats_formatTopTen: require('./mgStats_formatTopTen.js'),
+  mgstats: require('./mgStats.js'),
   rps_sendChallenge: require('./rps_sendChallenge.js'),
+  rps: require('./rps.js'),
   selfrole: require('./selfrole.js')
 };
---
+++ Utils/componentHandler/mgStats_formatTopTen.js
@@ -0,0 +1,22 @@
+const medals = [':first_place:', ':second_place:', ':third_place:'];
+
+module.exports = async function formatTopTen(input, sort, mode, lang) {
+  if (input.length > 1) {
+    switch (mode) {
+      case 'draws': input.sort(([, a], [, b]) => b.draws - a.draws || b.wins - a.wins || a.loses - b.loses); break;
+      case 'loses': input.sort(([, a], [, b]) => b.loses - a.loses || a.wins - b.wins || b.draws - a.draws); break;
+      case 'alphabet_user': input.sort(([, a], [, b]) => this.guild.members.cache.get(a).user.username.localeCompare(this.guild.members.cache.get(b).user.username, lang.__boundArgs__[0].locale, { sensitivity: 'base' })); break;
+      case 'alphabet_nick': input.sort(([, a], [, b]) => this.guild.members.cache.get(a).displayName.localeCompare(this.guild.members.cache.get(b).displayName, lang.__boundArgs__[0].locale, { sensitivity: 'base' })); break;
+      default: input.sort(([, a], [, b]) => b.wins - a.wins || a.draws - b.draws || a.loses - b.loses);
+    }
+
+    if (sort == 'f') input.reverse();
+  }
+
+  return input.slice(0, 10).reduce((acc, [id, stats], i) => acc + (
+    acc.length > 3997 ? '...' : `${medals[i] || i + 1 + '.'} <@${id}>\n` +
+      '> ' + lang('wins', stats.wins || 0) +
+      '> ' + lang('loses', stats.loses || 0) +
+      '> ' + lang('draws', stats.draws || 0)
+  ), '');
+};
