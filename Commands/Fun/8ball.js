a5b1f94a120f2803be973ab13b271e500c8475a2
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-02 19:02:29 +0200

    same question = same response (daily update)

--- Commands/Fun/8ball.js
+++ Commands/Fun/8ball.js
@@ -1,3 +1,20 @@
+
+function cyrb53(str, seed = 0) { //https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
+  let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+}
+
 module.exports = {
   name: '8ball',
   slashCommand: true,
@@ -9,5 +26,12 @@ module.exports = {
     required: true
   }],
 
-  run: function (lang) { return this.customReply(this.content || this.options?.getString('question') ? lang('responseList') : lang('noQuestion')); }
+  run: function (lang) {
+    const input = this.options?.getString('question') || this.content;
+    if (!input) return this.customReply(lang('noQuestion'));
+
+    const now = new Date();
+    const responseList = lang.__boundThis__.localeData[lang.__boundArgs__[0].locale][lang.__boundArgs__[0].backupPath + '.responseList'];
+    return this.customReply(responseList[cyrb53(input.toLowerCase(), parseInt(this.user.id) ^ cyrb53(`${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`)) % responseList.length]);
+  }
 };
