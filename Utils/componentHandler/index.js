0859af32cc633db1d9fd2f25bd91eafb6a87ed26
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-03 15:49:21 +0200

    added another one-button for /reddit

--- Utils/componentHandler/index.js
+++ Utils/componentHandler/index.js
@@ -2,6 +2,7 @@ module.exports = {
   infoCMDs: require('./infoCMDs.js'),
   mgStats_formatTopTen: require('./mgStats_formatTopTen.js'),
   mgstats: require('./mgStats.js'),
+  reddit: require('./reddit.js'),
   rps_sendChallenge: require('./rps_sendChallenge.js'),
   rps: require('./rps.js'),
   selfrole: require('./selfrole.js')
---
+++ Utils/componentHandler/reddit.js
@@ -0,0 +1,15 @@
+
+/** this.customId: `reddit.<subreddit>.<type>.<filterNSFW>`
+ * @this {import('discord.js').ButtonInteraction} @param {string}subreddit @param {string}type @param {'true'|'false'}filterNSFW*/
+module.exports = function reddit(lang, subreddit, type, filterNSFW) {
+  lang.__boundArgs__[0].backupPath = 'commands.fun.reddit';
+
+  this.options = {
+    getBoolean: () => filterNSFW == 'true',
+    getString: (str) => str == 'type' ? type : subreddit
+  };
+
+  this.update({ components: [] });
+
+  this.client.slashCommands.get('reddit').run.call(this, lang);
+};
