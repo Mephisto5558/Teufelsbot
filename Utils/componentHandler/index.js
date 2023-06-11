d867e48130a86e985e50b4d60dee1063f4666a6e
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-11 14:48:47 +0200

    i keep forgetting that

--- Utils/componentHandler/index.js
+++ Utils/componentHandler/index.js
@@ -7,5 +7,6 @@ module.exports = {
   reddit: require('./reddit.js'),
   rps_sendChallenge: require('./rps_sendChallenge.js'),
   rps: require('./rps.js'),
-  selfrole: require('./selfrole.js')
+  selfrole: require('./selfrole.js'),
+  topic: require('./topic.js')
 };
