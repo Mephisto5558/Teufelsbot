d81fd258b577f973e85a8d60647b981ddaf1d5b8
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-11 14:48:19 +0200

    added anotherone button for topic

--- Commands/Fun/topic.js
+++ Commands/Fun/topic.js
@@ -1,5 +1,5 @@
 //Credits for many of the response messages goes to the Lawliet Bot: https://github.com/Aninoss/lawliet-bot/tree/master/src/main/jib/data/resources
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
   name: 'topic',
@@ -9,12 +9,20 @@ module.exports = {
   dmPermission: true,
 
   run: function (lang) {
-    const embed = new EmbedBuilder({
-      title: lang('embedTitle'),
-      description: lang('embedDescription'),
-      footer: { text: this.user.tag, iconURL: this.member.displayAvatarURL() }
-    }).setColor('Random');
+    const
+      embed = new EmbedBuilder({
+        title: lang('embedTitle'),
+        description: lang('embedDescription'),
+        footer: { text: this.user.tag, iconURL: this.member.displayAvatarURL() }
+      }).setColor('Random'),
+      component = new ActionRowBuilder({
+        components: [new ButtonBuilder({
+          label: lang('global.anotherone'),
+          customId: 'topic',
+          style: ButtonStyle.Primary
+        })]
+      });
 
-    return this.customReply({ embeds: [embed] });
+    return this.customReply({ embeds: [embed], components: [component] });
   }
 };
