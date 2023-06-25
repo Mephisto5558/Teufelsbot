eab711ddcb2d810a56f4f6cfdafc7b4e4aaec1f6
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-06-25 10:50:35 +0200

    re-enabled chatgpt command, added regenerate button & model choices

--- Commands/Premium/chatgpt.js
+++ Commands/Premium/chatgpt.js
@@ -1,21 +1,22 @@
 const
-  fetch = require('node-fetch').default,
-  { createHash } = require('crypto'),
-  { Message, AttachmentBuilder } = require('discord.js');
+  { Message, AttachmentBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js'),
+  fetch = require('node-fetch').default;
 
 /**@returns {Promise<String>}*/
 async function fetchAPI(lang, deep) {
-  const res = await fetch('https://chatgpt-api.shn.hk/v1/', {
+  const res = await fetch('https://api.pawan.krd/v1/chat/completions', { //https://github.com/PawanOsman/ChatGPT
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      Authorization: `Bearer ${this.client.keys.chatGPTApiKey}`,
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify({
       model: this.options?.getString('model') ?? 'gpt-3.5-turbo',
-      user: createHash('sha256').update(this.user.id).digest('hex'),
       messages: [{ role: 'user', content: this.options?.getString('message') || this.content || lang('hello') }]
     })
-  }).then(e => e.text().then(e => JSON.parse(e.slice(e.indexOf('{')))));
+  }).then(e => e.text().then(e => JSON.parse(e)));
 
-  if (!res.error) return res.choices[0].message.content;
+  if (res.choices?.[0].message.content) return res.choices[0].message.content;
   if (['Rate limit reached', 'Too many requests'].some(e => res.error.message.startsWith(e))) return deep ? lang('rateLimit') : fetchAPI.call(this, lang, true);
   if (res.error.type == 'insufficient_quota' || res.error.message.startsWith('That model is currently overloaded')) return lang('notAvailable');
 
@@ -26,23 +27,47 @@ async function fetchAPI(lang, deep) {
 module.exports = {
   name: 'chatgpt',
   aliases: { prefix: ['gpt'] },
-  cooldowns: { guild: 0, user: 1000 },
+  cooldowns: { guild: 0, user: 2000 },
   slashCommand: true,
   prefixCommand: true,
   dmPermission: true,
   premium: true,
-  disabled: true,//API has been disabled by owner
-  options: [{
-    name: 'message',
-    type: 'String',
-    maxLength: 2000,
-    required: true
-  }], beta: true,
+  options: [
+    {
+      name: 'message',
+      type: 'String',
+      maxLength: 2000,
+      required: true
+    },
+    {
+      name: 'model',
+      type: 'String',
+      choices: ['gpt-4', 'gpt-4-0613', 'gpt-4-32k', 'gpt-4-32k-0613', 'gpt-3.5-turbo', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-16k-0613']
+    }
+  ],
 
+  /**@this {import('discord.js').Message}*/
   run: async function (lang) {
     if (this instanceof Message) this.channel.sendTyping();
 
-    const message = await fetchAPI.call(this, lang);
-    return this.customReply(message.length > 2000 ? { files: [new AttachmentBuilder(Buffer.from(message), { name: 'message.txt' })] } : message, null, { repliedUser: true });
+    const
+      content = await fetchAPI.call(this, lang),
+      component = new ActionRowBuilder({
+        components: [new ButtonBuilder({
+          label: lang('regenerate'),
+          customId: `chatgpt.regenerate.${this.options?.getString('model') ?? 'gpt-3.5-turbo'}`,
+          style: ButtonStyle.Secondary
+        })]
+      });
+
+    return (await this.customReply(content.length > 2000 ? { files: [new AttachmentBuilder(Buffer.from(content), { name: 'message.txt' })], components: [component] } : { content, components: [component] }, null, { repliedUser: true }))
+      .createMessageComponentCollector({ componentType: ComponentType.Button, filter: e => e.user.id == this.user.id })
+      .on('collect', async e => {
+        const reply = await e.deferReply();
+
+        const newContent = await fetchAPI.call(this, lang);
+        e.message.edit(newContent.length > 2000 ? { files: [new AttachmentBuilder(Buffer.from(newContent), { name: 'message.txt' })] } : newContent);
+        reply.delete();
+      });
   }
 };
