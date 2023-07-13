9299646720139eaac2b2a45128483e370e10e8b0
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-07-13 16:28:52 +0200

    added missing global vars

--- Commands/Owner-Only/eval.js
+++ Commands/Owner-Only/eval.js
@@ -1,4 +1,7 @@
-const AsyncFunction = (async function () { }).constructor;
+const
+  vars = ['__dirname', '__filename', 'exports', 'module', 'require', 'lang'],
+  BoundAsyncFunction = (async function () { }).constructor.bind(null, ...vars),
+  BoundFunction = Function.bind(null, ...vars);
 
 module.exports = {
   name: 'eval',
@@ -11,7 +14,7 @@ module.exports = {
     if (!this.content) return;
 
     try {
-      await (this.content.includes('await') ? new AsyncFunction('lang', this.content) : new Function('lang', this.content)).call(this, lang);
+      await (this.content.includes('await') ? new BoundAsyncFunction(this.content) : new BoundFunction(this.content)).call(this, __dirname, __filename, exports, module, require, lang);
       await this.customReply(lang('success', lang('finished', this.content)));
     }
     catch (err) { this.customReply(lang('error', { msg: lang('finished', this.content), name: err.name, err: err.message })); }
