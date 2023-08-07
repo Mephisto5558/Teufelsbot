44e6bf8c3165b1853ed832324bbc88ba5babec16
FloArmy <141579696+FloArmy@users.noreply.github.com>
2023-08-07 19:12:30 +0200

    Fixed

--- Commands/Fun/motherweight.js
+++ Commands/Fun/motherweight.js
@@ -10,6 +10,6 @@ module.exports = {
       target = this.options?.getMember('target') || this.mentions?.members.first() || this.guild.members.cache.find(e => [e.user.id, e.user.username, e.user.tag, e.nickname].some(e => [...(this.args || []), this.content].includes(e))),
       weight = Math.round(Math.random() * 1000);
 
-    return this.customReply(lang(`responses${target ? 'Other' : 'Self'}.${Math.ceil(weight / 100)}`, { user: target?.displayName }) + lang('weight', weight));
+    return this.customReply(lang(`responses${target ? 'Others' : 'Self'}.${Math.ceil(weight / 100)}`, { user: target?.displayName }) + lang('weight', weight));
   }
 };
