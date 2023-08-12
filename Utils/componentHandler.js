f0e581deb8397a7cff72bf93433015a25f7a5211
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-08-12 13:34:17 +0200

    fixed error

--- Utils/componentHandler.js
+++ Utils/componentHandler.js
@@ -15,7 +15,7 @@ module.exports = async function MessageComponentHandler(lang) {
   if (disabledList.members && disabledList.members.includes(this.user.id)) return this.reply({ embeds: [errorEmbed.setDescription(lang('events.notAllowed.member'))], ephemeral: true });
   if (disabledList.channels && disabledList.channels.includes(this.channel.id)) return this.reply({ embeds: [errorEmbed.setDescription(lang('events.notAllowed.channel'))], ephemeral: true });
   if (disabledList.roles && this.member.roles.cache.some(e => disabledList.roles.includes(e.id))) return this.reply({ embeds: [errorEmbed.setDescription(lang('events.notAllowed.role'))], ephemeral: true });
-  if (command.category.toLowerCase() == 'nsfw' && !this.channel.nsfw) return this.reply({ embeds: [errorEmbed.setDescription(lang('events.nsfwCommand'))], ephemeral: true });
+  if (command.category?.toLowerCase() == 'nsfw' && !this.channel.nsfw) return this.reply({ embeds: [errorEmbed.setDescription(lang('events.nsfwCommand'))], ephemeral: true });
   if (cooldown) return this.reply({ content: lang('events.buttonPressOnCooldown', cooldown), ephemeral: true });
 
   if (handlers[feature]) return handlers[feature].call(this, lang, id, mode, data, args);
