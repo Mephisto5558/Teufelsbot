787f6f5a9cc3d7232f56d00e846a2af078a47f11
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-09-09 13:02:03 +0200

    fixed S6638

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -1,5 +1,5 @@
 module.exports = async function guildCreate() {
   log.debug(`Joined new guild: ${this.id}`);
   if (this.client.botType == 'dev') return;
-  await this.client.db.update('guildSettings', `${this.id}.position`, (Object.values(this.client.db.get('guildSettings')).sort((a, b) => b?.position ?? 0 - a?.position ?? 0)[0].position || 0) + 1);
+  await this.client.db.update('guildSettings', `${this.id}.position`, (Object.values(this.client.db.get('guildSettings')).sort((a, b) => (b?.position || 0) - (a?.position || 0))[0].position || 0) + 1);
 };
