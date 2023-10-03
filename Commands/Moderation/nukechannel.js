ede972a2d4e4c877c0dece4da9881ad153116e79
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-10-03 20:06:31 +0200

    added reason to clone (removed parent because d.js handles that too)

--- Commands/Moderation/nukechannel.js
+++ Commands/Moderation/nukechannel.js
@@ -27,13 +27,13 @@ module.exports = {
       embed = new EmbedBuilder({
         description: lang('embedDescription'),
         color: Colors.Red,
-        image: { url: 'https://giphy.com/media/XUFPGrX5Zis6Y/giphy.gif' },
+        image: { url: 'https://i.giphy.com/media/XUFPGrX5Zis6Y/giphy.webp' },
         footer: { text: lang('embedFooterText', this.user.tag) }
       }),
       channel = this.options?.getChannel('channel') || this.channel,
-      cloned = await channel.clone({ parent: channel.parentId });
+      cloned = await channel.clone({ reason: lang('global.modReason', { command: this.commandName, user: this.user.username }) });
 
-    await channel.delete(lang('global.modReason', { command: this.commandName, user: this.user.tag }));
+    await channel.delete(lang('global.modReason', { command: this.commandName, user: this.user.username }));
     return cloned.send({ embeds: [embed] });
   }
 };
