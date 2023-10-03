171b4261329f86f8af0ded284be2eb0e5fb7c7f5
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-10-03 11:38:33 +0200

    corrected lang key

--- Events/messageDelete.js
+++ Events/messageDelete.js
@@ -24,15 +24,15 @@ module.exports = async function messageDelete() {
   await sleep(1000); //Make sure the audit log gets created before trying to fetching it
 
   const
-    lang = I18nProvider.__.bBind(I18nProvider, { locale: this.guild.db.config?.lang ?? this.guild.localeCode, backupPath: 'events.logger.messageDelete' }),
+    lang = I18nProvider.__.bBind(I18nProvider, { locale: this.guild.db.config?.lang ?? this.guild.localeCode, backupPath: 'events.logger' }),
     { executor, reason } = (await this.guild.fetchAuditLogs({ limit: 6, type: AuditLogEvent.MessageDelete })).entries.find(e => (!this.user?.id || e.target.id == this.user.id) && e.extra.channel.id == this.channel.id && Date.now() - e.createdTimestamp < 20000) ?? {},
     embed = new EmbedBuilder({
       author: executor ? { name: executor.tag, iconURL: executor.displayAvatarURL() } : null,
       thumbnail: this.member ? { url: this.member.displayAvatarURL({ size: 128 }) } : null,
-      description: lang('embedDescription', { executor: executor ? `<@${executor.id}>` : lang('events.logger.someone'), channel: this.channel.name }),
+      description: lang('messageDelete.embedDescription', { executor: executor ? `<@${executor.id}>` : lang('someone'), channel: this.channel.name }),
       fields: [
         { name: lang('global.channel'), value: `<#${this.channel.id}> (\`${this.channel.id}\`)`, inline: false },
-        { name: lang('content'), value: '', inline: false }
+        { name: lang('messageDelete.content'), value: '', inline: false }
       ],
       timestamp: Date.now(),
       color: 8530669,
@@ -40,14 +40,14 @@ module.exports = async function messageDelete() {
 
   if (this.originalContent) embed.data.fields[1].value += `${this.originalContent}\n`;
   if (this.attachments.size) embed.data.fields[1].value += this.attachments.map(e => `[${e.url}](${e.name})`).join(', ') + '\n';
-  if (this.embeds.length) embed.data.fields[1].value += lang('events.logger.embeds', this.embeds.length) + '\n';
-  if (this.components.length) embed.data.fields[1].value += lang('components', this.components.length);
-  if (!embed.data.fields[1].value) embed.data.fields[1].value += lang('event.logger.unknownContent');
+  if (this.embeds.length) embed.data.fields[1].value += lang('embeds', this.embeds.length) + '\n';
+  if (this.components.length) embed.data.fields[1].value += lang('messageDelete.components', this.components.length);
+  if (!embed.data.fields[1].value) embed.data.fields[1].value += lang('unknownContent');
 
   //We don't get the user/member if the message is not cached
-  if (this.user) embed.data.fields.push({ name: lang('author'), value: `${this.user.tag} (\`${this.user.id}\`)`, inline: false });
-  if (executor) embed.data.fields.push({ name: lang('events.logger.executor'), value: `${executor.tag} (\`${executor.id}\`)`, inline: false });
-  if (reason) embed.data.fields.push({ name: lang('events.logger.reason'), value: reason, inline: false });
+  if (this.user) embed.data.fields.push({ name: lang('messageDelete.author'), value: `${this.user.tag} (\`${this.user.id}\`)`, inline: false });
+  if (executor) embed.data.fields.push({ name: lang('executor'), value: `${executor.tag} (\`${executor.id}\`)`, inline: false });
+  if (reason) embed.data.fields.push({ name: lang('reason'), value: reason, inline: false });
 
   return channel.send({ embeds: [embed] });
 };
