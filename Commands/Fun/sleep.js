906b7b2f944dad59a0082abe4b0173d40a858033
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-10-21 16:45:01 +0200

    fixed a bug

--- Commands/Fun/sleep.js
+++ Commands/Fun/sleep.js
@@ -5,7 +5,7 @@ module.exports = {
   dmPermission: true,
 
   run: async function (lang) {
-    if (this.member.moderatable && this.member.displayName.length < 26 && !this.member.nickname?.startsWith('[AFK] ')) return this.member.setNickname(`[AFK] ${this.member.displayName}`);
+    if (this.member.moderatable && this.member.displayName.length < 26 && !this.member.nickname?.startsWith('[AFK] ')) this.member.setNickname(`[AFK] ${this.member.displayName}`);
 
     await this.client.db.update('userSettings', `${this.user.id}.afkMessage`, { message: lang('afkMessage'), createdAt: Math.round(this.createdTimestamp / 1000).toString() });
     return this.customReply(lang('responseList', this.member.customName));
