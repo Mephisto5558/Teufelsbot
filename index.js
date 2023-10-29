7c4f658ea7bd6f16fa980af3b3c13c3fdc75efff
Mephisto555 <87113906+Mephisto5558@users.noreply.github.com>
2023-10-29 18:34:58 +0100

    updated version

--- index.js
+++ index.js
@@ -48,13 +48,13 @@ console.time('Starting time');
   let env;
   try { env = require('./env.json'); }
   catch {
-    client.db = await new DB(process.env.dbConnectionStr, 100).fetchAll();
+    client.db = await new DB(process.env.dbConnectionStr, 'db-collection', 100).fetchAll();
     env = client.settings.env;
   }
 
   env = env.global.fMerge(env[env.global.environment]);
 
-  client.db ??= await new DB(env.dbConnectionStr, 100).fetchAll();
+  client.db ??= await new DB(env.dbConnectionStr, 'db-collection', 100).fetchAll();
 
   client.botType = env.environment;
   client.keys = env.keys;
