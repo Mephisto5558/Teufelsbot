f5de1eb7933a3c678e2741e601261e721d9cca2d
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-11-01 14:59:49 +0100

    general optimizations

--- Utils/checkTargetManageable.js
+++ Utils/checkTargetManageable.js
@@ -2,5 +2,5 @@
 module.exports = function checkTargetManageable(member) {
   if (member.id == this.member.id) return 'cantPunishSelf';
   if (!member.manageable) return 'global.noPermBot';
-  if (this.guild.ownerId != this.user.id && member.roles?.highest?.position - this.member.roles.highest.position >= 0) return 'global.noPermUser';
+  if (this.guild.ownerId != this.user.id && member.roles?.highest?.position >= this.member.roles.highest.position) return 'global.noPermUser';
 };
