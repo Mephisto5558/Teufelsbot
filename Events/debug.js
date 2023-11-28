185fd59288edfc071c8074897f42c219ba54d9e8
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2023-11-28 12:15:28 +0100

    removed accidentally added char

--- Events/debug.js
+++ Events/debug.js
@@ -5,7 +5,7 @@ module.exports = function debug() {
   if (debug.includes('Sending a heartbeat.') || debug.includes('Heartbeat acknowledged')) return;
 
   for (const match of /(?:Session |token: )(\w*)/gi.exec(debug)?.slice(1) || []) debug = debug.replace(match, '(CENSORED)');
--
+
   log.setType('API').debug(debug).setType();
   if (debug.includes('Hit a 429')) {
     if (this.isReady()) return void log.error('Hit a 429 while trying to execute a request');
