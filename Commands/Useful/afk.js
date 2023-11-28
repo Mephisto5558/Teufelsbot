88ef365c2402aa88f71ea793215d8ae026f5dcb6
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2023-11-28 08:07:54 +0100

    fixed the afk message from dm command

--- Commands/Useful/afk.js
+++ Commands/Useful/afk.js
@@ -18,15 +18,15 @@ module.exports = {
   /**@this Interaction|Message @param {lang}lang*/
   run: async function (lang) {
     const
-      global = !this.guildId || (this.options?.getBoolean('global') ?? this.args?.[0] == 'global'),
+      global = this.options?.getBoolean('global') ?? this.args?.[0] == 'global',
       message = this.options?.getString('message') || this.content?.substring(global ? 7 : 0, 1000) || 'AFK',
       createdAt = Math.round(this.createdTimestamp / 1000);
 
-    if (global) await this.client.db.update('userSettings', `${this.user.id}.afkMessage`, { message, createdAt });
+    if (global || !this.guildId) await this.client.db.update('userSettings', `${this.user.id}.afkMessage`, { message, createdAt });
     else await this.client.db.update('guildSettings', `${this.guild.id}.afkMessages.${this.user.id}`, { message, createdAt });
 
-    if (this.member && this.member.moderatable && this.member.displayName.length < 26 && !this.member.nickname?.startsWith('[AFK] ')) this.member.setNickname(`[AFK] ${this.member.displayName}`);
+    if (this.member?.moderatable && this.member.displayName.length < 26 && !this.member.nickname?.startsWith('[AFK] ')) this.member.setNickname(`[AFK] ${this.member.displayName}`);
 
-    return this.customReply({ content: lang(global ? 'globalSuccess' : 'success', message), allowedMentions: { parse: [AllowedMentionsTypes.User] } });
+    return this.customReply({ content: lang(global || !this.guildId ? 'globalSuccess' : 'success', message), allowedMentions: { parse: [AllowedMentionsTypes.User] } });
   }
 };
