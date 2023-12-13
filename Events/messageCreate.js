5583bc5a156144ddb16d9cacebfa6760aa2cbc5c
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2023-12-13 20:05:08 +0100

    added commandExecutionWrapper to remove duplicate code, added commandExecutionWrapper to component functions

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -1,11 +1,11 @@
 const
   { EmbedBuilder, Colors } = require('discord.js'),
-  { errorHandler, checkForErrors } = require('../Utils');
+  { checkForErrors, commandExecutionWrapper } = require('../Utils');
 
 /**@this Message*/
 module.exports = async function messageCreate() {
   if (this.client.settings.blacklist?.includes(this.user.id)) return;
-  
+
   if (this.botType != 'dev' && this.guild) {
     if (this.guild.db.config?.autopublish && this.crosspostable) this.crosspost();
 
@@ -25,11 +25,5 @@ module.exports = async function messageCreate() {
   if (errorKey === true) return;
   else if (errorKey) return this.customReply({ embeds: [new EmbedBuilder({ description: lang(...errorKey), color: Colors.Red })] }, 1e4);
 
-  const cmdLang = this.client.i18n.__.bBind(this.client.i18n, { locale: this.guild?.db.config?.lang ?? this.guild?.localeCode, backupPath: command ? `commands.${command.category.toLowerCase()}.${command.aliasOf ?? command.name}` : null });
-  
-  try {
-    log.debug(`Executing prefix command ${command.name}`);
-    command.run.call(this, cmdLang)?.catch(err => errorHandler.call(this.client, err, this, lang));
-    if (this.client.botType != 'dev') await this.client.db.update('botSettings', `stats.${command.name}`, this.client.settings.stats?.[command.name] + 1 || 1);
-  } catch (err) { errorHandler.call(this.client, err, this, lang); }
+  return commandExecutionWrapper.call(this, command, 'prefix', lang);
 };
