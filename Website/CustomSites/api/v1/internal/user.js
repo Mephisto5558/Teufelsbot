d3047db44a27738cb77f1473438f59b8f05ff36d
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-01-07 14:09:29 +0100

    fixed

--- Website/CustomSites/api/v1/internal/user.js
+++ Website/CustomSites/api/v1/internal/user.js
@@ -1,7 +1,7 @@
 module.exports = {
-  /**@this Client @param {Res}res @param {Req}req*/
+  /**@this WebServer @param {Res}res @param {Req}req*/
   run: async function (res, req) {
-    if ((await this.db.get('botSettings', 'blacklist'))?.includes(req.user?.id)) return res.json({ errorCode: 403, error: 'You have been blacklisted from using the bot.' });
-    return req.user ? res.json({ ...req.user, dev: this.application.owner.id == req.user?.id }) : res.json({ errorCode: 401, error: 'Not logged in' });
+    if (this.db.get('botSettings', 'blacklist')?.includes(req.user?.id)) return res.json({ errorCode: 403, error: 'You have been blacklisted from using the bot.' });
+    return req.user ? res.json({ ...req.user, dev: this.client.application.owner.id == req.user?.id }) : res.json({ errorCode: 401, error: 'Not logged in' });
   }
 };
