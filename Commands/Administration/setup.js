12ac3f9970b8f2990285c09ea43818c401bf506e
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-01-08 15:59:47 +0100

    restructured code

--- Commands/Administration/setup.js
+++ Commands/Administration/setup.js
@@ -2,8 +2,135 @@ const
   { Constants, EmbedBuilder, Colors } = require('discord.js'),
   backup = new Map([['creator', 0], ['owner', 1], ['creator+owner', 2], ['admins', 3]]),
   loggerActionTypes = ['messageDelete', 'messageUpdate', 'voiceChannelActivity', 'sayCommandUsed'],
+  getCMDs = /**@param {Client}client*/ client => [...new Set([...client.prefixCommands.filter(e => !e.aliasOf).keys(), ...client.slashCommands.filter(e => !e.aliasOf).keys()])],
+  setupMainFunctions = {
+    /**@this GuildInteraction @param {lang}lang*/
+    toggle_module: async function (lang) {
+      const
+        module = this.options.getString('module'),
+        setting = this.guild.db[module]?.enable;
 
-  getCmds = /**@param {Client}client*/ client => [...new Set([...client.prefixCommands.filter(e => !e.aliasOf).keys(), ...client.slashCommands.filter(e => !e.aliasOf).keys()])];
+      await this.client.db.update('guildSettings', `${this.guild.id}.${module}.enable`, !setting);
+      return this.editReply(lang('success', { name: module, state: lang(setting ? 'global.disabled' : 'global.enabled') }));
+    },
+
+    /**@this GuildInteraction @param {lang}lang*/
+    toggle_command: async function (lang) {
+      const
+        command = this.options.getString('command'),
+        commandData = this.guild.db.commandSettings?.[command]?.disabled || {},
+        { roles = [], channels = [], users = [] } = commandData,
+        count = { enabled: { channels: 0, users: 0, roles: 0 }, disabled: { channels: 0, users: 0, roles: 0 } };
+
+      if (!getCMDs(this.client).includes(command)) return this.editReply(lang('notFound'));
+
+      if (this.options.getBoolean('get')) {
+        const fields = [['roles', roles], ['channels', channels], ['users', users]].filter(([, e]) => e?.length).map(([k, v]) => ({
+          name: lang(k),
+          value: v.includes('*') ? lang('list.all') : v.map(e => {
+            if (k == 'roles') return `<@&${e}>`;
+            return (k == 'channels' ? '<#' : '<@') + `${e}>`;
+          }).join(', '),
+          inline: false
+        }));
+
+        const embed = new EmbedBuilder({
+          title: lang('list.embedTitle', command),
+          color: Colors.White,
+          ...(fields.length ? { fields } : { description: lang('list.embedDescription') }),
+        });
+
+        return this.editReply({ embeds: [embed] });
+      }
+
+      if (this.options.data[0].options.length == (this.options.data[0].options.some(e => e.name == 'get') ? 2 : 1)) {
+        await this.client.db.update('guildSettings', `${this.guild.id}.commandSettings.${command}.disabled.users`, users.includes('*') ? users.filter(e => e != '*') : ['*', ...users]);
+        return this.editReply(lang(users.includes('*') ? 'enabled' : 'disabled', command));
+      }
+
+      if (users.includes('*')) return this.editReply(lang('isDisabled', { command, id: this.command.id }));
+
+      for (const [typeIndex, typeFilter] of ['role', 'member', 'channel'].entries()) {
+        const ids = [...new Set(this.options.data[0].options.filter(e => e.name.includes(typeFilter)).map(e => e.value))];
+
+        let type = 'roles';
+        if (typeIndex == 1) type = 'users';
+        else if (typeIndex == 2) type = 'channels';
+
+        for (const id of ids) {
+          if (commandData[type]?.includes(id)) {
+            commandData[type] = commandData[type].filter(e => e !== id);
+            count.enabled[type]++;
+            continue;
+          }
+
+          commandData[type] = [...(commandData[type] || []), id];
+          count.disabled[type]++;
+        }
+      }
+
+      const embed = new EmbedBuilder({
+        title: lang('embedTitle', command),
+        description: lang('embedDescription', this.command.id),
+        fields: Object.entries(count).filter(([, v]) => Object.values(v).find(Boolean)).map(([k, v]) => ({
+          name: lang(`embed.${k}`),
+          value: Object.entries(v).filter(([, e]) => e).map(([k, v]) => lang(k) + `: **${v}**`).join('\n'),
+          inline: true
+        })),
+        color: Colors.White
+      });
+
+      await this.client.db.update('guildSettings', `${this.guild.id}.commandSettings.${command}.disabled`, commandData);
+      return this.editReply({ embeds: [embed] });
+    },
+
+    /**@this GuildInteraction @param {lang}lang*/
+    language: async function () {
+      const
+        language = this.options.getString('language'),
+        /**@type {lang}*/
+        newLang = this.client.i18n.__.bind(this.client.i18n, { locale: this.client.i18n.availableLocales.has(language) ? language : this.client.i18n.config.defaultLocale }),
+        { category, name } = this.client.slashCommands.get(this.commandName),
+        embed = new EmbedBuilder({
+          title: newLang(`commands.${category.toLowerCase()}.${name}.language.embedTitle`),
+          description: newLang(`commands.${category.toLowerCase()}.${name}.language.embedDescription`, newLang('global.languageName')),
+          color: Colors.Green
+        });
+
+      await this.client.db.update('guildSettings', `${this.guild.id}.config.lang`, language);
+      return this.editReply({ embeds: [embed] });
+    },
+
+    /**@this GuildInteraction @param {lang}lang*/
+    serverbackup: async function (lang) {
+      await this.client.db.update('guildSettings', 'serverbackup.allowedToLoad', parseInt(backup.get(this.options.getString('allowed_to_load'))));
+      return this.editReply(lang('success'));
+    },
+
+    /**@this GuildInteraction @param {lang}lang*/
+    autopublish: async function (lang) {
+      const enabled = this.options.getBoolean('enabled');
+      await this.client.db.update('guildSettings', `${this.guild.id}.config.autopublish`, enabled);
+      return this.customReply(lang('success', lang(`global.${enabled ? 'enabled' : 'disabled'}`)));
+    },
+
+    /**@this GuildInteraction @param {lang}lang*/
+    logger: async function (lang) {
+      const
+        channel = (this.options.getChannel('channel') ?? this.guild.channels.cache.get(this.guild.db.config.logger?.[action]?.channel))?.id ?? this.channel,
+        action = this.options.getString('action'),
+        enabled = this.options.getBoolean('enabled') ?? (action == 'all' ? null : !this.guild.db.config.logger?.[action]?.enabled);
+
+      if (!channel) return this.editReply(lang('noChannel'));
+      if (action == 'all') {
+        if (enabled == null) return this.editReply(lang('noEnabled'));
+        for (const action of loggerActionTypes) await this.client.db.update('guildSettings', `${this.guild.id}.config.logger.${action}`, { channel, enabled });
+      }
+
+      await this.client.db.update('guildSettings', `${this.guild.id}.config.logger.${action}`, { channel, enabled });
+      return this.editReply(lang(enabled ? 'enabled' : 'disabled', { channel, action: lang(`actions.${action}`) }));
+    }
+  };
 
 /**@type {command}*/
 module.exports = {
@@ -32,12 +159,12 @@ module.exports = {
           name: 'command',
           type: 'String',
           required: true,
-          autocompleteOptions: function () { return getCmds(this.client); },
+          autocompleteOptions: function () { return getCMDs(this.client); },
           strictAutocomplete: true
         },
         { name: 'get', type: 'Boolean' },
         ...Array.from({ length: 6 }, (_, i) => ({ type: 'Role', name: `role_${i + 1}` })),
-        ...Array.from({ length: 6 }, (_, i) => ({ type: 'Channel', channelTypes: Constants.TextBasedChannelTypes, name: `channel_${i + 1}` })),
+        ...Array.from({ length: 6 }, (_, i) => ({ type: 'Channel', name: `channel_${i + 1}`, channelTypes: Constants.TextBasedChannelTypes })),
         ...Array.from({ length: 6 }, (_, i) => ({ type: 'User', name: `member_${i + 1}` }))
       ]
     },
@@ -96,128 +223,6 @@ module.exports = {
   /**@this GuildInteraction*/
   run: async function (lang) {
     lang.__boundArgs__[0].backupPath += `.${this.options.getSubcommand().replace(/_./g, e => e[1].toUpperCase())}`;
-
-    switch (this.options.getSubcommand()) {
-      case 'toggle_module': {
-        const
-          module = this.options.getString('module'),
-          setting = this.guild.db[module]?.enable;
-
-        await this.client.db.update('guildSettings', `${this.guild.id}.${module}.enable`, !setting);
-        return this.editReply(lang('success', { name: module, state: lang(setting ? 'global.disabled' : 'global.enabled') }));
-      }
-
-      case 'toggle_command': {
-        const
-          command = this.options.getString('command'),
-          commandData = this.guild.db.commandSettings?.[command]?.disabled || {},
-          { roles = [], channels = [], users = [] } = commandData,
-          count = { enabled: { channels: 0, users: 0, roles: 0 }, disabled: { channels: 0, users: 0, roles: 0 } };
-
-        if (!getCmds(this.client).includes(command)) return this.editReply(lang('notFound'));
-
-        if (this.options.getBoolean('get')) {
-          const fields = [['roles', roles], ['channels', channels], ['users', users]].filter(([, e]) => e?.length).map(([k, v]) => ({
-            name: lang(k),
-            value: v.includes('*') ? lang('list.all') : v.map(e => {
-              if (k == 'roles') return `<@&${e}>`;
-              return (k == 'channels' ? '<#' : '<@') + `${e}>`;
-            }).join(', '),
-            inline: false
-          }));
-
-          const embed = new EmbedBuilder({
-            title: lang('list.embedTitle', command),
-            color: Colors.White,
-            ...(fields.length ? { fields } : { description: lang('list.embedDescription') }),
-          });
-
-          return this.editReply({ embeds: [embed] });
-        }
-
-        if (this.options.data[0].options.length == (this.options.data[0].options.some(e => e.name == 'get') ? 2 : 1)) {
-          await this.client.db.update('guildSettings', `${this.guild.id}.commandSettings.${command}.disabled.users`, users.includes('*') ? users.filter(e => e != '*') : ['*', ...users]);
-          return this.editReply(lang(users.includes('*') ? 'enabled' : 'disabled', command));
-        }
-
-        if (users.includes('*')) return this.editReply(lang('isDisabled', { command, id: this.command.id }));
-
-        for (const [typeIndex, typeFilter] of ['role', 'member', 'channel'].entries()) {
-          const ids = [...new Set(this.options.data[0].options.filter(e => e.name.includes(typeFilter)).map(e => e.value))];
-
-          let type = 'roles';
-          if (typeIndex == 1) type = 'users';
-          else if (typeIndex == 2) type = 'channels';
-
-          for (const id of ids) {
-            if (commandData[type]?.includes(id)) {
-              commandData[type] = commandData[type].filter(e => e !== id);
-              count.enabled[type]++;
-              continue;
-            }
-
-            commandData[type] = [...(commandData[type] || []), id];
-            count.disabled[type]++;
-          }
-        }
-
-        const embed = new EmbedBuilder({
-          title: lang('embedTitle', command),
-          description: lang('embedDescription', this.command.id),
-          fields: Object.entries(count).filter(([, v]) => Object.values(v).find(Boolean)).map(([k, v]) => ({
-            name: lang(`embed.${k}`),
-            value: Object.entries(v).filter(([, e]) => e).map(([k, v]) => lang(k) + `: **${v}**`).join('\n'),
-            inline: true
-          })),
-          color: Colors.White
-        });
-
-        await this.client.db.update('guildSettings', `${this.guild.id}.commandSettings.${command}.disabled`, commandData);
-        return this.editReply({ embeds: [embed] });
-      }
-
-      case 'language': {
-        const
-          language = this.options.getString('language'),
-          /**@type {lang}*/
-          newLang = this.client.i18n.__.bind(this.client.i18n, { locale: this.client.i18n.availableLocales.has(language) ? language : this.client.i18n.config.defaultLocale }),
-          { category, name } = this.client.slashCommands.get(this.commandName),
-          embed = new EmbedBuilder({
-            title: newLang(`commands.${category.toLowerCase()}.${name}.language.embedTitle`),
-            description: newLang(`commands.${category.toLowerCase()}.${name}.language.embedDescription`, newLang('global.languageName')),
-            color: Colors.Green
-          });
-
-        await this.client.db.update('guildSettings', `${this.guild.id}.config.lang`, language);
-        return this.editReply({ embeds: [embed] });
-      }
-
-      case 'serverbackup': {
-        await this.client.db.update('guildSettings', 'serverbackup.allowedToLoad', parseInt(backup.get(this.options.getString('allowed_to_load'))));
-        return this.editReply(lang('success'));
-      }
-
-      case 'autopublish': {
-        const enabled = this.options.getBoolean('enabled');
-        await this.client.db.update('guildSettings', `${this.guild.id}.config.autopublish`, enabled);
-        return this.customReply(lang('success', lang(`global.${enabled ? 'enabled' : 'disabled'}`)));
-      }
-
-      case 'logger': {
-        const
-          channel = (this.options.getChannel('channel') ?? this.guild.channels.cache.get(this.guild.db.config.logger?.[action]?.channel))?.id ?? this.channel,
-          action = this.options.getString('action'),
-          enabled = this.options.getBoolean('enabled') ?? (action == 'all' ? null : !this.guild.db.config.logger?.[action]?.enabled);
-
-        if (!channel) return this.editReply(lang('noChannel'));
-        if (action == 'all') {
-          if (enabled == null) return this.editReply(lang('noEnabled'));
-          for (const action of loggerActionTypes) await this.client.db.update('guildSettings', `${this.guild.id}.config.logger.${action}`, { channel, enabled });
-        }
-
-        await this.client.db.update('guildSettings', `${this.guild.id}.config.logger.${action}`, { channel, enabled });
-        return this.editReply(lang(enabled ? 'enabled' : 'disabled', { channel, action: lang(`actions.${action}`) }));
-      }
-    }
+    return setupMainFunctions[this.options.getSubcommand()].call(this, lang);
   }
 };
--- Commands/Fun/birthday.js
+++ Commands/Fun/birthday.js
@@ -1,7 +1,82 @@
 const
   { EmbedBuilder, Colors } = require('discord.js'),
   { getTargetMember, getAge } = require('../../Utils'),
-  currentYear = new Date().getFullYear();
+  currentYear = new Date().getFullYear(),
+
+  birthdayMainFunctions = {
+    /**@this GuildInteraction @param {lang}lang*/
+    set: async function set(lang) {
+      await this.client.db.update('userSettings', `${this.user.id}.birthday`, `${this.options.getInteger('year')}/${String(this.options.getInteger('month')).padStart(2, '0')}/${String(this.options.getInteger('day')).padStart(2, '0')}`);
+      return this.editReply(lang('saved')); //Todo: maybe add "your birthday is in <d> days"
+    },
+    
+    /**@this GuildInteraction @param {lang}lang*/
+    remove: async function remove(lang) {
+      await this.client.db.delete('userSettings', `${this.user.id}.birthday`);
+      return this.editReply(lang('removed'));
+    },
+
+    /**@this GuildInteraction @param {lang}lang*/
+    get: async function get(lang) {
+      const
+        target = getTargetMember.call(this),
+        doNotHide = this.options.getBoolean('do_not_hide'),
+        embed = new EmbedBuilder({
+          color: Colors.Blurple,
+          footer: {
+            text: this.user.tag,
+            iconURL: this.member.displayAvatarURL()
+          }
+        });
+
+      if (target) {
+        embed.data.title = lang('getUser.embedTitle', target.user.customTag);
+
+        const data = target.user.db.birthday?.split('/');
+        if (data) {
+          const age = getAge(data) + 1;
+          embed.data.description = lang('getUser.date', { user: target.customName, month: lang(`months.${data[1]}`), day: data[2] });
+          if (age < currentYear) embed.data.description += lang('getUser.newAge', age);
+        }
+        else embed.data.description = lang('getUser.notFound', target.customName);
+      }
+      else {
+        embed.data.title = lang('getAll.embedTitle');
+
+        const
+          guildMembers = (await this.guild.members.fetch()).map(e => e.id),
+          currentTime = new Date().getTime(),
+          data = Object.entries(this.client.db.get('userSettings') || {})
+            .reduce((acc, [k, { birthday } = {}]) => {
+              if (birthday && guildMembers.includes(k)) acc.push([k, ...birthday.split('/')]);
+              return acc;
+            }, [])
+            .sort(([, , month1, day1], [, , month2, day2]) => {
+              const time = [new Date(currentYear, month1 - 1, day1), new Date(currentYear, month2 - 1, day2)];
+              if (time[0] < currentTime) time[0].setFullYear(currentYear + 1, month1 - 1, day1);
+              if (time[1] < currentTime) time[1].setFullYear(currentYear + 1, month2 - 1, day2);
+
+              return time[0] - time[1];
+            })
+            .slice(0, 10);
+
+        embed.data.description = data.length ? '' : lang('getAll.notFound');
+        for (const [id, year, month, day] of data) {
+          const
+            date = lang('getAll.date', { month: lang(`months.${month}`), day }),
+            age = getAge([year, month, day]) + 1,
+            msg = `> <@${id}>${age < currentYear ? ' (' + age + ')' : ''}\n`;
+
+          embed.data.description += embed.data.description.includes(date) ? msg : `\n${date}${msg}`;
+        }
+      }
+
+      if (!doNotHide) return this.editReply({ embeds: [embed] });
+
+      await this.channel.send({ embeds: [embed] });
+      return this.editReply(lang('global.messageSent'));
+    }
+  };
 
 /**@type {command}*/
 module.exports = {
@@ -51,77 +126,6 @@ module.exports = {
 
   /**@this GuildInteraction*/
   run: async function (lang) {
-    const
-      target = getTargetMember.call(this),
-      doNotHide = this.options.getBoolean('do_not_hide');
-
-    switch (this.options.getSubcommand()) {
-      case 'set': {
-        await this.client.db.update('userSettings', `${this.user.id}.birthday`, `${this.options.getInteger('year')}/${String(this.options.getInteger('month')).padStart(2, '0')}/${String(this.options.getInteger('day')).padStart(2, '0')}`);
-        return this.editReply(lang('saved')); //Todo: maybe add "your birthday is in <d> days"
-      }
-
-      case 'remove': {
-        await this.client.db.delete('userSettings', `${this.user.id}.birthday`);
-        return this.editReply(lang('removed'));
-      }
-
-      case 'get': {
-        const embed = new EmbedBuilder({
-          color: Colors.Blurple,
-          footer: {
-            text: this.user.tag,
-            iconURL: this.member.displayAvatarURL()
-          }
-        });
-
-        if (target) {
-          embed.data.title = lang('getUser.embedTitle', target.user.customTag);
-
-          const data = target.user.db.birthday?.split('/');
-          if (data) {
-            const age = getAge(data) + 1;
-            embed.data.description = lang('getUser.date', { user: target.customName, month: lang(`months.${data[1]}`), day: data[2] });
-            if (age < currentYear) embed.data.description += lang('getUser.newAge', age);
-          }
-          else embed.data.description = lang('getUser.notFound', target.customName);
-        }
-        else {
-          embed.data.title = lang('getAll.embedTitle');
-
-          const
-            guildMembers = (await this.guild.members.fetch()).map(e => e.id),
-            currentTime = new Date().getTime(),
-            data = Object.entries(this.client.db.get('userSettings') || {})
-              .reduce((acc, [k, { birthday } = {}]) => {
-                if (birthday && guildMembers.includes(k)) acc.push([k, ...birthday.split('/')]);
-                return acc;
-              }, [])
-              .sort(([, , month1, day1], [, , month2, day2]) => {
-                const time = [new Date(currentYear, month1 - 1, day1), new Date(currentYear, month2 - 1, day2)];
-                if (time[0] < currentTime) time[0].setFullYear(currentYear + 1, month1 - 1, day1);
-                if (time[1] < currentTime) time[1].setFullYear(currentYear + 1, month2 - 1, day2);
-
-                return time[0] - time[1];
-              })
-              .slice(0, 10);
-
-          embed.data.description = data.length ? '' : lang('getAll.notFound');
-          for (const [id, year, month, day] of data) {
-            const
-              date = lang('getAll.date', { month: lang(`months.${month}`), day }),
-              age = getAge([year, month, day]) + 1,
-              msg = `> <@${id}>${age < currentYear ? ' (' + age + ')' : ''}\n`;
-
-            embed.data.description += embed.data.description.includes(date) ? msg : `\n${date}${msg}`;
-          }
-        }
-
-        if (!doNotHide) return this.editReply({ embeds: [embed] });
-
-        await this.channel.send({ embeds: [embed] });
-        return this.editReply(lang('global.messageSent'));
-      }
-    }
+    return birthdayMainFunctions[this.options.getSubcommand()].call(this, lang);
   }
 };
