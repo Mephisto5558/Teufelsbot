09da78c304d055ce82afa2db1ac070279e4ee957
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-01-09 11:23:56 +0100

    removed hardcoded languages

--- Website/DashboardSettings/config/lang.js
+++ Website/DashboardSettings/config/lang.js
@@ -3,6 +3,6 @@ module.exports = {
   name: 'Language',
   description: 'The language of the bot',
   /**@this WebServer*/
-  type: function () { return this.formTypes.select({ English: 'en', German: 'de' }); },
+  type: function () { return this.formTypes.select([...this.client.i18n.availableLocales.keys()].map(k => ({ [this.client.i18n.__({ locale: k }, 'global.languageName')]: k }))); },
   position: 1
 };
