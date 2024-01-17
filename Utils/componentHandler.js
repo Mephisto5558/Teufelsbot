56e9282b5603e35c9033601b7732e7b40c2b8460
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-01-17 16:55:43 +0100

    removed some duplicated parts

--- Utils/componentHandler.js
+++ Utils/componentHandler.js
@@ -7,16 +7,22 @@ const
 module.exports = async function MessageComponentHandler(lang) {
   const
     [feature, id, mode, data, ...args] = this.customId.split('.'),
+    /**@type {number}*/
     cooldown = cooldowns.call(this, { name: `buttonPressEvent.${this.message.id}`, cooldowns: { user: 1000 } }),
     command = this.client.slashCommands.get(feature) ?? this.client.prefixCommands.get(feature) ?? { name: feature },
-    errorEmbed = new EmbedBuilder({ color: Colors.Red }),
     disabledList = this.guild?.db.commandSettings?.[command.aliasOf || command.name]?.disabled || {};
 
-  if (disabledList.members?.includes(this.user.id)) return this.reply({ embeds: [errorEmbed.setDescription(lang('notAllowed.member'))], ephemeral: true });
-  if (disabledList.channels?.includes(this.channel.id)) return this.reply({ embeds: [errorEmbed.setDescription(lang('notAllowed.channel'))], ephemeral: true });
-  if (disabledList.roles && this.member.roles.cache.some(e => disabledList.roles.includes(e.id))) return this.reply({ embeds: [errorEmbed.setDescription(lang('notAllowed.role'))], ephemeral: true });
-  if (command.category?.toLowerCase() == 'nsfw' && !this.channel.nsfw) return this.reply({ embeds: [errorEmbed.setDescription(lang('nsfw'))], ephemeral: true });
-  if (cooldown) return this.reply({ content: lang('events.interaction.buttonOnCooldown', cooldown), ephemeral: true });
+  let err;
+  if (disabledList.members?.includes(this.user.id)) err = 'notAllowed.member';
+  else if (disabledList.channels?.includes(this.channel.id)) err = 'notAllowed.channel';
+  else if (disabledList.roles && this.member.roles.cache.some(e => disabledList.roles.includes(e.id))) err = 'notAllowed.role';
+  else if (command.category?.toLowerCase() == 'nsfw' && !this.channel.nsfw) err = 'nsfw';
+  else if (cooldown) err = 'events.interaction.buttonOnCooldown';
+
+  if (err) {
+    const embed = new EmbedBuilder({ description: lang(err, cooldown), color: Colors.Red });
+    return this.reply({ embeds: [embed], ephemeral: true });
+  }
 
   if (handlers[feature]) return handlers[feature].call(this, lang, id, mode, data, args);
 };
