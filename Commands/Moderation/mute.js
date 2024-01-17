c108cf329a080f1c8dfce7aba59bafef27fb587f
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-01-17 16:57:12 +0100

    merged ban, kick and mute command

--- Commands/Moderation/mute.js
+++ Commands/Moderation/mute.js
@@ -1,7 +1,4 @@
-const
-  { EmbedBuilder, Colors, PermissionFlagsBits, DiscordAPIError } = require('discord.js'),
-  { getMilliseconds } = require('better-ms'),
-  { timeValidator, checkTargetManageable } = require('../../Utils');
+const { timeValidator } = require('../../Utils');
 
 /**@type {command}*/
 module.exports = {
@@ -32,49 +29,5 @@ module.exports = {
   ],
 
   /**@this GuildInteraction*/
-  run: async function (lang) {
-    const
-      target = this.options.getMember('target'),
-      reason = this.options.getString('reason'),
-      duration = getMilliseconds(this.options.getString('duration'))?.limit?.({ min: 6e4, max: 2419e5 }),
-      date = new Date();
-
-    let noMsg;
-
-    if (target.id == this.client.user.id) return this.editReply('1984');
-
-    const err = checkTargetManageable.call(this, target);
-    if (err) return this.editReply(lang(err));
-    if (target.permissions.has(PermissionFlagsBits.Administrator)) return this.editReply(lang('targetIsAdmin'));
-    if (!duration || typeof duration == 'string') return this.editReply(lang('invalidDuration'));
-
-    date.setTime(date.getTime() + duration);
-
-    try { await target.disableCommunicationUntil(date.getTime(), `${reason} | ${lang('global.modReason', { command: this.commandName, user: this.user.tag })}`); }
-    catch (err) {
-      if (!(err instanceof DiscordAPIError)) throw err;
-      return this.editReply(lang('error', err.message));
-    }
-
-    const embed = new EmbedBuilder({
-      title: lang('dmEmbedTitle'),
-      description: lang('dmEmbedDescription', {
-        guild: this.guild.name, mod: this.user.tag, reason,
-        time: Math.round(target.communicationDisabledUntilTimestamp / 1000)
-      }),
-      color: Colors.Red
-    });
-
-    try { await target.send({ embeds: [embed] }); }
-    catch (err) {
-      if (err.code != 50007) throw err; // "Cannot send messages to this user"
-      noMsg = true;
-    }
-
-    embed.data.title = lang('infoEmbedTitle');
-    embed.data.description = lang('infoEmbedDescription', { user: target.user.tag, reason, time: Math.round(target.communicationDisabledUntilTimestamp / 1000) });
-    if (noMsg) embed.data.description += lang('noDM');
-
-    return this.editReply({ embeds: [embed] });
-  }
+  run: require('../../Utils/combinedCommands').ban_kick_mute
 };
