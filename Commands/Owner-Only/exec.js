9e01859c43301b4895253c795d643e59f55f0e4e
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-01-17 16:54:30 +0100

    added exec command for sh code execution

---
+++ Commands/Owner-Only/exec.js
@@ -0,0 +1,20 @@
+const exec = require('util').promisify(require('child_process').exec);
+
+/**@type {command}*/
+module.exports = {
+  name: 'exec',
+  slashCommand: false,
+  prefixCommand: true,
+  dmPermission: true,
+  beta: true,
+
+  /**@this Message*/
+  run: async function (lang) {
+    const { stdout = lang('global.none'), stderr } = await exec(this.content);
+
+    let msg = lang('finished', { code: this.content, stdout });
+    if (stderr) msg += lang('error', stderr);
+
+    return this.customReply(msg);
+  }
+};
