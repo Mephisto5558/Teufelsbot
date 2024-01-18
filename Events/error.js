406aa01148a70e8d20f6fe3257fe929b8f2a5355
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-01-18 11:25:12 +0100

    fixed error

--- Events/error.js
+++ Events/error.js
@@ -2,5 +2,5 @@ const { errorHandler } = require('../Utils');
 
 /**@this Client @param {Error}err @returns {Promise<void>}*/
 module.exports = function error(err, client) {
-  return errorHandler.call(this, client ? err : null); //sometimes there is no err
+  return errorHandler.call(this, client ? err : {}); //sometimes there is no err
 };
