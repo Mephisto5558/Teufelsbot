c613d8597e8b8c8ae57c44d58a1e6631f6169d4d
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-01-18 11:00:13 +0100

    fixed code and allow `\n`

--- Commands/Useful/passwordgenerator.js
+++ Commands/Useful/passwordgenerator.js
@@ -28,11 +28,11 @@ module.exports = {
     const
       count = this.options?.getInteger('count') || 1,
       exclude = this.options?.getString('exclude_chars') || '',
-      include = this.options?.getString('include_chars') || '';
+      include = this.options?.getString('include_chars') || '',
+      length = this.options?.getInteger('length') || 12;
 
     let
-      passwordList = '```',
-      length = this.options?.getInteger('length') || 12,
+      passwordList = '',
       charset = [...new Set(defaultCharset //new Set() makes sure there are no duplicate entries
         .filter(char => !exclude.includes(char)) //Remove exclude chars from the charset
         .concat(Array.from(include)) //Add include chars to the charset
@@ -42,22 +42,21 @@ module.exports = {
 
     for (let i = 0; i < count; i++) {
       let lastRandomChar;
-      if (passwordList.length + length > 1743) break;
+      if (passwordList.length + length > 1740) break;
+
+      passwordList += '```';
 
       for (let i = 0; i < length; i++) {
         const randomChar = charset.split('').filter(e => e != lastRandomChar).random(); //Filters the last selected entry out and selects a list entry based on a secure random number generator. Defined in Utils/prototypeRegisterer.js.
-        if (lastRandomChar + randomChar == '\n') { //'\n' should not appear in the list, it would break stuff
-          length++;
-          continue;
-        }
-        passwordList += randomChar; //Adds one of the chars in the charset to the password
+        if (lastRandomChar + randomChar == '\n') passwordList += '\\n'; //Escape \n so it doesn't break formatting
+        else passwordList += randomChar; //Adds one of the chars in the charset to the password
         lastRandomChar = randomChar; //Sets lastRandomChar to the last generated char
       }
-      passwordList += '```\n```';
+      passwordList += '```\n';
     }
 
     if (charset.length > 100) charset = charset.substring(0, 97) + '...'; //Limits the *displayed* charset length
 
-    return this.editReply(lang('success', { passwords: passwordList.slice(0, -4), charset }));
+    return this.editReply(lang('success', { passwords: passwordList, charset }));
   }
 };
