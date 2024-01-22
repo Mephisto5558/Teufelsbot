fe266ab6d5b3eabc62f37c34c271966a4b55f875
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-01-22 09:11:49 +0100

    added another image (I hope that URL will work xD)

--- Commands/Fun/happy.js
+++ Commands/Fun/happy.js
@@ -2,7 +2,8 @@ const responseList = [
   'c:', 'C:', ':D', 'uwu', 'Wuiiii',
   '<:gucken:725670318164672543>',
   'https://tenor.com/view/yell-shout-excited-happy-so-happy-gif-17583147',
-  'https://tenor.com/view/happy-cat-smile-cat-gif-26239281'
+  'https://tenor.com/view/happy-cat-smile-cat-gif-26239281',
+  'https://i.redd.it/goq4k091vrdc1.jpeg'
 ];
 
 /**@type {command}*/
