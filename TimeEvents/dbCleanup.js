fc5a567d9fb79075674d6f5cf6eec02732a5a12d
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-02-04 18:59:46 +0100

    added cleanUpMinigamesDB

--- TimeEvents/dbCleanup.js
+++ TimeEvents/dbCleanup.js
@@ -12,7 +12,7 @@ function cleanupGiveawaysDB(db) {
 /**Removes all lastMentions data older than one month
  * @this Client @param {string}guildId @param {{ [userId: string]: { createdAt: Date } }}db*/
 function cleanupMentionsDB(guildId, db) {
-  if (!db || guildId == 'default') return;
+  if (!db) return;
 
   for (const [userId, { createdAt }] of Object.entries(db)) {
     if (getOneMonthAgo() < createdAt.getTime()) continue;
@@ -23,11 +23,22 @@ function cleanupMentionsDB(guildId, db) {
 /**Removes all AFK-Messages older than one month
  * @this Client @param {string}guildId @param {{ [userId: string]: { createdAt: string } }}db createdAt is in seconds, not milliseconds*/
 function cleanupAfkMessagesDB(guildId, db) {
-  if (!db || guildId == 'default') return;
+  if (!db) return;
 
   for (const [userId, { createdAt }] of Object.entries(db)) {
     if (getOneMonthAgo() < Number(createdAt) * 1000) continue;
     this.db.delete('guildSettings', `${guildId}.afkMessages.${userId}`);
+  }
+}
+
+/**Removes all AFK-Messages older than one month
+ * @this Client @param {string}guildId @param {{ [game: string]: { [userId: string]: { createdAt: string } }}}db createdAt is in seconds, not milliseconds*/
+function cleanUpMinigamesDB(guildId, db) {
+  if (!db) return;
+
+  for (const [gameId, data] of Object.entries(db)) for (const [userId, { createdAt }] of Object.entries(data)) {
+    if (getOneMonthAgo() < Number(createdAt)) continue;
+    this.db.delete('guildSettings', `${guildId}.${gameId}.${userId}`);
   }
 }
 
@@ -44,11 +55,14 @@ module.exports = {
 
     cleanupGiveawaysDB.call(this, this.db.get('giveaways'));
     for (const [guildId, guild] of Object.entries(this.db.get('guildSettings'))) {
+      if (guildId == 'default') continue;
+
       cleanupMentionsDB.call(this, guildId, guild.lastMentions);
       cleanupAfkMessagesDB.call(this, guildId, guild.afkMessages);
+      cleanUpMinigamesDB.call(this, guildId, guild.minigames);
     }
 
-    log('Cleaned lastMentions DB')('Cleaned afkMessages DB');
+    log('Cleaned lastMentions, afkMessages & minigames DB');
 
     await this.db.update('botSettings', 'lastDBCleanup', now);
     log('Finished DB cleanup');
