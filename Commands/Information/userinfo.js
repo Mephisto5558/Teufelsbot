1c9ef0d43afb3e46d51aa44717cd025ca7622a33
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-02-04 18:59:47 +0100

    sorting roles alphabetically

--- Commands/Information/userinfo.js
+++ Commands/Information/userinfo.js
@@ -43,7 +43,7 @@ module.exports = {
           { name: lang('color'), value: `[${member.displayHexColor}](https://www.color-hex.com/color/${member.displayHexColor.substring(1)})`, inline: true },
           { name: lang('createdAt'), value: `<t:${Math.round(member.user.createdTimestamp / 1000)}>`, inline: true },
           { name: lang('joinedAt'), value: `<t:${Math.round(member.joinedTimestamp / 1000)}>`, inline: true },
-          { name: lang('rolesWithPerms'), value: Array.from(member.roles.cache.values()).filter(e => e.permissions.toArray().length && e.name != '@everyone').join(', '), inline: false },
+          { name: lang('rolesWithPerms'), value: Array.from(member.roles.cache.values()).filter(e => e.permissions.toArray().length && e.name != '@everyone').sort((a, b) => a.name.localeCompare(b.name)).join(', '), inline: false },
           { name: lang('perms'), value: `\`${member.permissions.has(PermissionFlagsBits.Administrator) ? lang('admin') : permissionTranslator(member.permissions.toArray(), lang.__boundArgs__[0].locale, this.client.i18n)?.join('`, `') || lang('global.none')}\` (${member.permissions.toArray().length})`, inline: false }
         ]
       }),
