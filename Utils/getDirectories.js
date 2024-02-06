4bc7c7507c30c4a944e896a9e0a6fd15d45611a8
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-02-06 16:14:33 +0100

    corrected return type

--- Utils/getDirectories.js
+++ Utils/getDirectories.js
@@ -1,4 +1,4 @@
 const { readdir } = require('fs/promises');
 
-/**@returns {string[]} directory names*/
+/**@returns {Promise<string[]>} directory names*/
 module.exports = async path => (await readdir(path, { withFileTypes: true })).reduce((acc, e) => e.isDirectory() ? [...acc, e.name] : acc, []);
