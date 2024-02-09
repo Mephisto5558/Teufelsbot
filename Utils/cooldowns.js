f15048fedd166a192596a62e15108ce2e6d46216
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-02-09 02:36:39 +0100

    fixed issues with group-subcommand cooldows

--- Utils/cooldowns.js
+++ Utils/cooldowns.js
@@ -1,29 +1,30 @@
-const { ApplicationCommandOptionType, BaseInteraction } = require('discord.js');
+const { ApplicationCommandOptionType, ChatInputCommandInteraction } = require('discord.js');
 
 /** 
  * A wrapper for {@link cooldown}, used for subcommand(group) support.
- * @this BaseInteraction 
+ * @this ChatInputCommandInteraction 
  * @param {string}name 
  * @returns {number} current cooldown in seconds*/
 function subCommandCooldowns(name) {
   const depth = name.split('.').length - 1;
-  if (depth >= 2 || !(this instanceof BaseInteraction)) return 0;
+  if (depth >= 2 || !(this instanceof ChatInputCommandInteraction)) return 0;
 
+  let groupOptions;
   const group = this.options.getSubcommandGroup(false);
   if (group && !depth) {
-    const { cooldowns } = this.client.slashCommands.get(this.commandName)?.options?.find(e => e.name == group && e.type == ApplicationCommandOptionType.SubcommandGroup) ?? {};
-    return cooldown.call(this, `${name}.${group}`, cooldowns);
+    groupOptions = this.client.slashCommands.get(this.commandName)?.options?.find(e => e.name == group && e.type == ApplicationCommandOptionType.SubcommandGroup);
+    if (groupOptions?.cooldowns) return cooldown.call(this, `${name}.${group}`, groupOptions.cooldowns);
   }
 
   const subCmd = this.options.getSubcommand(false);
   if (!subCmd) return 0;
 
-  const { cooldowns } = (group ?? this)?.options?.find?.(e => e.name == subCmd && e.type == ApplicationCommandOptionType.Subcommand) || {};
+  const { cooldowns } = (groupOptions ?? this)?.options?.find?.(e => e.name == subCmd && e.type == ApplicationCommandOptionType.Subcommand) ?? {};
   return cooldowns ? cooldown.call(this, group ? `${name}.${group}.${subCmd}` : `${name}.${subCmd}`, cooldowns) : 0;
 }
 
 /** 
- * @this {Message | BaseInteraction}
+ * @this {Message | import('discord.js').BaseInteraction}
  * @param {string}name name of the cooldown space, eg. a command name 
  * @param {Record<string, number>}cooldowns
  * @returns {number} current cooldown in seconds*/
