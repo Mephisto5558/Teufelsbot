1eb351564f55f20672af6be6dd4056452c958dab
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-02-13 09:22:23 +0100

    better typeguarding

--- Utils/autocompleteGenerator.js
+++ Utils/autocompleteGenerator.js
@@ -1,8 +1,9 @@
 /** 
- * @this {import('discord.js').AutocompleteInteraction} 
- * @param {command<*, boolean, true>}command 
+ * @this {import('discord.js').AutocompleteInteraction}
+ * @param {command<*, boolean, true>}command
  * @param {string}locale*/
 module.exports = async function autocompleteGenerator(command, locale) {
+  /** @param {string|number}v*/
   const response = v => ({ name: this.client.i18n.__({ locale, undefinedNotFound: true }, `commands.${command.category.toLowerCase()}.${command.name}.options.${this.options?._group ? this.options._group + '.' : ''}${this.options?._subcommand ? this.options._subcommand + '.' : ''}${this.focused.name}.choices.${v}`) ?? v, value: v });
 
   /** @type {command<'both', boolean, true>}*/
@@ -10,12 +11,14 @@ module.exports = async function autocompleteGenerator(command, locale) {
   if (this.options?._group) options = options.find(e => e.name == this.options._group);
   if (this.options?._subcommand) options = options.find(e => e.name == this.options._subcommand).options;
 
-  /** @type {commandOptions}*/
+  /** @type {{autocompleteOptions: Exclude<commandOptions['autocompleteOptions'], Function>}} Excludes<> because we call autocompleteOptions below if it is a function*/
   let { autocompleteOptions } = options.find(e => e.name == this.focused.name);
   if (typeof autocompleteOptions == 'function') autocompleteOptions = await autocompleteOptions.call(this);
 
-  if (autocompleteOptions.constructor == Object) return [autocompleteOptions];
-  return typeof autocompleteOptions == 'string' ? [response(autocompleteOptions)] : autocompleteOptions
-    .filter(e => !this.focused.value || (e.toLowerCase?.() || e.value.toLowerCase()).includes(this.focused.value.toLowerCase()))
+  if (typeof autocompleteOptions == 'string') return [response(autocompleteOptions)];
+  if (Array.isArray(autocompleteOptions))  return autocompleteOptions
+    .filter(e => !this.focused.value || (typeof e == 'object' ? e.value.toLowerCase() : e.toLowerCase()).includes(this.focused.value.toLowerCase()))
     .slice(0, 25).map(e => typeof e == 'object' ? e : response(e));
+
+  return [autocompleteOptions];
 };
