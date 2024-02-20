9ecb3df1534dbf343cf1bf6d2a9b9718bda00c1b
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-02-20 08:32:14 +0100

    spread should be preferred over concat

--- Commands/Owner-Only/reload.js
+++ Commands/Owner-Only/reload.js
@@ -54,7 +54,7 @@ async function reloadCommand(command, reloadedArray) {
     this.slashCommands.set(slashFile.name, slashFile);
     reloadedArray.push(`</${slashFile.name}:${slashFile.id ?? 0}>`);
 
-    for (const alias of new Set((slashFile.aliases?.slash ?? []).concat(command.aliases?.slash))) {
+    for (const alias of new Set([...slashFile.aliases?.slash ?? [], ...command.aliases?.slash ?? []])) {
       const { id } = this.slashCommands.get(alias) ?? {};
       let cmdId;
 
