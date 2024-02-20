c02ac37bc77686948bd221a08f78e7e575503c58
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-02-20 10:28:33 +0100

    using undefined instead of null

--- Website/CustomSites/api/v1/internal/vote/list.js
+++ Website/CustomSites/api/v1/internal/vote/list.js
@@ -1,7 +1,7 @@
 /** @type {import('@mephisto5558/bot-website').customPage}*/
 module.exports = {
   run: function (res, req) {
-    const reply = this.voteSystem.getMany(Number.parseInt(req.query.amount) || null, Number.parseInt(req.query.offset ?? 0), req.query.filter, req.query.includePending == 'true', req.user?.id);
+    const reply = this.voteSystem.getMany(Number.parseInt(req.query.amount) || undefined, Number.parseInt(req.query.offset ?? 0), req.query.filter, req.query.includePending == 'true', req.user?.id);
     return res.status(reply.errorCode ?? 200).json(reply);
   }
 };
