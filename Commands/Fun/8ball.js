5ff07e1566931e58976e3dfdeb0615f7ef47a1cf
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-02-20 08:00:00 +0100

    switched to cyrb53a

--- Commands/Fun/8ball.js
+++ Commands/Fun/8ball.js
@@ -1,23 +1,23 @@
 /**
+ * https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
  * @param {string}str
  * @param {number}seed*/
-function cyrb53(str, seed = 0) { // https://github.com/bryc/code/blob/master/jshash/experimental/cyrb53.js
+function cyrb53a(str, seed = 0) {
   let
     h1 = 0xDE_AD_BE_EF ^ seed,
     h2 = 0x41_C6_CE_57 ^ seed;
 
   for (let i = 0, ch; i < str.length; i++) {
     ch = str.codePointAt(i);
-    h1 = Math.imul(h1 ^ ch, 2_654_435_761);
-    h2 = Math.imul(h2 ^ ch, 1_597_334_677);
+    h1 = Math.imul(h1 ^ ch, 0x85_EB_CA_77);
+    h2 = Math.imul(h2 ^ ch, 0xC2_B2_AE_3D);
   }
 
-  h1 = Math.imul(h1 ^ (h1 >>> 16), 2_246_822_507);
-  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3_266_489_909);
-  h2 = Math.imul(h2 ^ (h2 >>> 16), 2_246_822_507);
-  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3_266_489_909);
+  h1 ^= Math.imul(h1 ^ (h2 >>> 15), 0x73_5A_2D_97);
+  h2 ^= Math.imul(h2 ^ (h1 >>> 15), 0xCA_F6_49_A9);
+  h1 ^= h2 >>> 16; h2 ^= h1 >>> 16;
 
-  return 4_294_967_296 * (2_097_151 & h2) + (h1 >>> 0);
+  return 2_097_152 * (h2 >>> 0) + (h1 >>> 11);
 }
 
 /** @type {command<'both', false>}*/
@@ -38,6 +38,6 @@ module.exports = {
 
     const now = new Date();
     const responseList = lang.__boundThis__.localeData[lang.__boundArgs__[0].locale][`${lang.__boundArgs__[0].backupPath}.responseList`];
-    return this.customReply(responseList[cyrb53(input.toLowerCase(), Number.parseInt(this.user.id) ^ cyrb53(`${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`)) % responseList.length]);
+    return this.customReply(responseList[cyrb53a(input.toLowerCase(), Number.parseInt(this.user.id) ^ cyrb53a(`${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`)) % responseList.length]);
   }
 };
