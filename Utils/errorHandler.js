d98edd99ac487e05b232b8c86ddd6998f23e4ca0
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-02-20 10:19:47 +0100

    added eslint-disable comment

--- Utils/errorHandler.js
+++ Utils/errorHandler.js
@@ -67,6 +67,7 @@ module.exports = async function errorHandler(err, message, lang) {
           if (err.code != DiscordAPIErrorCodes.CannotSendMessagesToThisUser) throw err;
         }
 
+        /* eslint-disable-next-line unicorn/no-null */
         return msg.edit({ embeds: [embed.setFooter(null).setDescription(lang('reportSuccess', json.html_url))], components: [] });
       }
       catch (err) {
