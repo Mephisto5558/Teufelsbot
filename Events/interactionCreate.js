5de7a01af6cd1b7bba5ec17957093fcf2554b67e
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-02-24 22:48:56 +0100

    simplified the code by using functions

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -1,5 +1,5 @@
 const
-  { EmbedBuilder, Colors, InteractionType } = require('discord.js'),
+  { EmbedBuilder, Colors } = require('discord.js'),
   { componentHandler, autocompleteGenerator, checkForErrors, commandExecutionWrapper } = require('../Utils');
 
 /** @this {import('discord.js').Interaction}*/
@@ -12,16 +12,16 @@ module.exports = async function interactionCreate() {
     /** @type {lang}*/
     lang = this.client.i18n.__.bBind(this.client.i18n, { locale, backupPath: 'events.command' });
 
-  if (this.type == InteractionType.MessageComponent) return componentHandler.call(this, lang);
+  if (this.isMessageComponent()) return componentHandler.call(this, lang);
 
   const command = this.client.slashCommands.get(this.commandName);
-  if (command && this.type == InteractionType.ApplicationCommandAutocomplete) return this.respond(await autocompleteGenerator.call(this, command, locale));
+  if (command && this.isAutocomplete()) return this.respond(await autocompleteGenerator.call(this, command, locale));
 
   const errorKey = await checkForErrors.call(this, command, lang);
   if (errorKey === true) return;
   else if (errorKey) return this.customReply({ embeds: [new EmbedBuilder({ description: lang(...errorKey), color: Colors.Red })], ephemeral: true });
 
-  if (this.type == InteractionType.ApplicationCommand) {
+  if (this.isCommand()) {
     if (!command.noDefer && !this.replied) await this.deferReply({ ephemeral: command.ephemeralDefer ?? false });
 
     return commandExecutionWrapper.call(this, command, 'slash', lang);
