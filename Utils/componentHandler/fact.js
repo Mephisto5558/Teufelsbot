8385068d4b4af98aac61828b70ccc5a0170326c3
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-03-02 11:13:43 +0100

    removed lang backupPath overwrite as it's unnecessary here

--- Utils/componentHandler/fact.js
+++ Utils/componentHandler/fact.js
@@ -5,8 +5,6 @@ const commandExecutionWrapper = require('../commandExecutionWrapper.js');
  * @this {import('discord.js').ButtonInteraction}
  * @param {lang}lang*/
 module.exports = function fact(lang) {
-  lang.__boundArgs__[0].backupPath = 'commands.fun.fact';
-
   this.update({ components: [] });
 
   return commandExecutionWrapper.call(this, this.client.slashCommands.get('fact'), 'component', lang);
