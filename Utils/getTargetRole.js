413d1a09f3f366067b8cc20dfa643c98dbbb8842
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-03-02 13:31:52 +0100

    fixed missing this in call

--- Utils/getTargetRole.js
+++ Utils/getTargetRole.js
@@ -5,7 +5,7 @@
  * @param {boolean?}options.returnSelf return this.member.roles.highest if nothing else has been found
  * @returns {import('discord.js').Role|undefined}
  */
-module.exports = function getTargetMember({ targetOptionName = 'target', returnSelf } = {}) {
+module.exports = function getTargetRole({ targetOptionName = 'target', returnSelf } = {}) {
   /** @type {import('discord.js').Role?}*/
   let target = this.options?.getRole(targetOptionName) ?? this.mentions?.roles.first();
   if (!target && this.content) target = this.guild.roles.cache.find(e => [e.id, e.name].some(e => [...this.args ?? [], this.content].includes(e)));
