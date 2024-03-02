d9d55fd6d3b6a3065a750d8925dfc88234696489
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-03-02 10:52:03 +0100

    moved checkForErrors to commandExecutionWrapper because of componentHander, also moved the noDefer check.

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -1,9 +1,7 @@
-const
-  { EmbedBuilder, Colors } = require('discord.js'),
-  { componentHandler, autocompleteGenerator, checkForErrors, commandExecutionWrapper } = require('../Utils');
+const { componentHandler, autocompleteGenerator, commandExecutionWrapper } = require('../Utils');
 
 /** @this {import('discord.js').Interaction}*/
-module.exports = async function interactionCreate() {
+module.exports = function interactionCreate() {
   if (this.client.settings.blacklist?.includes(this.user.id)) return;
 
   const
@@ -17,13 +15,5 @@ module.exports = async function interactionCreate() {
   const command = this.client.slashCommands.get(this.commandName);
   if (command && this.isAutocomplete()) return this.respond(autocompleteGenerator.call(this, command, locale));
 
-  const errorKey = await checkForErrors.call(this, command, lang);
-  if (errorKey === true) return;
-  else if (errorKey) return this.customReply({ embeds: [new EmbedBuilder({ description: lang(...errorKey), color: Colors.Red })], ephemeral: true });
-
-  if (this.isCommand()) {
-    if (!command.noDefer && !this.replied) await this.deferReply({ ephemeral: command.ephemeralDefer ?? false });
-
-    return commandExecutionWrapper.call(this, command, 'slash', lang);
-  }
+  if (this.isCommand()) return commandExecutionWrapper.call(this, command, 'slash', lang);
 };
