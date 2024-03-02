0c230cef0baad325c5aac6a722fc67e63e532f75
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-03-02 13:28:43 +0100

    moved setting the prefix to setup

--- Commands/Information/prefix.js
+++ Commands/Information/prefix.js
@@ -1,27 +1,13 @@
-const { PermissionFlagsBits } = require('discord.js');
-
 /** @type {command<'both'>}*/
 module.exports = {
   name: 'prefix',
   cooldowns: { user: 1000 },
   slashCommand: true,
   prefixCommand: true,
-  options: [
-    { name: 'new_prefix', type: 'String' },
-    { name: 'case_insensitive', type: 'Boolean' }
-  ],
 
-  run: async function (lang) {
-    const newPrefix = this.content ?? this.options?.getString('new_prefix');
-    const prefixCaseInsensitive = this.options?.getBoolean('case_insensitive') ?? this.guild.db.config?.prefix?.caseinsensitive ?? false;
-
-    if (newPrefix && this.member.permissions.has(PermissionFlagsBits.ManageGuild)) {
-      await this.client.db.update('guildSettings', `${this.guild.id}.config.${this.client.botType == 'dev' ? 'betaBotP' : 'p'}refix`, { prefix: newPrefix, caseinsensitive: prefixCaseInsensitive });
-      return this.customReply(lang('saved', newPrefix));
-    }
-
+  run: function (lang) {
     const currentPrefix = this.guild.db.config?.prefix?.prefix ?? this.client.defaultSettings.config.prefix;
-    if (!currentPrefix) throw new Error('No Default Prefix Found in DB');
+    const prefixCaseInsensitive = this.guild.db.config?.prefix?.caseinsensitive ?? false;
 
     return this.customReply(lang('currentPrefix', currentPrefix) + (prefixCaseInsensitive ? lang('caseInsensitive') : ''));
   }
