2a485465ccb6e641bf80427d0acfc74d203630b6
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-03-20 13:38:07 +0100

    updated dependency, improved code

--- Website/CustomSites/api/v1/git/pull.js
+++ Website/CustomSites/api/v1/git/pull.js
@@ -2,8 +2,5 @@ const { gitpull } = require('../../../../../Utils');
 
 /** @type {import('@mephisto5558/bot-website').customPage}*/
 module.exports = {
-  run: async res => {
-    const pulled = await gitpull();
-    return res?.sendStatus?.(pulled == 'OK' ? 200 : 500);
-  }
+  run: async res => res.sendStatus(await gitpull() == 'OK' ? 200 : 500)
 };
