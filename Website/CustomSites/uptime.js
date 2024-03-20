94436467fe3340d21a588360eff3b55c65936788
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-03-20 13:38:07 +0100

    updated dependency, improved code

--- Website/CustomSites/uptime.js
+++ Website/CustomSites/uptime.js
@@ -2,5 +2,5 @@
 module.exports = {
   title: 'Uptime',
 
-  run: function (res) { return res.redirect('https://hetrixtools.com/report/uptime/1035f132348b8690d60da6a6213e171c/'); }
+  run: new URL('https://hetrixtools.com/report/uptime/1035f132348b8690d60da6a6213e171c/')
 };
