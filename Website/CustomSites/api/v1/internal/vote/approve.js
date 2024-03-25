3a228325c5e92dca83ed72ef7ed368052a2cb008
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-03-25 12:13:13 +0100

    using POST requests instead of GET with query

--- Website/CustomSites/api/v1/internal/vote/approve.js
+++ Website/CustomSites/api/v1/internal/vote/approve.js
@@ -1,7 +1,9 @@
 /** @type {import('@mephisto5558/bot-website').customPage}*/
 module.exports = {
+  method: 'POST',
+
   run: async function (res, req) {
-    const reply = await this.voteSystem.approve(req.query.featureId, req.user?.id);
+    const reply = await this.voteSystem.approve(req.body.featureId, req.user?.id);
     return res.status(reply.errorCode ?? 200).json(reply);
   }
 };
