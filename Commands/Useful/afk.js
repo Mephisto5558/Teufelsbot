c3f6b719476cd661b8eb5c1e3cb638a1b9dbe5bd
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-03-27 10:09:16 +0100

    afkMessage.createdAt is now a date

--- Commands/Useful/afk.js
+++ Commands/Useful/afk.js
@@ -18,12 +18,11 @@ module.exports = {
   run: async function (lang) {
     const
       global = this.options?.getBoolean('global') ?? this.args?.[0] == 'global',
-      message = this.options?.getString('message') ?? (this.content?.slice(global ? 7 : 0, 1000) || 'AFK'),
-      createdAt = Math.round(this.createdTimestamp / 1000);
+      message = this.options?.getString('message') ?? (this.content?.slice(global ? 7 : 0, 1000) || 'AFK');
 
     /* eslint-disable-next-line unicorn/prefer-ternary */ // Line too long
-    if (global || !this.inGuild()) await this.client.db.update('userSettings', `${this.user.id}.afkMessage`, { message, createdAt });
-    else await this.client.db.update('guildSettings', `${this.guild.id}.afkMessages.${this.user.id}`, { message, createdAt });
+    if (global || !this.inGuild()) await this.client.db.update('userSettings', `${this.user.id}.afkMessage`, { message, createdAt: this.createdAt });
+    else await this.client.db.update('guildSettings', `${this.guild.id}.afkMessages.${this.user.id}`, { message, createdAt: this.createdAt });
 
     if (this.member?.moderatable && this.member.displayName.length < 26 && !this.member.nickname?.startsWith('[AFK] ')) this.member.setNickname(`[AFK] ${this.member.displayName}`);
 
