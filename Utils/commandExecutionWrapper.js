3750999ade2bd0124418d7d68e18257a242ce456
Mephisto <87113906+Mephisto5558@users.noreply.github.com>
2024-04-09 14:18:38 +0200

    updated db path

--- Utils/commandExecutionWrapper.js
+++ Utils/commandExecutionWrapper.js
@@ -27,7 +27,7 @@ module.exports = async function commandExecutionWrapper(command, commandType, la
   try {
     await command.run.call(this, cmdLang);
     if (this.client.botType != 'dev') {
-      await this.client.db.update('botSettings', `stats.${commandName}.${commandType}`, (this.client.settings.stats[commandName]?.[commandType] ?? 0) + 1);
+      await this.client.db.update('botSettings', `stats.${commandName}.${commandType}`, (this.client.settings.cmdStats[commandName]?.[commandType] ?? 0) + 1);
       await this.client.db.update('userSettings', `cmdStats.${commandName}.${commandType}`, (this.user.db.cmdStats?.[commandName]?.[commandType] ?? 0) + 1);
       if (this.inGuild()) await this.client.db.update('guildSettings', `cmdstats.${commandName}.${commandType}`, (this.guild.db.cmdStats?.[commandName]?.[commandType] ?? 0) + 1);
     }
