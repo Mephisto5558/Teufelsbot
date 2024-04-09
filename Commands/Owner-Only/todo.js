15b3cc596d8bb7b2bbf89fd59831cf6d248b44e7
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-04-09 16:13:08 +0200

    added simple todo command because I am lazy

---
+++ Commands/Owner-Only/todo.js
@@ -0,0 +1,13 @@
+/** @type {command<'prefix'>}*/
+module.exports = {
+  slashCommand: false,
+  prefixCommand: true,
+
+  run: function () {
+    return this.reply(
+      `[ToDo excel](<${this.client.config.website.domain}/todo>), `
+      + `[Voting page](<${this.client.config.website.domain}/vote>), `
+      + '[Notes in the support server](<https://discord.com/channels/1011956895529041950/1183014623507656745>)'
+    );
+  }
+};
