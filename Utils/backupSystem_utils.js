551804044f1e88fb6d7345668da03ffdbfd49e53
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-04-30 15:46:57 +0200

    moved static utils to their own file; fixes https://github.com/Mephisto5558/Teufelsbot/security/code-scanning/6340 (#383)

---
+++ Utils/backupSystem_utils.js
@@ -0,0 +1,230 @@
+const
+  { ChannelType, OverwriteType, Constants, GuildFeature, AttachmentBuilder, DiscordAPIError } = require('discord.js'),
+  fetch = require('node-fetch'),
+  DiscordAPIErrorCodes = require('../Utils/DiscordAPIErrorCodes.json');
+
+/** @param {string}url*/
+function fetchToBase64(url) {
+  if (url) return fetch(url).then(e => e.buffer()).then(e => e.toString('base64'));
+}
+
+/** @param {string}base64Str*/
+function loadFromBase64(base64Str) {
+  if (base64Str) return Buffer.from(base64Str, 'base64');
+}
+
+/**
+ * @param {import('discord.js').CategoryChannel}category
+ * @param {boolean}saveImages
+ * @param {number}maxMessagesPerChannel*/
+function fetchCategoryChildren(category, saveImages, maxMessagesPerChannel) {
+  return Promise.all(category.children.cache
+    .sort((a, b) => a.position - b.position)
+    .map(async child => {
+      if (Constants.TextBasedChannelTypes.includes(child.type) && !Constants.ThreadChannelTypes.includes(child.type))
+        return fetchTextChannelData(child, saveImages, maxMessagesPerChannel);
+
+      let channelData = { type: child.type, name: child.name, position: child.position, permissions: fetchChannelPermissions(child) };
+
+      if (Constants.VoiceBasedChannelTypes.includes(child.type)) {
+        channelData.bitrate = child.bitrate;
+        channelData.userLimit = child.userLimit;
+      }
+
+      if (child.type == ChannelType.GuildForum) {
+        channelData = {
+          ...channelData,
+          nsfw: child.nsfw,
+          topic: child.topic,
+          defaultAutoArchiveDuration: child.defaultAutoArchiveDuration,
+          defaultForumLayout: child.defaultForumLayout,
+          defaultReactionEmoji: child.defaultReactionEmoji.name,
+          defaultSortOrder: category.defaultSortOrder,
+          defaultThreadRateLimitPerUser: category.defaultThreadRateLimitPerUser,
+          threads: await fetchChannelThreads(child, saveImages, maxMessagesPerChannel),
+          availableTags: child.availableTags.map(e => ({ name: e.name, emoji: e.emoji?.name, moderated: e.moderated }))
+        };
+      }
+      else log.warn(`BackupSystem: Unhandled Channel type "${ChannelType[child.type] ?? child.type}"!`);
+
+      return channelData;
+    }));
+}
+
+/**
+ * @param {import('discord.js').GuildTextBasedChannel}channel
+ * @param {boolean}saveImages
+ * @param {number}maxMessagesPerChannel*/
+async function fetchChannelMessages(channel, saveImages, maxMessagesPerChannel = 10) {
+  const messages = await channel.messages.fetch({ limit: Number.isNaN(Number.parseInt(maxMessagesPerChannel)) ? 10 : maxMessagesPerChannel.limit({ min: 1, max: 100 }) });
+
+  return Promise.all(
+    messages.filter(e => e.author).map(async e => ({
+      username: e.author.username,
+      avatar: e.author.avatarURL(),
+      content: e.cleanContent,
+      embeds: e.embeds?.map(e => e.data),
+      files: await fetchMessageAttachments(e, saveImages),
+      pinned: e.pinned,
+      createdAt: e.createdAt.toISOString()
+    }))
+  );
+}
+
+/** @param {import('discord.js').GuildChannel}channel*/
+function fetchChannelPermissions(channel) {
+  return channel.permissionOverwrites.cache
+    .filter(e => e.type == OverwriteType.Role)
+
+    .map(e => {
+      const role = channel.guild.roles.cache.get(e.id);
+      if (!role) return undefined; /* eslint-disable-line unicorn/no-useless-undefined -- prevents `eslint/array-callback-return`*/
+
+      return {
+        name: role.name,
+        allow: e.allow.bitfield.toString(),
+        deny: e.deny.bitfield.toString()
+      };
+    });
+}
+
+/**
+ * @param {import('discord.js').GuildChannel}channel
+ * @param {boolean}saveImages
+ * @param {number}maxMessagesPerChannel*/
+async function fetchChannelThreads(channel, saveImages, maxMessagesPerChannel) {
+  const threads = (await channel.threads?.fetch())?.threads;
+
+  return threads?.map(async e => ({
+    type: e.type,
+    name: e.name,
+    archived: e.archived,
+    autoArchiveDuration: e.autoArchiveDuration,
+    locked: e.locked,
+    rateLimitPerUser: e.rateLimitPerUser,
+    messages: await fetchChannelMessages(e, saveImages, maxMessagesPerChannel).catch(err => { if (!(err instanceof DiscordAPIError)) throw err; })
+  })) ?? [];
+}
+
+/**
+ * @param {import('discord.js').Message}message
+ * @param {boolean}saveImages */
+async function fetchMessageAttachments(message, saveImages) {
+  const imageExtensions = new Set(['png', 'jpg', 'jpeg', 'jpe', 'jif', 'jfif', 'jfi']);
+  return (await Promise.all(message.attachments.map(async ({ name, url }) => ({
+    name, attachment: saveImages && imageExtensions.has(url) ? await fetchToBase64(url) : url
+  })))).filter(e => e.attachment);
+}
+
+/**
+ * @param {import('discord.js').GuildChannel}channel
+ * @param {boolean}saveImages
+ * @param {number}maxMessagesPerChannel*/
+async function fetchTextChannelData(channel, saveImages, maxMessagesPerChannel) {
+  return {
+    type: channel.type,
+    name: channel.name,
+    nsfw: channel.nsfw,
+    isNews: channel.type == ChannelType.GuildAnnouncement,
+    rateLimitPerUser: channel.type == ChannelType.GuildText ? channel.rateLimitPerUser : undefined,
+    topic: channel.topic,
+    permissions: fetchChannelPermissions(channel),
+    messages: await fetchChannelMessages(channel, saveImages, maxMessagesPerChannel).catch(err => { if (!(err instanceof DiscordAPIError)) throw err; }),
+    threads: await fetchChannelThreads(channel, saveImages, maxMessagesPerChannel)
+  };
+}
+
+/**
+ * @param {import('discord.js').GuildChannel}channel
+ * @param {import('discord.js').Guild}guild
+ * @param {string}category
+ * @param {number}maxMessagesPerChannel
+ * @param {import('discord.js').APIAllowedMentions}allowedMentions*/
+async function loadChannel(channel, guild, category, maxMessagesPerChannel, allowedMentions) {
+  const createOptions = {
+    name: channel.name,
+    type: channel.type == 5 && !guild.features.includes(GuildFeature.Community) ? 0 : channel.type,
+    parent: category,
+    permissionOverwrites: channel.permissions.reduce((acc, e) => {
+      const role = guild.roles.cache.find(e2 => e2.name == e.name);
+      if (role) acc.push({ id: role.id, allow: BigInt(e.allow), deny: BigInt(e.deny) });
+      return acc;
+    }, [])
+  };
+
+  if (Constants.TextBasedChannelTypes.includes(channel.type) && !Constants.ThreadChannelTypes.includes(channel.type)) {
+    createOptions.topic = channel.topic;
+    createOptions.nsfw = channel.nsfw;
+    createOptions.rateLimitPerUser = channel.rateLimitPerUser;
+  }
+  else if (Constants.VoiceBasedChannelTypes.includes(channel.type)) {
+    createOptions.bitrate = channel.bitrate > guild.maximumBitrate ? guild.maximumBitrate : channel.bitrate;
+    createOptions.userLimit = channel.userLimit;
+  }
+
+  const newChannel = await guild.channels.create(createOptions);
+  if (Constants.TextBasedChannelTypes.includes(channel.type)) {
+    let webhook;
+    if (channel.messages.length > 0) {
+      try { webhook = await loadChannelMessages(newChannel, channel.messages, undefined, maxMessagesPerChannel, allowedMentions); }
+      catch (err) {
+        if (!(err instanceof DiscordAPIError)) throw err;
+      }
+    }
+
+    for (const threadData of channel.threads) {
+      const thread = await newChannel.threads.create({ name: threadData.name, autoArchiveDuration: threadData.autoArchiveDuration });
+      if (webhook) await loadChannelMessages(thread, threadData.messages, webhook, maxMessagesPerChannel, allowedMentions);
+    }
+  }
+
+  return newChannel;
+}
+
+/**
+ * @param {import('discord.js').GuildTextBasedChannel}channel
+ * @param {Message[]}messages
+ * @param {import('discord.js').Webhook?}webhook
+ * @param {number}maxMessagesPerChannel
+ * @param {import('discord.js').APIAllowedMentions}allowedMentions*/
+async function loadChannelMessages(channel, messages, webhook, maxMessagesPerChannel, allowedMentions) {
+  try { webhook ??= await channel.createWebhook({ name: 'MessagesBackup', avatar: channel.client.user.displayAvatarURL() }); }
+  catch (err) {
+    if (![DiscordAPIErrorCodes.MaximumNumberOfWebhooksReached, DiscordAPIErrorCodes.MaximumNumberOfWebhooksPerGuildReached].includes(err.code))
+      throw err;
+  }
+
+  if (!webhook) return;
+
+  for (const msg of messages.filter(e => e.content.length > 0 || e.embeds.length > 0 || e.files.length > 0).reverse().slice(-maxMessagesPerChannel)) {
+    try {
+      const sentMsg = await webhook.send({
+        content: msg.content.length ? msg.content : undefined,
+        username: msg.username,
+        avatarURL: msg.avatar,
+        embeds: msg.embeds,
+        files: msg.files.map(e => new AttachmentBuilder(e.attachment, { name: e.name })),
+        allowedMentions,
+        threadId: channel.isThread() ? channel.id : undefined
+      });
+
+      if (msg.pinned && sentMsg.pinnable && (await channel.messages.fetchPinned()).size < 50) await sentMsg.pin();
+    }
+    catch (err) { log.error('Backup load error:', err); }
+  }
+
+  return webhook;
+}
+
+module.exports = {
+  fetchToBase64,
+  fetchCategoryChildren,
+  fetchChannelMessages,
+  fetchChannelPermissions,
+  fetchChannelThreads,
+  fetchMessageAttachments,
+  fetchTextChannelData,
+  loadChannel,
+  loadChannelMessages,
+  loadFromBase64
+};
