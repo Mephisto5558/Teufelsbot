e9564f99941d6675bc04809482872973e7dadc73
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-05-24 10:48:10 +0200

    improved readability by splitting into multiple functions

--- Utils/prototypeRegisterer/message_runMessages.js
+++ Utils/prototypeRegisterer/message_runMessages.js
@@ -2,56 +2,53 @@ const
   { AllowedMentionsTypes } = require('discord.js'),
   cooldowns = require('../cooldowns.js');
 
-/** @type {Message['runMessages']}*/
-module.exports = async function runMessages() {
-  const
-    { afkMessages = {}, triggers = {}, counting: { [this.channel.id]: countingData } = {} } = this.guild.db,
-    triggerList = Object.values(triggers).filter(e => this.originalContent?.toLowerCase()?.includes(e.trigger.toLowerCase())).slice(0, 3);
+/** @this {Message}*/
+function replyToTriggers() {
+  const triggerList = Object.values(this.guild.db.triggers ?? {}).filter(e => this.originalContent?.toLowerCase()?.includes(e.trigger.toLowerCase())).slice(0, 3);
+  if (!triggerList.length || cooldowns.call(this, 'triggers', { channel: 1e4 })) return;
 
-  if (this.client.botType != 'dev' && triggerList.length && !cooldowns.call(this, 'triggers', { channel: 1e4 }))
-    for (const trigger of triggerList) this.customReply(trigger.response);
-  else if (this.originalContent.includes(this.client.user.id) && !cooldowns.call(this, 'botMentionReaction', { user: 5000 }))
-    this.react('👀');
+  for (const trigger of triggerList) this.customReply(trigger.response);
+}
 
-  if (this.client.botType == 'dev') return this;
+/** @this {Message}*/
+async function handleCounting() {
+  const countingData = this.guild.db.counting[this.channel.id];
+  if (!countingData) return;
 
-  if (countingData && Number(this.originalContent)) {
-    if (countingData.lastNumber + 1 == this.originalContent && countingData.lastAuthor != this.user.id) {
-      await this.guild.updateDB(`counting.${this.channel.id}`, { lastNumber: countingData.lastNumber + 1, lastAuthor: this.user.id });
-      this.react('✅');
-    }
-    else {
-      this.react('❌');
-
-      if (countingData.lastNumber != 0) {
-        await this.guild.updateDB(`counting.${this.channel.id}`, { lastNumber: 0 });
-        this.reply(
-          this.client.i18n.__({ locale: this.guild.localeCode }, 'events.message.counting.error', countingData.lastNumber)
-          + this.client.i18n.__(
-            { locale: this.guild.localeCode },
-            countingData.lastAuthor == this.user.id ? 'events.message.counting.sameUserTwice' : 'events.message.counting.wrongNumber'
-          )
-        );
-      }
-    }
+  if (countingData.lastNumber + 1 == this.originalContent && countingData.lastAuthor != this.user.id) {
+    await this.guild.updateDB(`counting.${this.channel.id}`, { lastNumber: countingData.lastNumber + 1, lastAuthor: this.user.id });
+    return this.react('✅');
   }
 
-  if (!this.originalContent.toLowerCase().includes('--afkignore')) {
-    if (this.member.moderatable && this.member.nickname?.startsWith('[AFK] ')) this.member.setNickname(this.member.nickname.slice(6));
+  this.react('❌');
+  if (!countingData.lastNumber) return;
 
-    const { createdAt, message } = (this.user.id in afkMessages ? afkMessages[this.user.id] : this.user.db.afkMessage) ?? {};
-    if (message) {
-      await this.client.db.delete('userSettings', `${this.user.id}.afkMessage`);
-      await this.client.db.delete('guildSettings', `${this.guild.id}.afkMessages.${this.user.id}`);
-      this.customReply(this.client.i18n.__({ locale: this.guild.localeCode }, 'events.message.afkEnd', { timestamp: Math.round(createdAt.getTime() / 1000), message }));
-    }
-  }
+  await this.guild.updateDB(`counting.${this.channel.id}`, { lastNumber: 0 });
+  return this.reply(
+    this.client.i18n.__({ locale: this.guild.localeCode }, 'events.message.counting.error', countingData.lastNumber)
+    + this.client.i18n.__(
+      { locale: this.guild.localeCode },
+      countingData.lastAuthor == this.user.id ? 'events.message.counting.sameUserTwice' : 'events.message.counting.wrongNumber'
+    )
+  );
+}
 
+/** @this {Message}*/
+async function removeAfkStatus() {
+  if (this.member.moderatable && this.member.nickname?.startsWith('[AFK] ')) this.member.setNickname(this.member.nickname.slice(6));
 
-  if (cooldowns.call(this, 'afkMsg', { channel: 1e4, user: 1e4 })) return this;
+  const { createdAt, message } = this.guild.db.afkMessages?.[this.user.id] ?? this.user.db.afkMessage ?? {};
+  if (!message) return;
 
+  await this.client.db.delete('userSettings', `${this.user.id}.afkMessage`);
+  await this.client.db.delete('guildSettings', `${this.guild.id}.afkMessages.${this.user.id}`);
+  return this.customReply(this.client.i18n.__({ locale: this.guild.localeCode }, 'events.message.afkEnd', { timestamp: Math.round(createdAt.getTime() / 1000), message }));
+}
+
+/** @this {Message}*/
+function sendAfkMessages() {
   const afkMsgs = this.mentions.members.reduce((acc, e) => {
-    const { message, createdAt } = (e.id in afkMessages ? afkMessages[e.id] : e.user.db.afkMessage) ?? {};
+    const { message, createdAt } = this.guild.db.afkMessages?.[this.user.id] ?? this.user.db.afkMessage ?? {};
     if (!message || e.id == this.user.id) return acc;
 
     const afkMessage = this.client.i18n.__({ locale: this.guild.localeCode }, 'events.message.afkMsg', {
@@ -67,6 +64,20 @@ module.exports = async function runMessages() {
     return `${acc}${afkMessage}\n`;
   }, '');
 
-  if (afkMsgs.length) this.customReply({ content: afkMsgs, allowedMentions: { parse: [AllowedMentionsTypes.User] } });
+  if (afkMsgs.length) return this.customReply({ content: afkMsgs, allowedMentions: { parse: [AllowedMentionsTypes.User] } });
+}
+
+/** @type {Message['runMessages']}*/
+module.exports = function runMessages() {
+  if (this.originalContent.includes(this.client.user.id) && !cooldowns.call(this, 'botMentionReaction', { user: 5000 }))
+    this.react('👀');
+
+  if (this.client.botType == 'dev') return this;
+
+  if (this.guild.db.triggers) replyToTriggers();
+  if (Number(this.originalContent)) handleCounting();
+  if (!this.originalContent.toLowerCase().includes('--afkignore')) removeAfkStatus();
+  if (!cooldowns.call(this, 'afkMsg', { channel: 1e4, user: 1e4 })) sendAfkMessages();
+
   return this;
 };
