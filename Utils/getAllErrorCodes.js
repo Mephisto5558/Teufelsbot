a59e4c7a61534ece328d844e11c236eb1ddea29c
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-05-27 09:33:58 +0200

    actually using the newest version of the gist now, improved formatting

--- Utils/getAllErrorCodes.js
+++ Utils/getAllErrorCodes.js
@@ -4,11 +4,11 @@ const
 
 /** Writes all error code to a file*/
 module.exports = async function fetchAndProcess() {
-  const res = await fetch('https://gist.githubusercontent.com/Dziurwa14/de2498e5ee28d2089f095aa037957cbb/raw/6fc8e4f461d252e11fca3be8a63c750d783b67f5/codes.md').then(e => e.text());
+  const res = await fetch('https://gist.githubusercontent.com/Dziurwa14/de2498e5ee28d2089f095aa037957cbb/raw/codes.md').then(e => e.text());
   const codes = res.split('\n').reduce((acc, line) => {
     const [code, description] = line.slice(2, -2).split(' | ') ?? [];
     if (Number(code) && description) {
-      const name = description.split(/ +/g).reduce((nameAcc, word) => nameAcc + word[0].toUpperCase() + word.slice(1), '').replaceAll(/\(.*?\)/g, '');
+      const name = description.split(/\s+/g).reduce((nameAcc, word) => nameAcc + word[0].toUpperCase() + word.slice(1), '').replaceAll(/\(.*?\)|\..*/g, '');
       acc[name] = Number(code);
     }
     return acc;
