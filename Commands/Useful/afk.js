34599de21d7fc8fdea6c7e294af3f5cc94e917e4
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-05-27 11:46:49 +0200

    added afk get subcommand

--- Commands/Useful/afk.js
+++ Commands/Useful/afk.js
@@ -8,14 +8,54 @@ module.exports = {
   dmPermission: true,
   options: [
     {
-      name: 'message',
-      type: 'String',
-      maxLength: 1000
+      name: 'set',
+      type: 'Subcommand',
+      options: [
+        {
+          name: 'message',
+          type: 'String',
+          maxLength: 1000
+        },
+        { name: 'global', type: 'Boolean' }
+      ]
     },
-    { name: 'global', type: 'Boolean' }
+    {
+      name: 'get',
+      type: 'Subcommand',
+      options: [{ name: 'target', type: 'User' }]
+    }
   ],
 
   run: async function (lang) {
+    if (this.options?.getSubcommand() == 'get') {
+      const target = this.options.getMember('target');
+      if (target) {
+        const { message, createdAt } = this.guild.db.afkMessages?.[this.user.id] ?? this.user.db.afkMessage ?? {};
+        if (message) {
+          return this.customReply(lang('events.message.afkMsg', {
+            member: target.nickname?.startsWith('[AFK] ') ? target.nickname.slice(6) : target.displayName,
+            message, timestamp: Math.round(createdAt / 1000)
+          }));
+        }
+        return this.customReply(lang('getNoneFound'));
+      }
+
+      const afkMessages = this.guild.members.cache.reduce((acc, e) => {
+        const { message, createdAt } = this.guild.db.afkMessages?.[e.user.id] ?? e.user.db.afkMessage ?? {};
+        if (message) {
+          acc.push('- ' + lang('events.message.afkMsg', {
+            member: e.nickname?.startsWith('[AFK] ') ? e.nickname.slice(6) : e.displayName,
+            message, timestamp: Math.round(createdAt / 1000)
+          }));
+        }
+
+        return acc;
+      }, []).join('\n');
+
+      if (afkMessages) return this.customReply(afkMessages);
+      return this.customReply(lang('getNoneFound'));
+    }
+
     const
       global = this.options?.getBoolean('global') ?? this.args?.[0] == 'global',
       message = this.options?.getString('message') ?? (this.content?.slice(global ? 7 : 0, 1000) || 'AFK');
