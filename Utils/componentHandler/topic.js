436eed54943a335f82c9f0d810b44a813ef4a986
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-08-09 10:01:47 +0200

    awaiting this.update() to prevent updating two times

--- Utils/componentHandler/topic.js
+++ Utils/componentHandler/topic.js
@@ -2,8 +2,7 @@
 const commandExecutionWrapper = require('../commandExecutionWrapper.js');
 
 /** @type {import('.').topic}*/
-module.exports = function topic(lang) {
-  void this.update({ components: [] });
-
+module.exports = async function topic(lang) {
+  await this.update({ components: [] });
   return commandExecutionWrapper.call(this, this.client.slashCommands.get('topic'), 'component', lang);
 };
