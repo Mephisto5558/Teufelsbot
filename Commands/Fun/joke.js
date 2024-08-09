2ab5085b122c5f0d7c7c0b28fb02f4563a0d95d4
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-08-09 10:10:23 +0200

    removed unnecessary props

--- Commands/Fun/joke.js
+++ Commands/Fun/joke.js
@@ -52,12 +52,8 @@ async function getJoke(apiList = [], type = '', blacklist = '', maxLength = 2000
   }
   catch (err) {
     if ([402, 403, 522].includes(err.status)) log.error('joke.js: ', err.response);
-    else if (err instanceof FetchError) {
-      log.error(
-        `joke.js: ${api?.url ?? JSON.stringify(api)} responded with error ${err.status ?? err.response?.status ?? err.name}, `
-        + `${err.statusText ?? err.response?.statusText ?? err.code}: ${err.response?.data.message ?? err.message}`
-      );
-    }
+    else if (err instanceof FetchError)
+      log.error(`joke.js: ${api?.url ?? JSON.stringify(api)} responded with error ${err.name} ${err.code ? ', ' + err.code : ''}: ${err.message}`);
     else throw err;
   }
 
