9ce12877af8d76abfe9377bdd71d8eb9da3e4c68
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-08-09 08:24:46 +0200

    using  `Constants.GuildTextBasedChannelTypes` instead of `[ChannelType.GuildText, ChannelType.GuildAnnouncement]`

--- Website/DashboardSettings/gatekeeper/joinChannel.js
+++ Website/DashboardSettings/gatekeeper/joinChannel.js
@@ -1,10 +1,10 @@
-const { ChannelType } = require('discord.js');
+const { Constants } = require('discord.js');
 
 /** @type {import('@mephisto5558/bot-website').dashboardSetting}*/
 module.exports = {
   id: 'joinChannel',
   name: 'Welcome Channel',
   description: 'Select the channel to send the welcome message to',
-  type: function () { return this.formTypes.channelsSelect(false, [ChannelType.GuildText, ChannelType.GuildAnnouncement]); },
+  type: function () { return this.formTypes.channelsSelect(false, Constants.GuildTextBasedChannelTypes); },
   position: 1
 };
