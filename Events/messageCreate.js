adf2511c84cf499fc1bd49304dd3a10ca258972e
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-08-14 15:22:34 +0200

    fixed lastmention detecting non-mentioning message references

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -7,7 +7,7 @@ module.exports = function messageCreate() {
   if (this.botType != 'dev' && this.inGuild()) {
     if (this.guild.db.config.autopublish && this.crosspostable) void this.crosspost();
 
-    const mentions = [this.mentions.repliedUser?.id, ...this.mentions.users.keys(), ...this.mentions.roles.flatMap(e => e.members).keys()].filter(e => e && e != this.user.id);
+    const mentions = [...this.mentions.users.keys(), ...this.mentions.roles.flatMap(e => e.members).keys()].filter(e => e != this.user.id); // repliedUser is in mentions.users
     if (mentions.length) {
       void this.guild.updateDB('lastMentions', mentions.reduce((acc, e) => (
         { ...acc, [e]: { content: this.content, url: this.url, author: this.author, channel: this.channel.id, createdAt: this.createdAt } }
