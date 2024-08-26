Mephisto5558/Teufelsbot

commit d461083c233914a09346eef6b9495d148f6bd821

fixed ESLint alerts

Events/messageDelete.js
@@ -1,12 +1,12 @@
 const { EmbedBuilder, PermissionFlagsBits, AuditLogEvent, Colors } = require('discord.js');
 
 /**
- * @this {Message<true>}
+ * @this {Message<true> | PartialMessage<true>}
  * @param {lang}lang
  * @param {string | Record<string, string>} descriptionData*/
 async function sendeMinigameDeletedEmbed(lang, descriptionData) {
   const embed = new EmbedBuilder({
-    author: { name: this.user.username, iconURL: this.member?.displayAvatarURL() },
+    author: { name: this.user?.username, iconURL: this.member?.displayAvatarURL() },
     title: lang('embedTitle'),
     description: lang('embedDescription', descriptionData),
     color: Colors.Red,
@@ -17,7 +17,7 @@ async function sendeMinigameDeletedEmbed(lang, descriptionData) {
 }
 
 /**
- * @this {Message<true>}
+ * @this {Message<true> | PartialMessage<true>}
  * @param {lang}lang*/
 function countingHandler(lang) {
   const { lastNumber } = this.guild.db.channelMinigames?.counting?.[this.channel.id] ?? {};
@@ -28,7 +28,7 @@ function countingHandler(lang) {
 }
 
 /**
- * @this {Message<true>}
+ * @this {Message<true> | PartialMessage<true>}
  * @param {lang}lang*/
 function wordchainHandler(lang) {
   const { lastWordChar } = this.guild.db.channelMinigames?.wordchain?.[this.channel.id] ?? {};
@@ -38,7 +38,7 @@ function wordchainHandler(lang) {
   return sendeMinigameDeletedEmbed.call(this, lang, this.originalContent);
 }
 
-/** @this {Message | import('discord.js').PartialMessage}*/
+/** @this {Message | PartialMessage}*/
 module.exports = async function messageDelete() {
   if (this.client.botType == 'dev' || !this.inGuild()) return;
 
@@ -61,29 +61,29 @@ module.exports = async function messageDelete() {
 
   const
     { executor, reason } = (await this.guild.fetchAuditLogs({ limit: 6, type: AuditLogEvent.MessageDelete })).entries
-      .find(e => (!this.user?.id || e.target.id == this.user.id) && e.extra.channel.id == this.channel.id && Date.now() - e.createdTimestamp < 20_000) ?? {},
+      .find(e => (!this.user || e.target.id == this.user.id) && e.extra.channel.id == this.channel.id && Date.now() - e.createdTimestamp < 20_000) ?? {},
     embed = new EmbedBuilder({
       author: executor ? { name: executor.tag, iconURL: executor.displayAvatarURL() } : undefined,
       thumbnail: this.member ? { url: this.member.displayAvatarURL({ size: 128 }) } : undefined,
       description: lang('messageDelete.embedDescription', { executor: executor ? `<@${executor.id}>` : lang('someone'), channel: this.channel.name }),
       fields: [
-        { name: lang('global.channel'), value: `<#${this.channel.id}> (\`${this.channel.id}\`)`, inline: false },
+        { name: lang('global.channel'), value: `<#${this.channel.id}> (\`${this.channel.id}\`)`, inline: true },
+        { name: lang('messageDelete.author'), value: `${this.user.tag} (\`${this.user.id}\`)`, inline: true },
         { name: lang('messageDelete.content'), value: '', inline: false }
       ],
       timestamp: Date.now(),
       color: 0x822AED
     });
 
-  if (this.originalContent) embed.data.fields[1].value += `${this.originalContent}\n`;
-  if (this.attachments.size) embed.data.fields[1].value += this.attachments.map(e => `[${e.url}](${e.name})`).join(', ') + '\n';
-  if (this.embeds.length) embed.data.fields[1].value += lang('embeds', this.embeds.length) + '\n';
-  if (this.components.length) embed.data.fields[1].value += lang('messageDelete.components', this.components.length);
+  const field = embed.data.fields.at(-1);
+  if (this.originalContent) field.value += `${this.originalContent}\n`;
+  if (this.attachments.size) field.value += this.attachments.map(e => `[${e.url}](${e.name})`).join(', ') + '\n';
+  if (this.embeds.length) field.value += lang('embeds', this.embeds.length) + '\n';
+  if (this.components.length) field.value += lang('messageDelete.components', this.components.length);
 
-  if (!embed.data.fields[1].value) embed.data.fields[1].value += lang('unknownContent');
-  else if (embed.data.fields[1].value.length > 1024) embed.data.fields[1].value = embed.data.fields[1].value.slice(0, 1021) + '...';
+  if (!field.value) field.value += lang('unknownContent');
+  else if (field.value.length > 1024) field.value = field.value.slice(0, 1021) + '...';
 
-  // We don't get the user/member if the message is not cached
-  if (this.user) embed.data.fields.push({ name: lang('messageDelete.author'), value: `${this.user.tag} (\`${this.user.id}\`)`, inline: false });
   if (executor) embed.data.fields.push({ name: lang('executor'), value: `${executor.tag} (\`${executor.id}\`)`, inline: false });
   if (reason) embed.data.fields.push({ name: lang('reason'), value: reason, inline: false });
 

Commands/Fun/trigger.js
@@ -3,7 +3,7 @@ const
 
   /**
    * @type {Record<string,
-   * (this: GuildInteraction, lang: lang, oldData: Exclude<Exclude<Database['guildSettings'][Snowflake], undefined>['triggers'], undefined>, query: string) => Promise<Message>>
+   * (this: GuildInteraction, lang: lang, oldData: NonNullable<NonNullable<Database['guildSettings'][Snowflake]>['triggers']>, query: string) => Promise<Message>>
    * }*/
   triggerMainFunctions = {
     add: async function (lang, oldData) {

Commands/Useful/afk.js
@@ -1,4 +1,4 @@
-const { AllowedMentionsTypes } = require('discord.js');
+const { AllowedMentionsTypes, Message } = require('discord.js');
 
 /** @type {command<'both', false>}*/
 module.exports = {
@@ -28,17 +28,19 @@ module.exports = {
 
   run: async function (lang) {
     if (this.options?.getSubcommand() == 'get') {
-      const target = this.options.getMember('target');
+      if (this instanceof Message) return; // typeguard
+
+      const target = this.inGuild() ? this.options.getMember('target') : this.options.getUser('target') ?? this.user;
       if (target) {
-        const { message, createdAt } = this.guild.db.afkMessages?.[this.user.id] ?? this.user.db.afkMessage ?? {};
+        const { message, createdAt } = this.guild?.db.afkMessages?.[target.id] ?? ('user' in target ? target.user : target).db.afkMessage ?? {};
         if (!message) return this.customReply(lang('getNoneFound'));
 
         return this.customReply(lang('events.message.afkMsg', {
-          member: target.nickname?.startsWith('[AFK] ') ? target.nickname.slice(6) : target.displayName,
-          message, timestamp: Math.round(createdAt / 1000)
+          member: target.displayName.replace(/^\[AFK] /, ''), message, timestamp: Math.round(createdAt / 1000)
         }));
       }
 
+      if (!this.inGuild()) return; // typeguard
       const afkMessages = this.guild.members.cache.reduce((acc, e) => {
         const { message, createdAt } = this.guild.db.afkMessages?.[e.user.id] ?? e.user.db.afkMessage ?? {};
         if (message) {

Events/messageDeleteBulk.js
@@ -1,13 +1,12 @@
 const { EmbedBuilder, PermissionFlagsBits, AuditLogEvent } = require('discord.js');
 
 /**
- * @this {import('discord.js').Collection<string, Message<true>>}
+ * @this {import('discord.js').Collection<string, Message<true> | import('discord.js').PartialMessage>}
  * @param {import('discord.js').GuildTextBasedChannel}channel*/
 module.exports = async function messageDeleteBulk(channel) {
-  const setting = channel.guild.db.config.logger?.messageDelete ?? {};
+  const setting = channel.guild.db.config.logger?.messageDelete;
 
-  /* eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- could be a partial channel, not able to type that tho*/
-  if (channel.client.botType == 'dev' || !channel.guild || !setting.enabled || !setting.channel) return;
+  if (channel.client.botType == 'dev' || !setting?.enabled) return;
 
   const channelToSend = channel.guild.channels.cache.get(setting.channel);
   if (!channelToSend || channelToSend.permissionsFor(channel.guild.members.me).missing([PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages, PermissionFlagsBits.ViewAuditLog]).length)

TimeEvents/dbCleanup.js
@@ -24,7 +24,7 @@ async function cleanupGuildsDB(guildId, db) {
  * Deletes giveaway records that concluded over a month ago
  * @this {Client}
  * @param {string}guildId
- * @param {Exclude<Exclude<Database['guildSettings'][Snowflake], undefined>['giveaway'], undefined>['giveaways'] | undefined}db*/
+ * @param {NonNullable<NonNullable<Database['guildSettings'][Snowflake]>['giveaway']>['giveaways'] | undefined}db*/
 function cleanupGiveawaysDB(guildId, db) {
   if (!db) return;
 
@@ -38,7 +38,7 @@ function cleanupGiveawaysDB(guildId, db) {
  * Removes all lastMentions data older than one month
  * @this {Client}
  * @param {string}guildId
- * @param {Exclude<Database['guildSettings'][''], undefined>['lastMentions']}db*/
+ * @param {NonNullable<Database['guildSettings'][Snowflake]>['lastMentions']}db*/
 function cleanupMentionsDB(guildId, db) {
   if (!db) return;
 
@@ -52,7 +52,7 @@ function cleanupMentionsDB(guildId, db) {
  * Removes all AFK-Messages older than one month
  * @this {Client}
  * @param {string}guildId
- * @param {Exclude<Database['guildSettings'][''], undefined>['afkMessages']}db*/
+ * @param {NonNullable<Database['guildSettings'][Snowflake]>['afkMessages']}db*/
 function cleanupAfkMessagesDB(guildId, db) {
   if (!db) return;
 
@@ -66,7 +66,7 @@ function cleanupAfkMessagesDB(guildId, db) {
  * Removes all AFK-Messages older than one month
  * @this {Client}
  * @param {string}guildId
- * @param {Exclude<Database['guildSettings'][''], undefined>['minigames']}db*/
+ * @param {NonNullable<Database['guildSettings'][Snowflake]>['minigames']}db*/
 function cleanUpMinigamesDB(guildId, db) {
   if (!db) return;
 

Utils/combinedCommands/lock_unlock.js
@@ -17,19 +17,22 @@ module.exports = async function lock_unlock(lang) {
       title: lang('embedTitle'),
       description: lang('embedDescription', { mod: this.user.username, reason }),
       color: Colors.Red
-    });
+    }),
+    roles = await this.guild.roles.fetch(),
+    members = await this.guild.members.fetch();
 
-  /** @type {Record<Snowflake, import('discord.js').OverwriteType> | undefined}*/
+  /** @type {NonNullable<import('../../types/database').Database['guildSettings'][Snowflake]>['lockedChannels']} */
   let overwrites;
 
   if (this.commandName == 'lock') {
-    overwrites = await channel.permissionOverwrites.cache.reduce(async (acc, e) => {
+    overwrites = channel.permissionOverwrites.cache.reduce((acc, e) => {
       if (
         e.allow.has(PermissionFlagsBits.SendMessages) && !e.allow.has(PermissionFlagsBits.Administrator)
-        && (e.type == OverwriteType.Role && (await this.guild.roles.fetch(e.id))?.editable || (await this.guild.members.fetch(e.id)).manageable)
-      ) return { ...await acc, [e.id]: e.type };
+        && (e.type == OverwriteType.Role && roles.get(e.id)?.editable || members.get(e.id).manageable)
+      ) acc[e.id] = e.type;
+
       return acc;
-    }, Promise.resolve({}));
+    }, {});
 
     if (
       this.guild.roles.everyone.editable
@@ -39,12 +42,10 @@ module.exports = async function lock_unlock(lang) {
     if (overwrites.__count__) await this.guild.updateDB(`lockedChannels.${channel.id}`, overwrites);
   }
   else {
-    /* eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- false positive */
-    overwrites = Object.entries(this.guild.db.lockedChannels?.[channel.id] ?? {}).filter(async ([k, v]) => {
-      if (channel.permissionOverwrites.cache.get(k)?.allow.has(PermissionFlagsBits.SendMessages)) return;
-      /* eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- false positive */
-      if (v == OverwriteType.Role) return (await this.guild.roles.fetch(k))?.position - this.guild.members.me.roles.highest.position < 0;
-      return (await this.guild.members.fetch(k)).manageable;
+    overwrites = Object.entries(this.guild.db.lockedChannels?.[channel.id] ?? {}).filter(([k, v]) => {
+      if (channel.permissionOverwrites.cache.get(k)?.allow.has(PermissionFlagsBits.SendMessages)) return false;
+      if (v == OverwriteType.Role) return roles.get(k).position - this.guild.members.me.roles.highest.position < 0;
+      return members.get(k).manageable;
     });
 
     if (!overwrites.length) return msg.edit(lang('notLocked'));

Utils/prototypeRegisterer/message__patch.js
@@ -11,7 +11,7 @@ module.exports = function _patch(data, ...rest) {
     let
       prefixLength = 0,
 
-      /** @type {Database<true>['guildSettings']['']['config']['prefixes']|Database<true>['guildSettings']['']['config']['betaBotPrefixes']}*/
+      /** @type {NonNullable<Database['guildSettings'][Snowflake]>['config']['prefixes'] | NonNullable<Database['guildSettings'][Snowflake]>['config']['betaBotPrefixes']}*/
       prefixes = this.guild?.db.config[prefixType];
 
     if (!prefixes?.[0].prefix) prefixes = this.client.defaultSettings.config[prefixType];

types/database.d.ts
@@ -92,8 +92,8 @@ type Database = {
       createdAt: Date;
     };
     birthday?: Date;
-    lastVoted?: Exclude<WebsiteDB['userSettings'][Snowflake], undefined>['lastVoted'];
-    featureRequestAutoApprove?: Exclude<WebsiteDB['userSettings'][Snowflake], undefined>['featureRequestAutoApprove'];
+    lastVoted?: NonNullable<WebsiteDB['userSettings'][Snowflake]>['lastVoted'];
+    featureRequestAutoApprove?: NonNullable<WebsiteDB['userSettings'][Snowflake]>['featureRequestAutoApprove'];
     lastFeatureRequested?: number;
     cmdStats?: cmdStats;
   } | undefined>;

commit fbe056d4b9fec9df4a940dfbef140dda0eaeaf2a

fixed an error introduced in d461083c233914a09346eef6b9495d148f6bd821

Events/messageDelete.js
@@ -68,7 +68,6 @@ module.exports = async function messageDelete() {
       description: lang('messageDelete.embedDescription', { executor: executor ? `<@${executor.id}>` : lang('someone'), channel: this.channel.name }),
       fields: [
         { name: lang('global.channel'), value: `<#${this.channel.id}> (\`${this.channel.id}\`)`, inline: true },
-        { name: lang('messageDelete.author'), value: `${this.user.tag} (\`${this.user.id}\`)`, inline: true },
         { name: lang('messageDelete.content'), value: '', inline: false }
       ],
       timestamp: Date.now(),
@@ -84,6 +83,8 @@ module.exports = async function messageDelete() {
   if (!field.value) field.value += lang('unknownContent');
   else if (field.value.length > 1024) field.value = field.value.slice(0, 1021) + '...';
 
+  // We don't get the user if the message is not cached
+  if (this.user) embed.data.fields.push({ name: lang('messageDelete.author'), value: `${this.user.tag} (\`${this.user.id}\`)`, inline: true });
   if (executor) embed.data.fields.push({ name: lang('executor'), value: `${executor.tag} (\`${executor.id}\`)`, inline: false });
   if (reason) embed.data.fields.push({ name: lang('reason'), value: reason, inline: false });
 