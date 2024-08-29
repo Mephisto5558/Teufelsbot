e93243eb0d94795b5aba8fa7723c925399da669f
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-08-29 18:51:17 +0200

    fixed not setting config

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -3,9 +3,10 @@ module.exports = async function guildCreate() {
   log.debug(`Joined new guild: ${this.id}`);
   if (this.client.botType == 'dev') return;
 
-  if (!this.guild.db.position)
+  if (!this.db.position)
     await this.updateDB('position', (Math.max(...Object.values(this.client.db.get('guildSettings')).map(e => e.position)) || 0) + 1);
-  if (!this.guild.db.config) await this.updateDB('config', {});
+  /* eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, @typescript-eslint/strict-boolean-expressions -- it is made sure here that it is set.*/
+  if (!this.db.config) await this.updateDB('config', {});
 
   await this.client.db.delete('guildSettings', `${this.id}.leftAt`);
 };
