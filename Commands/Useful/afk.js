908015e908c8825af0cd86112d45ebb05777d34e
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-08-31 14:54:29 +0200

    fixed #437

--- Commands/Useful/afk.js
+++ Commands/Useful/afk.js
@@ -35,6 +35,6 @@ module.exports = {
     }
 
     const global = this.options?.getBoolean('global') ?? this.args?.[0] == 'global';
-    return setAfkStatus.call(this, global, this.options?.getString('message') ?? this.content?.slice(global ? 7 : 0, 1000));
+    return setAfkStatus.call(this, lang, global, this.options?.getString('message') ?? this.content?.slice(global ? 7 : 0, 1000));
   }
 };
