c3afc17179db6d2c94c2846e150f283718abcab0
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-08-31 19:44:08 +0200

    fixed userinfo on a member without status
    Signed-off-by: Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>

--- Commands/Information/userinfo.js
+++ Commands/Information/userinfo.js
@@ -16,7 +16,7 @@ module.exports = new MixedCommand({
       member = getTargetMember(this, { returnSelf: true }),
       birthday = this.client.db.get('userSettings', `${member.id}.birthday`),
       bannerURL = (await member.user.fetch()).bannerURL(),
-      status = member.presence?.activities.find(e => e.type == ActivityType.Custom);
+      status = member.presence?.activities.find(e => e.type == ActivityType.Custom && e.state);
 
     let type = member.user.bot ? 'Bot, ' : '';
 
