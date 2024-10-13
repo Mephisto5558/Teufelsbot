c18cab93415b7593cd325b7735d0c236fbd21f3a
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-10-13 23:24:48 +0200

    massively improved the name conversion

--- Utils/getAllErrorCodes.js
+++ Utils/getAllErrorCodes.js
@@ -2,14 +2,15 @@ const
   fetch = require('node-fetch').default,
   { writeFile } = require('node:fs/promises');
 
-/** Writes all error code to a file*/
+/** Writes all error codes to a file*/
 module.exports = async function fetchAndProcess() {
   const res = await fetch('https://gist.githubusercontent.com/Dziurwa14/de2498e5ee28d2089f095aa037957cbb/raw/codes.md').then(e => e.text());
   const codes = res.split('\n').reduce((acc, line) => {
-    const [code, description] = line.slice(2, -2).split(' | ');
+    const [code, description] = line.slice(2, -2).replaceAll(/[',`’]|[(.:].*/g, '').split(' | ');
 
     if (Number(code) && description) {
-      const name = description.split(/\s+/g).reduce((nameAcc, word) => nameAcc + word[0].toUpperCase() + word.slice(1), '').replaceAll(/\(.*?\)|\..*/g, '');
+      const name = description.trim().split(/[\s\-_]+/)
+        .reduce((nameAcc, word) => nameAcc + word[0].toUpperCase() + (/^[ A-Z_]+$/.test(description) ? word.slice(1).toLowerCase() : word.slice(1)), '');
       acc[name] = Number(code);
     }
 
