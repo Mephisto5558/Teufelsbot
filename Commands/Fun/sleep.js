4eae908e7f7830caf2517969c63a87b14c95bc03
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-10-27 14:12:33 +0100

    fixed afk set with .sleep

--- Commands/Fun/sleep.js
+++ Commands/Fun/sleep.js
@@ -7,7 +7,7 @@ module.exports = {
   dmPermission: true,
 
   async run(lang) {
-    void setAfkPrefix(this.member, 'set');
+    void setAfkPrefix(this.member);
 
     await this.user.updateDB('afkMessage', { message: lang('afkMessage'), createdAt: this.createdAt });
     return this.customReply(lang('responseList', { user: this.member.customName, emoji: [getEmoji('angel'), getEmoji('derp_ball')].random() }));
