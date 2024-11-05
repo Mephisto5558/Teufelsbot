ad0608bc68d6d6ee1b60efdcb2ac309e7a572269
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-11-05 12:52:46 +0100

    added the last clicker

--- Utils/componentHandler/clickCounter.js
+++ Utils/componentHandler/clickCounter.js
@@ -1,16 +1,19 @@
 /** @type {import('.').clickCounter}*/
-module.exports = async function clickCounter(_, count) {
+module.exports = async function clickCounter(lang, count) {
+  lang.__boundArgs__[0].backupPath = 'commands.minigames.clickcounter';
+
   const
     embed = this.message.embeds[0].data,
     button = this.message.components[0].components[0].data;
 
   count = Number(count) + 1;
 
-  /* eslint-disable sonarjs/sonar-no-magic-numbers -- `-1` is last item */
+  /* eslint-disable camelcase, sonarjs/sonar-no-magic-numbers -- `-1` is last item */
   embed.description = embed.description.split(':').toSpliced(-1, 1, count).join(': ');
-  /* eslint-disable-next-line camelcase -- not my decision*/
+  embed.footer = { text: lang('embedFooterText', this.member.displayName), icon_url: this.member.displayAvatarURL() };
+
   button.custom_id = this.customId.split('.').toSpliced(-1, 1, count).join('.');
-  /* eslint-enable sonarjs/sonar-no-magic-numbers */
+  /* eslint-enable camelcase, sonarjs/sonar-no-magic-numbers */
 
   return this.update({ embeds: this.message.embeds, components: this.message.components });
 };
