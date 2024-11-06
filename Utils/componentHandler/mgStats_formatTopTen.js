3525359cb836ed48ddd24696c949ed1cc8111507
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-11-06 10:14:20 +0100

    fixed a typo

--- Utils/componentHandler/mgStats_formatTopTen.js
+++ Utils/componentHandler/mgStats_formatTopTen.js
@@ -6,15 +6,15 @@ const
 module.exports = function formatTopTen(input, sort, mode, lang, maxLength = messageMaxLength) {
   if (input.length > 1) {
     switch (mode) {
-      case 'draws': input.sort(([, a], [, b]) => b.draws - a.draws || b.wins - a.wins || a.loses - b.loses); break;
-      case 'loses': input.sort(([, a], [, b]) => b.loses - a.loses || a.wins - b.wins || b.draws - a.draws); break;
+      case 'draws': input.sort(([, a], [, b]) => b.draws - a.draws || b.wins - a.wins || a.losses - b.losses); break;
+      case 'losses': input.sort(([, a], [, b]) => b.losses - a.losses || a.wins - b.wins || b.draws - a.draws); break;
       case 'alphabet_user': input.sort(([, a], [, b]) => this.guild.members.cache.get(a).user.username.localeCompare(
         this.guild.members.cache.get(b).user.username, lang.__boundArgs__[0].locale, { sensitivity: 'base' }
       )); break;
       case 'alphabet_nick': input.sort(([, a], [, b]) => this.guild.members.cache.get(a).displayName.localeCompare(
         this.guild.members.cache.get(b).displayName, lang.__boundArgs__[0].locale, { sensitivity: 'base' }
       )); break;
-      default: input.sort(([, a], [, b]) => b.wins - a.wins || a.draws - b.draws || a.loses - b.loses);
+      default: input.sort(([, a], [, b]) => b.wins - a.wins || a.draws - b.draws || a.losses - b.losses);
     }
 
     if (sort == 'f') input.reverse();
@@ -25,7 +25,7 @@ module.exports = function formatTopTen(input, sort, mode, lang, maxLength = mess
       ? '...'
       : `${medals[i] ?? i + 1 + '.'} <@${id}>\n`
         + '> ' + lang('wins', stats.wins ?? 0)
-        + '> ' + lang('loses', stats.loses ?? 0)
+        + '> ' + lang('losses', stats.losses ?? 0)
         + '> ' + lang('draws', stats.draws ?? 0)
   ), '');
 };
