7742d474e7f865606c7aef1a4fb5ebbdaf972222
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-11-15 18:02:07 +0100

    formatting

--- Utils/componentHandler/advice.js
+++ Utils/componentHandler/advice.js
@@ -1,8 +1,8 @@
+/** @type {import('..').commandExecutionWrapper}*/
 const commandExecutionWrapper = require('../commandExecutionWrapper.js');
 
 /** @type {import('.').advice}*/
 module.exports = async function advice(lang) {
   await this.update({ components: [] });
-
   return commandExecutionWrapper.call(this, this.client.slashCommands.get('advice'), 'component', lang);
 };
