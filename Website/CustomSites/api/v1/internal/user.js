096479af7c54170fcd5dbea034a5ddce8af2df71
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-11-16 17:17:56 +0100

    small improvement

--- Website/CustomSites/api/v1/internal/user.js
+++ Website/CustomSites/api/v1/internal/user.js
@@ -3,7 +3,11 @@ const { HTTP_STATUS_FORBIDDEN, HTTP_STATUS_UNAUTHORIZED } = require('node:http2'
 /** @type {import('@mephisto5558/bot-website').customPage}*/
 module.exports = {
   run(res, req) {
-    if (this.db.get('botSettings', 'blacklist').includes(req.user?.id)) return res.json({ errorCode: HTTP_STATUS_FORBIDDEN, error: 'You have been blacklisted from using the bot.' });
-    return res.json(req.user ? { ...req.user, dev: this.client.config.devIds.has(req.user.id) } : { errorCode: HTTP_STATUS_UNAUTHORIZED, error: 'Not logged in' });
+    if (this.db.get('botSettings', 'blacklist').includes(req.user?.id))
+      return res.status(HTTP_STATUS_FORBIDDEN).json({ errorCode: HTTP_STATUS_FORBIDDEN, error: 'You have been blacklisted from using the bot.' });
+    if (!req.user)
+      return res.status(HTTP_STATUS_UNAUTHORIZED).json({ errorCode: HTTP_STATUS_UNAUTHORIZED, error: 'Not logged in' });
+
+    return res.json({ ...req.user, dev: this.client.config.devIds.has(req.user.id) });
   }
 };
