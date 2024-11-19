f419fbc0361141d393c60dc65c7753e66686fce4
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-11-19 14:57:12 +0100

    fixed redirect upon login in voting page

--- Website/CustomSites/auth/discord.js
+++ Website/CustomSites/auth/discord.js
@@ -1,7 +1,7 @@
 /** @type {import('@mephisto5558/bot-website').customPage}*/
 module.exports = {
   run(res, req, next) {
-    if ([undefined, ''].includes(req.query.redirectURL)) req.session.redirectURL = req.query.redirectURL;
+    if (req.query.redirectURL) req.session.redirectURL = req.query.redirectURL;
     return this.passport.authenticate('discord')(req, res, next);
   }
 };
