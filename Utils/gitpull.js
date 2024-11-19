e6cec8505718c550294a572a6bf5b5d39b9199b4
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-11-19 15:52:44 +0100

    not logging anymore when not pushing to the main branch, main branch is looked for automatically

--- Utils/gitpull.js
+++ Utils/gitpull.js
@@ -1,5 +1,7 @@
 /** @type {import('.').shellExec} */
-const shellExec = require('./shellExec.js');
+const
+  shellExec = require('./shellExec.js'),
+  origin = /'(?<branch>.*)'/.exec((await shellExec('git status').catch(() => { /** empty */ }))?.stdout.split('\n')[1] ?? '')?.groups.branch;
 
 /** @type {import('.').gitpull}*/
 module.exports = async function gitpull() {
@@ -10,6 +12,8 @@ module.exports = async function gitpull() {
     log.error(`GIT PULL\nExec error: ${err}`);
     return err;
   }
+
+  if (origin && !data.stderr.includes(`-> ${origin}`)) return 'OK';
 
   log(
     'GIT PULL\n'
