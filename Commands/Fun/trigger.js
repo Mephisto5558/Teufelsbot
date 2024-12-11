459c1df9bbf9b8eb9087625d49bc630cfc1fbb86
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-12-11 15:15:44 +0100

    added an option to edit a trigger

--- Commands/Fun/trigger.js
+++ Commands/Fun/trigger.js
@@ -2,6 +2,14 @@
  * @typedef {NonNullable<NonNullable<Database['guildSettings'][Snowflake]>['triggers']>}triggers
  * @typedef {[keyof triggers, NonNullable<triggers[keyof triggers]>]}triggersArray *//* eslint-disable-line jsdoc/valid-types -- false positive */
 
+/**
+ * @param {string}query
+ * @param {triggers}data
+ * @returns {keyof triggers | undefined}*/
+const findTriggerId = (query, data) => query in data
+  ? query
+  : Object.entries(data).find((/** @type {triggersArray} */[, { trigger }]) => trigger.toLowerCase() == query.toLowerCase())?.[0];
+
 const
   { EmbedBuilder, Colors, codeBlock } = require('discord.js'),
   { embedMaxFieldAmt, suffix } = require('#Utils').constants,
@@ -21,15 +29,27 @@ const
       return this.editReply(lang('saved', data.trigger));
     },
 
-    async delete(lang, oldData, query) {
-      let id;
-      if (query) {
-        id = query in oldData
-          ? query
-          : Object.entries(oldData).find((/** @type {triggersArray} */[tId, { trigger }]) => trigger.toLowerCase() == query.toLowerCase() || tId.toLowerCase() == query.toLowerCase())?.[0];
-      }
-      else id = Math.max(...Object.keys(oldData).map(Number)); // Returns `-Infinity` on an empty array
+    async edit(lang, oldData, query) {
+      if (!oldData.__count__) return this.editReply(lang('noneFound'));
 
+      const
+        id = findTriggerId(query, oldData),
+        /** @type {triggersArray[1]} */ { trigger, response, wildcard } = query ? oldData[id] : undefined;
+
+      if (!trigger) return this.editReply(lang('notFound'));
+
+      const data = {
+        trigger: this.options.getString('trigger') ?? trigger,
+        response: this.options.getString('response')?.replaceAll('/n', '\n') ?? response,
+        wildcard: this.options.getBoolean('wildcard') ?? wildcard
+      };
+
+      await this.guild.updateDB(`triggers.${id}`, data);
+      return this.editReply(lang('saved', data.trigger));
+    },
+
+    async delete(lang, oldData, query) {
+      const id = query ? Number(findTriggerId(query, oldData) ?? -1) : Math.max(...Object.keys(oldData).map(Number));
       if (id < 0) return this.editReply(lang('noneFound'));
 
       await this.client.db.delete('guildSettings', `${this.guild.id}.triggers.${id}`);
@@ -51,7 +71,7 @@ const
 
       if (query) {
         /** @type {triggersArray} */
-        const [id, { trigger, response, wildcard }] = Object.entries(oldData).find(([k, v]) => k == query || v.trigger.toLowerCase() == query) ?? {};
+        const [id, { trigger, response, wildcard }] = oldData[findTriggerId(query, oldData)] ?? {};
         if (!trigger) return this.editReply(lang('notFound'));
 
         const maxLength = 1900;
@@ -95,7 +115,7 @@ const
 
 /**
  * @this {import('discord.js').AutocompleteInteraction}
- * @returns  {string[]} */
+ * @returns {string[]} */
 function triggerQuery() {
   return Object.entries(this.guild.db.triggers ?? {}).reduce((acc, [k, v]) => {
     acc[0].push(v.trigger);
@@ -125,6 +145,20 @@ module.exports = {
           type: 'String',
           required: true
         },
+        { name: 'wildcard', type: 'Boolean' }
+      ]
+    },
+    {
+      name: 'edit',
+      type: 'Subcommand',
+      options: [
+        {
+          name: 'query_or_id',
+          type: 'String',
+          autocompleteOptions: triggerQuery
+        },
+        { name: 'trigger', type: 'String' },
+        { name: 'response', type: 'String' },
         { name: 'wildcard', type: 'Boolean' }
       ]
     },
