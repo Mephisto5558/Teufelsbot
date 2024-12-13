e1d50a2e7953d07541628de1241d4256c59995d7
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-12-13 17:31:00 +0100

    removed duplicate code

--- Commands/Moderation/unlock.js
+++ Commands/Moderation/unlock.js
@@ -1,17 +1,1 @@
-const
-  { Constants } = require('discord.js'),
-  { msInSecond } = require('#Utils').timeFormatter;
-
-/** @type {command<'both'>} */
-module.exports = {
-  permissions: { client: ['ManageRoles'], user: ['ManageRoles'] },
-  cooldowns: { user: msInSecond },
-  slashCommand: true,
-  prefixCommand: true,
-  options: [
-    { name: 'channel', type: 'Channel', channelTypes: Constants.GuildTextBasedChannelTypes },
-    { name: 'reason', type: 'String' }
-  ],
-
-  run: require('#Utils/combinedCommands').lock_unlock
-};
+module.exports = require('./lock.js');
