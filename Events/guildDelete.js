c43c0a8788dd31c5cee1a101402b2306f8f86fdc
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-01-02 14:52:42 +0100

    fixed the bot thinking all guilds are gone

--- Events/guildDelete.js
+++ Events/guildDelete.js
@@ -1,5 +1,7 @@
 /** @this {import('discord.js').ClientEvents['guildDelete'][0]} */
 module.exports = async function guildDelete() {
+  if (this.client.guilds.cache.has(this.id)) return; // guild unavailable
+
   log.debug(`Left guild: ${this.id}`);
   await this.updateDB('leftAt', new Date());
 };
