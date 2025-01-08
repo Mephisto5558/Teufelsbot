103630eab7634eb383f8af3b4a223ab1375f61fa
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-01-08 08:23:27 +0100

    added template for subcommand files

---
+++ Templates/subcommand.js
@@ -0,0 +1,13 @@
+// This template is for use in subcommand(groups) that have their own file.
+
+module.exports = {
+  /** @type {NonNullable<command<'slash'>['options']>[number]['options']} */
+  options: [
+
+  ],
+
+  /** @type {command<'slash'>['run']} */
+  async run(lang) {
+
+  }
+};
