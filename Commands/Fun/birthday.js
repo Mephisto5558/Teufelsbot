d3d7d5c123380debea69b3de03bf2954ee67d561
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-01-18 14:55:03 +0100

    users with a birthday today get an emoji and are shown on top of the list.

--- Commands/Fun/birthday.js
+++ Commands/Fun/birthday.js
@@ -6,14 +6,14 @@ const
 /**
  * @param {Date | number} a
  * @param {Date | number | undefined} b @default Date.now()
- * @param {Date | number | undefined} currentTime @default Date.now()
  * @returns {number} negative if `a > b`, positive if `a < b`, `0` otherwise */
-function sortDates(a, b = Date.now(), currentTime = Date.now()) {
+function sortDates(a, b = Date.now()) {
   const
-    diffA = new Date(a).setFullYear(currentYear) - currentTime,
-    diffB = new Date(b).setFullYear(currentYear) - currentTime;
+    todayMs = new Date().setHours(0, 0, 0, 0),
+    diffA = new Date(a).setFullYear(currentYear) - todayMs,
+    diffB = new Date(b).setFullYear(currentYear) - todayMs;
 
-  if (diffA * diffB > 0) return diffA - diffB; // both are positive or both are negative
+  if (diffA * diffB >= 0) return diffA - diffB; // both are positive (or 0) OR both are negative (or 0)
   return diffA <= 0 && diffB >= 0 ? 1 : -1;
 }
 
@@ -72,7 +72,7 @@ const birthdayMainFunctions = {
 
       const
         guildMembers = new Set((await this.guild.members.fetch()).map(e => e.id)),
-        currentTime = Date.now(),
+        today = new Date(),
 
         /** @type {[Snowflake, Date][]} */
         data = Object.entries(this.client.db.get('userSettings'))
@@ -80,17 +80,19 @@ const birthdayMainFunctions = {
             if (birthday && guildMembers.has(k)) acc.push([k, birthday]);
             return acc;
           }, [])
-          .sort(([, a], [, b]) => sortDates(a, b, currentTime))
+          .sort(([, a], [, b]) => sortDates(a, b))
           .slice(0, 10);
 
       embed.data.description = data.length ? '' : lang('getAll.notFound');
       for (const [id, date] of data) {
         const
-          dateStr = lang('getAll.date', { month: lang(`months.${date.getMonth() + 1}`), day: date.getDate() }),
+          dateStr = date.getMonth() == today.getMonth() && date.getDate() == today.getDate()
+            ? `${lang('getAll.today')} 🎉`
+            : lang('getAll.date', { month: lang(`months.${date.getMonth() + 1}`), day: date.getDate() }),
           age = getAge(date) + 1,
           msg = '> ' + userMention(id) + (age < currentYear ? ` (${age})` : '') + '\n';
 
-        embed.data.description += embed.data.description.includes(dateStr) ? msg : `\n${dateStr}${msg}`;
+        embed.data.description += embed.data.description.includes(dateStr) ? msg : `\n${dateStr}\n${msg}`;
       }
     }
 
