e9633333799507be72b59eb8ccff82375a12f76f
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-07-10 11:20:02 +0200

    fixed using `newData` instead of `data`

--- Website/DashboardSettings/config/prefix.js
+++ Website/DashboardSettings/config/prefix.js
@@ -10,6 +10,6 @@ module.exports = {
     return (this.db.get('guildSettings', `${options.guild.id}.${option.optionId}`) ?? this.db.get('botSettings', `defaultGuild.${option.optionId}`)).map(e => e.prefix);
   },
   async set(option, options) {
-    return this.db.pushToSet('guildSettings', `${options.guild.id}.${option.optionId}`, ...options.newData);
+    return this.db.pushToSet('guildSettings', `${options.guild.id}.${option.optionId}`, ...options.data);
   }
 };
