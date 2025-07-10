b3b7c2c07de81553ff73d471873cd0e802ff001d
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-07-10 09:53:00 +0200

    added average color to `/avatar`

--- Commands/Information/avatar.js
+++ Commands/Information/avatar.js
@@ -1,5 +1,6 @@
 const
-  { EmbedBuilder, Colors, ActionRowBuilder, ButtonBuilder, ButtonStyle, ALLOWED_SIZES, bold } = require('discord.js'),
+  { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ALLOWED_SIZES, bold, hyperlink } = require('discord.js'),
+  { getAverageColor } = require('fast-average-color-node'),
   { getTargetMembers, timeFormatter: { msInSecond } } = require('#Utils');
 
 /** @type {command<'both', false>} */
@@ -26,10 +27,12 @@ module.exports = {
           ?? (ALLOWED_SIZES.includes(Number.parseInt(this.args?.at(-1))) ? this.args?.at(-1) : undefined)
           ?? ALLOWED_SIZES.at(-2) /* eslint-disable-line @typescript-eslint/no-magic-numbers -- 2nd largest resolution */
       }),
+      averageColor = (await getAverageColor(target.displayAvatarURL())).hex,
       embed = new EmbedBuilder({
         description: bold(lang('embedDescription', target.user?.username ?? target.username)),
-        color: Colors.White,
         image: { url: avatarURL },
+        fields: [{ name: lang('averageColor'), value: hyperlink(averageColor, `https://www.color-hex.com/color/${averageColor.slice(1)}`), inline: true }],
+        color: Number.parseInt(averageColor.slice(1), 16),
         footer: { text: this.user.username }
       }),
       component = new ActionRowBuilder({
