d3290e3b6e2b309b8d3f582d9ffc713f388a1e95
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-09-17 15:25:20 +0200

    move from `passport-discord` to `passport-discord-auth`

--- Website/CustomSites/api/v1/internal/user.js
+++ Website/CustomSites/api/v1/internal/user.js
@@ -5,16 +5,16 @@ const
 /** @type {import('#types/locals').customPage} */
 module.exports = {
   run(res, req) {
-    if (this.db.get('botSettings', 'blacklist')?.includes(req.user?.id))
-      return res.status(HTTP_STATUS_FORBIDDEN).json({ errorCode: HTTP_STATUS_FORBIDDEN, error: 'You have been blacklisted from using the bot.' });
-    if (!req.user)
+    if (!req.isAuthenticated())
       return res.status(HTTP_STATUS_UNAUTHORIZED).json({ errorCode: HTTP_STATUS_UNAUTHORIZED, error: 'Not logged in' });
+    if (this.db.get('botSettings', 'blacklist')?.includes(req.user.id))
+      return res.status(HTTP_STATUS_FORBIDDEN).json({ errorCode: HTTP_STATUS_FORBIDDEN, error: 'You have been blacklisted from using the bot.' });
 
     return res.json({
       ...req.user,
       avatarURL: this.client.users.cache.get(req.user.id)
       /* eslint-disable-next-line @typescript-eslint/no-magic-numbers -- 1024 */
-        ?.avatarURL({ size: req.user.avatar.match(/\?size=(?<size>\d+)/)?.groups?.size ?? ALLOWED_SIZES[6] }) ?? req.user.avatar,
+        ?.avatarURL({ size: /\?size=(?<size>\d+)/.exec(req.user.avatar)?.groups?.size ?? ALLOWED_SIZES[6] }) ?? req.user.avatar,
       dev: this.client.config.devIds.has(req.user.id)
     });
   }
