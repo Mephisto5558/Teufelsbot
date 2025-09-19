ed73ac159f15f7f00a96eb22ebef97e7ecca296a
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-09-19 13:20:11 +0200

    remove pages now handled by `@mephisto5558/bot-website`

--- Website/CustomSites/auth/discord.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@mephisto5558/bot-website').customPage} */
-module.exports = {
-  run(res, req, next) {
-    if (typeof req.query.redirectURL == 'string') req.session.redirectURL = req.query.redirectURL;
-    return this.passport.authenticate('discord')(req, res, next);
-  }
-};
