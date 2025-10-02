d4906841f9f4b89eea9b8f6f17e8152ca8504a9f
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-10-02 08:48:03 +0200

    remove hardcoded devIds from ship

--- Commands/Fun/ship.js
+++ Commands/Fun/ship.js
@@ -1,16 +1,43 @@
 const
+  { Team } = require('discord.js'),
   { createHash } = require('node:crypto'),
   { getTargetMembers } = require('#Utils'),
   maxPercentage = 100,
   hashPartLength = 5,
+  botIds = ['964315306015211560', '948978571802710047'],
+  botDevShip = 80,
+  /** @type {[Snowflake, Snowflake, number][]} */ customShips = [];
 
-  /** @type {[Snowflake, Snowflake, number][]} */
-  customShips = [
-    /* eslint-disable @typescript-eslint/no-magic-numbers */
-    ['691550551825055775', '948978571802710047', 80],
-    ['691550551825055775', '964315306015211560', 80]
-    /* eslint-enable @typescript-eslint/no-magic-numbers */
-  ];
+/**
+ * @this {Message}
+ * @param {Snowflake} target1
+ * @param {Snowflake} target2 */
+function getCustomShipPercentage(target1, target2) {
+  const { owner } = this.client.application;
+  if (owner && botIds.some(e => e == target1 || e == target2)) {
+    const devIds = owner instanceof Team ? [...owner.members.keys()] : [owner.id];
+    if (devIds.some(e => e == target1 || e == target2)) return botDevShip;
+  }
+
+  /* eslint-disable-next-line sonarjs/no-empty-collection -- may be filled in the future */
+  return customShips.find(e => e.includes(target1) && e.includes(target2))?.[2];
+}
+
+
+/**
+ * @this {Message}
+ * @param {Snowflake} user1Id
+ * @param {Snowflake} user2Id */
+function calculatePercentage(user1Id, user2Id) {
+  const customShip = getCustomShipPercentage.call(this, user1Id, user2Id);
+  if (customShip) return customShip;
+
+  const combinedHash = createHash('sha256').update(
+    createHash('sha256').update(String(BigInt(user1Id) + BigInt(user2Id))).digest('hex')
+  ).digest('hex');
+
+  return Number.parseInt(combinedHash.slice(0, hashPartLength), 16) % (maxPercentage + 1);
+}
 
 /** @type {command<'both'>} */
 module.exports = {
@@ -29,20 +56,6 @@ module.exports = {
     const [user1, user2] = getTargetMembers(this, [{ targetOptionName: 'user1' }, { targetOptionName: 'user2', returnSelf: true }]);
 
     if (!user1) return this.customReply(lang('global.unknownUser'));
-    return this.customReply(`${user1.customName} :heart: ${user2.customName}: ${calculatePercentage(user1, user2)}%`);
+    return this.customReply(`${user1.customName} :heart: ${user2.customName}: ${calculatePercentage.call(this, user1, user2)}%`);
   }
 };
-
-/**
- * @param {Snowflake} user1Id
- * @param {Snowflake} user2Id */
-function calculatePercentage(user1Id, user2Id) {
-  const customShip = customShips.find(e => e[0] == user1Id && e[1] == user2Id || e[0] == user2Id && e[1] == user1Id);
-  if (customShip) return customShip[2];
-
-  const combinedHash = createHash('sha256').update(
-    createHash('sha256').update(String(BigInt(user1Id) + BigInt(user2Id))).digest('hex')
-  ).digest('hex');
-
-  return Number.parseInt(combinedHash.slice(0, hashPartLength), 16) % (maxPercentage + 1);
-}
