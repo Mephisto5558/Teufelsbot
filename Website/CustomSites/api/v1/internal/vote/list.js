6b74f01ffc61d048e033492c5f02efbd97c75ff8
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-11-27 13:13:31 +0100

    add missing await

--- Website/CustomSites/api/v1/internal/vote/list.js
+++ Website/CustomSites/api/v1/internal/vote/list.js
@@ -4,8 +4,8 @@ const { HTTP_STATUS_OK } = require('node:http2').constants;
 
 /** @type {customPage} */
 module.exports = {
-  run(res, req) {
-    const reply = this.voteSystem.getMany(
+  async run(res, req) {
+    const reply = await this.voteSystem.getMany(
       Number.parseInt(req.query.amount) || undefined, Number.parseInt(req.query.offset ?? 0), req.query.filter,
       req.query.includePending == 'true', req.user?.id
     );
