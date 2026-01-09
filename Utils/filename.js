34e216f4cceb284557e16f02af59d0f04504c9ac
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-01-09 14:33:47 +0100

    remove unused utils

--- Utils/filename.js
+++
@@ -1,8 +0,0 @@
-/** @import { filename } from '.' */
-
-const { basename, extname } = require('node:path');
-
-/** @type {filename} */
-module.exports = function getFilename(path) {
-  return basename(path, extname(path));
-};
--- Utils/getDirectories.js
+++
@@ -1,8 +0,0 @@
-/** @import { getDirectories } from '.' */
-
-const { readdir } = require('node:fs/promises');
-
-/** @type {getDirectories} */
-module.exports = async function getDirectories(path) {
-  return (await readdir(path, { withFileTypes: true })).reduce((acc, e) => e.isDirectory() ? [...acc, e.name] : acc, []);
-};
