2e517acf51cef9d4602886d4a214291a48070353
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-01-29 14:59:13 +0100

    fix find&replace error

--- Utils/updateCommandStats.js
+++ Utils/updateCommandStats.js
@@ -6,18 +6,18 @@ const
 
 /** @type {commandDoneFn} */
 module.exports = async function updateCommandStats(command) {
-  if (!this.client.settings.commandStats[command.name]?.createdAt)
-    await this.client.db.update('botSettings', `commandStats.${command.name}.createdAt`, new Date());
+  if (!this.client.settings.cmdStats[command.name]?.createdAt)
+    await this.client.db.update('botSettings', `cmdStats.${command.name}.createdAt`, new Date());
 
   if (this.client.botType != 'dev') {
     const commandType = this instanceof CommandInteraction ? commandTypes.slash : commandTypes.prefix;
 
     await this.client.db.update(
-      'botSettings', `commandStats.${command.name}.${commandType}`,
-      (this.client.settings.commandStats[command.name]?.[commandType] ?? 0) + 1
+      'botSettings', `cmdStats.${command.name}.${commandType}`,
+      (this.client.settings.cmdStats[command.name]?.[commandType] ?? 0) + 1
     );
-    await this.user.updateDB(`commandStats.${command.name}.${commandType}`, (this.user.db.commandStats?.[command.name]?.[commandType] ?? 0) + 1);
+    await this.user.updateDB(`cmdStats.${command.name}.${commandType}`, (this.user.db.cmdStats?.[command.name]?.[commandType] ?? 0) + 1);
     if (this.inGuild())
-      await this.guild.updateDB(`commandStats.${command.name}.${commandType}`, (this.guild.db.commandStats?.[command.name]?.[commandType] ?? 0) + 1);
+      await this.guild.updateDB(`cmdStats.${command.name}.${commandType}`, (this.guild.db.cmdStats?.[command.name]?.[commandType] ?? 0) + 1);
   }
 };
