1224e7bf0c9fdc2811ecb1c9c634b107a6578fb3
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2026-02-04 19:48:36 +0100

    maybe fix spawning zombie clients on emoji sync while starting up

--- TimeEvents/syncEmojis.js
+++ TimeEvents/syncEmojis.js
@@ -9,18 +9,19 @@ const
  * @param {string} env
  * @param {string} token */
 async function getClient(env, token) {
-  if (!token) return;
-
   const client = new Client({ intents: [] });
   try { await client.login(token); }
   catch (err) {
+    await client.destroy().catch(() => { /* empty */ });
+
     if (err.code == DiscordAPIErrorCodes.InvalidToken) return void log.error(`Invalid token ${token} for env "${env}"`);
     throw err;
   }
+
   await client.application.fetch();
   await client.application.emojis.fetch();
 
-  log.debug(`Logged into "${client.application.name}" (${env})`);
+  log.debug(`Temporarily logged into "${client.application.name}" (${env})`);
   return client;
 }
 
@@ -61,30 +62,35 @@ module.exports = {
 
     if (this?.settings.timeEvents.lastEmojiSync?.toDateString() == now.toDateString()) return void log('Already ran emoji sync today');
 
-    if (this instanceof Client && this.isReady()) {
-      if (!this.application.name) await this.application.fetch();
-      if (!this.application.emojis.cache.size) await this.application.emojis.fetch();
-    }
-
     log('Started emoji sync').debug('Started emoji sync');
 
-    const clients = await (await readdir('.', { withFileTypes: true })).reduce(async (acc, e) => {
+    const clients = await (await readdir('.', { withFileTypes: true })).reduce(async (/** @type {Promise<Client<true>[]>} */ acc, e) => {
       if (!e.isFile() || !e.name.startsWith('.env')) return acc;
 
       const { token, environment = 'main' } = parseEnv(await readFile(join(e.parentPath, e.name), 'utf8'));
       if (!token || (await acc).some(e => e.token == token)) return acc;
 
+      if (this?.token == token && this.isReady()) {
+        await this.application.fetch();
+        await this.application.emojis.fetch();
+
+        (await acc).push(this);
+        return acc;
+      }
+
       const client = await getClient(environment, token);
       if (client) (await acc).push(client);
 
       return acc;
-    }, Promise.resolve(this?.isReady() ? [this] : []));
+    }, Promise.resolve([]));
 
-    if (clients.length < 2) log('Not enough clients to sync.');
-    else await syncClientsEmojis(clients);
-
-    // log out of all clients except the original one
-    for (const client of clients) if (client != this) void client.destroy();
+    try {
+      if (clients.length < 2) log('Not enough clients to sync.');
+      else await syncClientsEmojis(clients);
+    }
+    finally {
+      for (const client of clients) if (client != this) void client.destroy();
+    }
 
     await this?.db.update('botSettings', 'timeEvents.lastEmojiSync', now);
 
